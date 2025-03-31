84e70a3aa8ac265c76f498dbe2a151bd6508a16c
Yesid Llamas <yellaber@gmail.com>
2025-03-30 21:48:52 -0500

    src/app/environments/ creada

---
+++ src/app/environments/environment.developments.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  appNamePre: 'Movie',
+  appNamePost: 'Verso',
+  slogan: 'Tu universo de peliculas',
+  tmdbApiKey: 'c6eb6487500a7727c7f8fec7a57eb1ab',
+  tmdbApiUrl: 'https://api.themoviedb.org/3'
+};
