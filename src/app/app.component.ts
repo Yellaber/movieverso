525b9fc04ae66b9a1cd5696819dceefe94f83d2e
Yesid Llamas <yellaber@gmail.com>
2025-03-30 21:39:17 -0500

    app.component.ts creado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenubarComponent } from "./components/menubar/menubar.component";
+import { FooterSiteComponent } from "./components/footer-site/footer-site.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MenubarComponent,
+    FooterSiteComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'movieverso';
+  title = 'MovieVerso';
 }
