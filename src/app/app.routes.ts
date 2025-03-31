7d0681acc967d7f93ff774bd6af7043d40089d15
Yesid Llamas <yellaber@gmail.com>
2025-03-30 21:40:57 -0500

    app.routes.ts creado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/home-page/home-page.component')
+  },
+  {
+    path: 'nuevo',
+    loadComponent: () => import('./pages/new-page/new-page.component')
+  },
+  {
+    path: 'popular',
+    loadComponent: () => import('./pages/popular-page/popular-page.component')
+  },
+  {
+    path: 'listado',
+    loadComponent: () => import('./pages/list-page/list-page.component')
+  },
+  {
+    path: 'buscar',
+    loadComponent: () => import('./pages/search-page/search-page.component')
+  },
+  {
+    path: 'iniciar-sesion',
+    loadComponent: () => import('./pages/sign-in-page/sign-in-page.component')
+  },
+  {
+    path: 'registro',
+    loadComponent: () => import('./pages/sign-up-page/sign-up-page.component')
+  },
+  {
+    path: 'politica-de-privacidad',
+    loadComponent: () => import('./pages/politica-privacidad/politica-privacidad.component')
+  },
+  {
+    path: 'terminos-y-condiciones',
+    loadComponent: () => import('./pages/terminos-condiciones/terminos-condiciones.component')
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
