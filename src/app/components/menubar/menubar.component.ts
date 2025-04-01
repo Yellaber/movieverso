2400dd51a8283b8acb51e29fabab02bff8b0e733
Yesid Llamas <yellaber@gmail.com>
2025-04-01 13:59:51 -0500

    menubar.component.ts modificado

--- src/app/components/menubar/menubar.component.ts
+++ src/app/components/menubar/menubar.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { LogoComponent } from './logo/logo.component';
-import { NavigationBarComponent } from '../../shared/navigation-bar/navigation-bar.component';
-import { SearchBarComponent } from '../../shared/search-bar/search-bar.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 
 @Component({
   selector: 'menubar',
   imports: [
     LogoComponent,
-    NavigationBarComponent,
+    NavigationComponent,
     SearchBarComponent,
     SignInComponent
 ],
