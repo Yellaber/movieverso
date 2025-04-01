a5301b3dd85f92ea0e3d2eb4921e887a02725b63
Yesid Llamas <yellaber@gmail.com>
2025-04-01 14:03:39 -0500

    src/app/components/menubar/navigation/ creado

---
+++ src/app/components/menubar/navigation/navigation.component.ts
@@ -0,0 +1,33 @@
+import { Component, input } from '@angular/core';
+import { Route, RouterLink, RouterLinkActive } from '@angular/router';
+import { routes } from '../../../app.routes';
+import { NgClass, TitleCasePipe } from '@angular/common';
+
+@Component({
+  selector: 'navigation-bar',
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    NgClass,
+    TitleCasePipe
+  ],
+  template: `
+    <ul [ngClass]="layoutClass()">
+      @for (route of routes; track $index) {
+        <li>
+          <a [routerLink]="[route.path]" routerLinkActive="yellow-color" class="text-xs lg:text-sm transition-colors duration-300">{{ route.path | titlecase }}</a>
+        </li>
+      }
+    </ul>
+  `
+})
+export class NavigationComponent {
+  layoutClass = input.required<string>();
+  routes: Route[] = [];
+
+  constructor() {
+    this.routes = routes.filter(route =>
+      route.path === 'nuevo' || route.path === 'popular' || route.path === 'listado'
+    );
+  };
+}
