6e2fdacac91e5abf5057914b1c820b749bdc62da
Yesid Llamas <yellaber@gmail.com>
2025-04-06 00:46:01 -0500

    src/app/components/top-vote/ creado

---
+++ src/app/components/top-vote/top-vote.component.ts
@@ -0,0 +1,24 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { TmdbService } from '../../services/tmdb.service';
+import { Movie } from '../../interfaces/movie-response.interface';
+import { CarruselMoviesComponent } from '../../shared/carrusel-movies/carrusel-movies.component';
+
+@Component({
+  selector: 'top-vote',
+  imports: [ CarruselMoviesComponent ],
+  template: `
+    <carrusel-movies carruselTitle="Top 10 - Mejor valoradas" [movies]="movies()"/>
+  `
+})
+export class TopVoteComponent implements OnInit {
+  movies = signal<Movie[]>(Object.create({}));
+  private tmdbService = inject(TmdbService);
+
+  ngOnInit(): void {
+    this.getTopRatedMovies();
+  }
+
+  getTopRatedMovies() {
+    this.tmdbService.getTopRatedMovies(10).subscribe(movies => this.movies.set(movies));
+  }
+}
