9db7d83f78108fe1ad4e17d7552e2a8f15133a73
Yesid Llamas <yellaber@gmail.com>
2025-04-06 00:45:26 -0500

    src/app/components/top-popular/ creado

---
+++ src/app/components/top-popular/top-popular.component.ts
@@ -0,0 +1,24 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { TmdbService } from '../../services/tmdb.service';
+import { Movie } from '../../interfaces/movie-response.interface';
+import { CarruselMoviesComponent } from "../../shared/carrusel-movies/carrusel-movies.component";
+
+@Component({
+  selector: 'top-popular',
+  imports: [ CarruselMoviesComponent ],
+  template: `
+    <carrusel-movies carruselTitle="Top 10 - Más populares" [movies]="movies()"/>
+  `
+})
+export class TopPopularComponent implements OnInit {
+  movies = signal<Movie[]>(Object.create({}));
+  private tmdbService = inject(TmdbService);
+
+  ngOnInit(): void {
+    this.getPopularMovies();
+  }
+
+  getPopularMovies() {
+    this.tmdbService.getPopularMovies(10).subscribe(movies => this.movies.set(movies));
+  }
+}
