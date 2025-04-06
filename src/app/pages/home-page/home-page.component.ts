e49a3aa46f1c29a97e3acf6bd47c0085eb0a446e
Yesid Llamas <yellaber@gmail.com>
2025-04-06 00:41:12 -0500

    home-page.component.ts modificado

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
+import { BannerHeroPopularComponent } from '../../components/banner-hero-popular/banner-hero-popular.component';
+import { BannerHeroRatedComponent } from "../../components/banner-hero-rated/banner-hero-rated.component";
+import { TopPopularComponent } from "../../components/top-popular/top-popular.component";
+import { TopVoteComponent } from "../../components/top-vote/top-vote.component";
 
 @Component({
-  imports: [],
+  imports: [
+    BannerHeroPopularComponent,
+    BannerHeroRatedComponent,
+    TopPopularComponent,
+    TopVoteComponent
+],
   templateUrl: './home-page.component.html',
 })
 export default class HomePageComponent { }
