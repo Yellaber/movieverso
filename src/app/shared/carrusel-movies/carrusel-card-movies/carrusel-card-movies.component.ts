ddfccf55a1f5a2b66215d54b49c31f6e6e997efa
Yesid Llamas <yellaber@gmail.com>
2025-04-07 16:59:53 -0500

    carrusel-card-movies.component.ts modificado

--- src/app/shared/carrusel-movies/carrusel-card-movies/carrusel-card-movies.component.ts
+++ src/app/shared/carrusel-movies/carrusel-card-movies/carrusel-card-movies.component.ts
@@ -1,18 +1,22 @@
 import { Component, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { PopularityComponent } from "../../popularity/popularity.component";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBookmark } from '@fortawesome/free-solid-svg-icons';
+import { PopularityComponent } from '../../popularity/popularity.component';
 import { Movie } from '../../../interfaces/movie-response.interface';
-import { VoteComponent } from "../../vote/vote.component";
+import { VoteComponent } from '../../vote/vote.component';
 
 @Component({
   selector: 'carrusel-card-movies',
   imports: [
     RouterLink,
     PopularityComponent,
-    VoteComponent
+    VoteComponent,
+    FontAwesomeModule
 ],
   templateUrl: './carrusel-card-movies.component.html'
 })
 export class CarruselCardMoviesComponent {
+  faBokmark = faBookmark;
   movie = input.required<Movie>();
 }
