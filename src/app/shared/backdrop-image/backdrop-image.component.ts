b54fd29a69884bee8c25a950efb29736db822dec
Yesid Llamas <yellaber@gmail.com>
2025-04-09 12:31:17 -0500

    backdrop-image.component.ts modificado

--- src/app/shared/backdrop-image/backdrop-image.component.ts
+++ src/app/shared/backdrop-image/backdrop-image.component.ts
@@ -1,13 +1,16 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
+import { environment } from '../../environments/environment.developments';
 
 @Component({
   selector: 'backdrop-image',
   imports: [],
   template: `
-    <img class="md:object-cover md:object-center rounded-sm" [src]="'https://image.tmdb.org/t/p/w500/' + backdropPath()" [alt]="originalTitle()">
+    <img class="md:object-cover md:object-center rounded-sm" [src]="backdropImage()"
+    [alt]="originalTitle()">
   `
 })
 export class BackdropImageComponent {
   backdropPath = input.required<string>();
   originalTitle = input.required<string>();
+  backdropImage = computed(() => environment.imageUrl + this.backdropPath());
 }
