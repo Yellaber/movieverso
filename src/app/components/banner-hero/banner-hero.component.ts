e7fb06647475af52d42263009d6f6067d403e816
Yesid Llamas <yellaber@gmail.com>
2025-04-09 12:30:29 -0500

    src/app/components/banner-hero/ modificado

--- src/app/components/banner-hero/banner-hero.component.ts
+++ src/app/components/banner-hero/banner-hero.component.ts
@@ -1,12 +1,12 @@
 import { Component, computed, input } from '@angular/core';
-import { TagComponent } from '../../shared/tag/tag.component';
-import { PopularityComponent } from '../../shared/popularity/popularity.component';
-import { VoteComponent } from '../../shared/vote/vote.component';
-import { GenreListComponent } from '../../shared/genre-list/genre-list.component';
-import { BackdropImageComponent } from '../../shared/backdrop-image/backdrop-image.component';
 import { RouterLink } from '@angular/router';
 import { Movie } from '../../interfaces/movie-response.interface';
 import { Genre } from '../../interfaces/genre-movies-response.interface';
+import { TagComponent } from '../../shared/tag/tag.component';
+import { PopularityComponent } from '../../shared/popularity/popularity.component';
+import { VoteComponent } from '../../shared/vote/vote.component';
+import { BadgeListComponent } from '../../shared/badge-list/bandge-list.component';
+import { BackdropImageComponent } from '../../shared/backdrop-image/backdrop-image.component';
 
 type typeTag = 'popularity' | 'rated' | '';
 
@@ -16,14 +16,15 @@ type typeTag = 'popularity' | 'rated' | '';
     TagComponent,
     PopularityComponent,
     VoteComponent,
-    GenreListComponent,
+    BadgeListComponent,
     BackdropImageComponent,
-    RouterLink
-  ],
+    RouterLink,
+],
   templateUrl: './banner-hero.component.html'
 })
 export class BannerHeroComponent {
   heroType = input.required<typeTag>();
+  heroTitle = input.required<string>();
   movie = input.required<Movie>();
   genres = input.required<Genre[]>();
   genresPopularMovie = computed<Genre[]>(() =>
