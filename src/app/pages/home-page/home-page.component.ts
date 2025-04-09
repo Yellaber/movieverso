be45baed7183577d6329224626b4b4893efea208
Yesid Llamas <yellaber@gmail.com>
2025-04-09 12:28:43 -0500

    src/app/pages/home-page/ modificado

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { BannerHeroComponent } from '../../components/banner-hero/banner-hero.component';
 import { TopPopularComponent } from '../../components/top-popular/top-popular.component';
 import { TopVoteComponent } from '../../components/top-vote/top-vote.component';
-import { TmdbService } from '../../services/tmdb.service';
+import { TmdbService } from '../../services/tmdb-service/tmdb.service';
 import { Movie } from '../../interfaces/movie-response.interface';
 import { Genre } from '../../interfaces/genre-movies-response.interface';
 
