5281482b348c590975028ec05c2fa9cdd84a5dcc
Yesid Llamas <yellaber@gmail.com>
2025-04-10 12:22:44 -0500

    src/app/pages/detail-movie/ modificado

--- src/app/pages/detail-movie/detail-movie.component.ts
+++ src/app/pages/detail-movie/detail-movie.component.ts
@@ -1,44 +1,32 @@
-import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
 import { CurrencyPipe, JsonPipe } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faBookmark, faHeart, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 import { TmdbService } from '../../services/tmdb-service/tmdb.service';
 import { DetailMovieResponse } from '../../interfaces/detail-movie-response.interface';
-import { environment } from '../../environments/environment.developments';
-import { TagComponent } from "../../shared/tag/tag.component";
 import { BadgeListComponent } from '../../shared/badge-list/bandge-list.component';
-import { PopularityComponent } from '../../shared/popularity/popularity.component';
-import { VoteComponent } from '../../shared/vote/vote.component';
 import { Keyword } from '../../interfaces/movie-keyword-response';
+import { BannerDetailComponent } from '../../components/banner-detail/banner-detail.component';
 
 const flagCdnUrl = 'https://flagcdn.com/w80/';
 
 @Component({
+  selector: 'detail-movie',
   imports: [
     JsonPipe,
     CurrencyPipe,
-    TagComponent,
     FontAwesomeModule,
     BadgeListComponent,
-    PopularityComponent,
-    VoteComponent,
+    BannerDetailComponent
 ],
-  templateUrl: './detail-movie.component.html'
+  templateUrl: './detail-movie.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class DetailMovieComponent implements OnInit {
   private route = inject(ActivatedRoute);
   private tmdbService = inject(TmdbService);
-  faBookmark = faBookmark;
-  faHeart = faHeart;
-  faThumbsUp = faThumbsUp;
-  movie = signal<DetailMovieResponse>(Object.create({}));
+  movieDetail = signal<DetailMovieResponse>(Object.create({}));
   movieKeywords = signal<Keyword[]>([]);
-  posterImage = computed(() => environment.imageUrl + this.movie().poster_path);
-  backdropImage = computed(() => environment.imageUrl + this.movie().backdrop_path);
-  flagCountry = computed(() =>
-    flagCdnUrl + this.movie().production_countries[0].iso_3166_1.toLowerCase() + '.png'
-  );
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
@@ -52,11 +40,15 @@ export default class DetailMovieComponent implements OnInit {
 
   getMovieById(id: number) {
     return this.tmdbService.getMovieById(id)
-      .subscribe(detailMovie => this.movie.set(detailMovie));
+      .subscribe(detailMovie => this.movieDetail.set(detailMovie));
   }
 
   getMovieKeywords(id: number) {
     return this.tmdbService.getMovieKeywords(id)
       .subscribe(movieKeywords => this.movieKeywords.set(movieKeywords));
+  }
+
+  getFlagCountry(iso31661Code: string) {
+    return flagCdnUrl + iso31661Code.toLowerCase() + '.png';
   }
 }
