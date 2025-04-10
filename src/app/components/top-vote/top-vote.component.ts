bf5e394f1ea8399dca59052ff425becf34131e37
Yesid Llamas <yellaber@gmail.com>
2025-04-10 12:29:11 -0500

    src/app/components/top-vote/top-vote.component.ts modificado

--- src/app/components/top-vote/top-vote.component.ts
+++ src/app/components/top-vote/top-vote.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
 import { TmdbService } from '../../services/tmdb-service/tmdb.service';
 import { Movie } from '../../interfaces/movie-response.interface';
 import { CarruselMoviesComponent } from '../../shared/carrusel-movies/carrusel-movies.component';
@@ -8,7 +8,8 @@ import { CarruselMoviesComponent } from '../../shared/carrusel-movies/carrusel-m
   imports: [ CarruselMoviesComponent ],
   template: `
     <carrusel-movies carruselTitle="Top 10 - Mejor valoradas" [movies]="movies()"/>
-  `
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TopVoteComponent implements OnInit {
   movies = signal<Movie[]>(Object.create({}));
