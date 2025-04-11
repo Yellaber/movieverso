47e895b122a3c83b297fdb9a2ff5becd33d048fa
Yesid Llamas <yellaber@gmail.com>
2025-04-11 13:19:49 -0500

    search.component.ts modificado

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input, OnInit, output } from '@angular/core';
+import { SeoFriendlyService } from '../../services/seo-friendly/SeoFriendly.service';
 
 @Component({
   selector: 'search',
@@ -6,11 +7,16 @@ import { ChangeDetectionStrategy, Component, input, output } from '@angular/core
   templateUrl: './search.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export default class SearchComponent {
+export default class SearchComponent implements OnInit {
   keySearch = input.required<string>();
   onKeySearch = output<string>();
+  private seoFriendlyService = inject(SeoFriendlyService);
+
+  ngOnInit(): void {
+    this.seoFriendlyService.setMetaTags('Search', 'Esta es la página para las busquedas de películas');
+  }
 
   eventSearch() {
     this.onKeySearch.emit(this.keySearch().toLowerCase().trim());
-  };
+  }
 }
