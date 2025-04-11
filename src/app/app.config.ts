815dd38d45341f843a1ce0a39e776dcad70af32e
Yesid Llamas <yellaber@gmail.com>
2025-04-11 13:15:23 -0500

    app.config.ts modificado

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(withFetch()), provideClientHydration(withEventReplay()),
     provideRouter(routes),
-    provideHttpClient(withFetch())
   ]
 };
