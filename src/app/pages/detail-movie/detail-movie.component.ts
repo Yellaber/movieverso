1c90c1471c27a98d454465357ccfd2321d05da2a
Yesid Llamas <yellaber@gmail.com>
2025-04-13 14:17:47 -0500

    src/app/pages/detail-movie/ modificado

--- src/app/pages/detail-movie/detail-movie.component.ts
+++ src/app/pages/detail-movie/detail-movie.component.ts
@@ -1,13 +1,18 @@
 import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
-import { CurrencyPipe, JsonPipe } from '@angular/common';
+import { JsonPipe } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map, tap } from 'rxjs';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { TmdbService } from '../../services/tmdb-service/tmdb.service';
 import { DetailMovieResponse } from '../../interfaces/detail-movie-response.interface';
-import { BadgeListComponent } from '../../shared/badge-list/bandge-list.component';
 import { Keyword } from '../../interfaces/movie-keyword-response';
 import { BannerDetailComponent } from '../../components/banner-detail/banner-detail.component';
-import { SeoFriendlyService } from '../../services/seo-friendly/SeoFriendly.service';
+import { SideDetailComponent } from '../../components/side-detail/side-detail.component';
+import { BannerDetailSkeletonComponent } from '../../components/banner-detail-skeleton/banner-detail-skeleton.component';
+import { TmdbService } from '../../services/tmdb-service/tmdb.service';
+import { SeoFriendlyService } from '../../services/seo-friendly/seo-friendly.service';
+import { SideDetailSkeletonComponent } from '../../components/side-detail-skeleton/side-detail-skeleton.component';
+import { environment } from '../../environments/environment.developments';
 
 const flagCdnUrl = 'https://flagcdn.com/w80/';
 
@@ -15,10 +20,11 @@ const flagCdnUrl = 'https://flagcdn.com/w80/';
   selector: 'detail-movie',
   imports: [
     JsonPipe,
-    CurrencyPipe,
     FontAwesomeModule,
-    BadgeListComponent,
-    BannerDetailComponent
+    BannerDetailComponent,
+    SideDetailComponent,
+    BannerDetailSkeletonComponent,
+    SideDetailSkeletonComponent
 ],
   templateUrl: './detail-movie.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -29,25 +35,34 @@ export default class DetailMovieComponent implements OnInit {
   private seoFriendlyService = inject(SeoFriendlyService);
   movieDetail = signal<DetailMovieResponse>(Object.create({}));
   movieKeywords = signal<Keyword[]>([]);
+  idMovie = toSignal<number>(
+              this.route.paramMap
+                .pipe(
+                  map(params => params.get('id')?? ''),
+                  map(id => isNaN(+id)? 0: +id)
+                )
+              );
+  errorMovie = signal<boolean>(false);
 
   ngOnInit(): void {
-    this.seoFriendlyService.setMetaTags('Detail', 'Esta es la página que muestrala información de cada película');
-    this.route.paramMap.subscribe(params => {
-      if(params.get('id')) {
-        const idMovie = +params.get('id')!;
-        this.getMovieById(idMovie);
-        this.getMovieKeywords(idMovie);
-      }
-    });
+    this.getMovieById();
+    this.getMovieKeywords();
   }
 
-  getMovieById(id: number) {
-    return this.tmdbService.getMovieById(id)
-      .subscribe(detailMovie => this.movieDetail.set(detailMovie));
+  getMovieById() {
+    this.tmdbService.getMovieById(this.idMovie()!)
+      .pipe(
+        tap(detailMovie =>
+          this.seoFriendlyService.setMetaTags(`${ detailMovie.title }`, `Esta página muestra la información de ${ detailMovie.title }.`, `${ environment.imageUrl }${ detailMovie.backdrop_path }`)
+        ))
+      .subscribe({
+          next: detailMovie => this.movieDetail.set(detailMovie),
+          error: () => this.errorMovie.set(true)
+      });
   }
 
-  getMovieKeywords(id: number) {
-    return this.tmdbService.getMovieKeywords(id)
+  getMovieKeywords() {
+    this.tmdbService.getMovieKeywords(this.idMovie()!)
       .subscribe(movieKeywords => this.movieKeywords.set(movieKeywords));
   }
 
