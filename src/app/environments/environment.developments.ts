ef6ea7a1f5b48bdc9a1b5e167a129cda01352331
Yesid Llamas <yellaber@gmail.com>
2025-04-13 14:19:50 -0500

    src/app/environments/ modificado

--- src/app/environments/environment.developments.ts
+++ src/app/environments/environment.developments.ts
@@ -4,5 +4,5 @@ export const environment = {
   slogan: 'Tu universo de peliculas',
   tmdbApiKey: 'c6eb6487500a7727c7f8fec7a57eb1ab',
   tmdbApiUrl: 'https://api.themoviedb.org/3',
-  imageUrl: 'https://image.tmdb.org/t/p/original/'
+  imageUrl: 'https://image.tmdb.org/t/p/original'
 };
