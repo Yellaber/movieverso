346cd6ec001492d44afa5a676f18585af6656b8f
Yesid Llamas <yellaber@gmail.com>
2025-04-13 20:57:51 -0500

    src/app/app.routes.server.ts modificado

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -6,7 +6,7 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Prerender,
   },
   {
-    path: 'pelicula/:id',
+    path: 'pelicula/:id-slug',
     renderMode: RenderMode.Client
   }
 ];
