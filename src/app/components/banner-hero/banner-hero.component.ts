bf8b6ac284201f54c1c0df7df7f3efb016cd5346
Yesid Llamas <yellaber@gmail.com>
2025-04-15 19:07:22 -0500

    banner-hero.component.ts modificado

--- src/app/components/banner-hero/banner-hero.component.ts
+++ src/app/components/banner-hero/banner-hero.component.ts
@@ -14,7 +14,8 @@ type typeTag = 'popularity' | 'rated' | '';
     ShortInformationComponent
   ],
   templateUrl: './banner-hero.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: { class: 'relative flex items-center rounded-md shadow-md md:gap-10 lg:gap-20 p-5 md:p-10 mt-5' }
 })
 export class BannerHeroComponent {
   heroType = input.required<typeTag>();
