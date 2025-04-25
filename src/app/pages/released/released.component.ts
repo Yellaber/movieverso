1afa9ea663dc19c57149bc032b750b5c2c185107
Yesid Llamas <yellaber@gmail.com>
2025-04-24 22:55:12 -0500

    src/app/pages/released/ creado

---
+++ src/app/pages/released/released.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-released',
+  imports: [],
+  templateUrl: './released.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class ReleasedComponent { }
