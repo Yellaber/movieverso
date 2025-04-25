1ae41cb3d30c3335221a9a06bfa0834964abec82
Yesid Llamas <yellaber@gmail.com>
2025-04-24 22:54:36 -0500

    src/app/pages/rated/ creado

---
+++ src/app/pages/rated/rated.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'rated',
+  imports: [],
+  templateUrl: './rated.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class RatedComponent { }
