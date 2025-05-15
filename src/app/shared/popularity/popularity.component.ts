fb9735ad473b8e696e07bb5f70e275237982c640
Yesid Llamas <yellaber@gmail.com>
2025-05-14 20:59:25 -0500

    popularity.component.ts modificado

--- src/app/shared/popularity/popularity.component.ts
+++ src/app/shared/popularity/popularity.component.ts
@@ -11,8 +11,8 @@ import { faFire } from '@fortawesome/free-solid-svg-icons';
   ],
   template: `
     <div class="flex items-center gap-2">
-      <fa-icon class="yellow-color" [icon]="faFire"></fa-icon>
-      <small>{{ popularity() | number: '1.1-1' }}</small>
+      <fa-icon class="text-yellow-600" [icon]="faFire"></fa-icon>
+      <small class="text-stone-300">{{ popularity() | number: '1.1-1' }}</small>
     </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush
