b9a6aa229e8b7abb5bbd93ccfe62c6435612176a
Yesid Llamas <yellaber@gmail.com>
2025-05-16 12:22:28 -0500

    list.component.ts modificado

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,8 +1,7 @@
 import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
-import { SeoFriendlyService } from '../../services/seo-friendly/seo-friendly.service';
+import { SeoFriendlyService } from '@services/seo-friendly/seo-friendly.service';
 
 @Component({
-  selector: 'list',
   imports: [],
   templateUrl: './list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
