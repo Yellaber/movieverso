c9153d6d9f899418931cb8822c90e36c3e7325c5
Yesid Llamas <yellaber@gmail.com>
2025-05-16 12:26:02 -0500

    released.component.ts modificado

--- src/app/pages/released/released.component.ts
+++ src/app/pages/released/released.component.ts
@@ -1,7 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'app-released',
   imports: [],
   templateUrl: './released.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
