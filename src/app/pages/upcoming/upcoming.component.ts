38bb3d1f569998f8cfe4a0ed67d2a5dcf8bd5e00
Yesid Llamas <yellaber@gmail.com>
2025-05-16 12:28:30 -0500

    upcoming.component.ts modificado

--- src/app/pages/upcoming/upcoming.component.ts
+++ src/app/pages/upcoming/upcoming.component.ts
@@ -1,9 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'upcoming',
   imports: [],
-  templateUrl: './upcomming.component.html',
+  templateUrl: './upcoming.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class UpcomingComponent { }
