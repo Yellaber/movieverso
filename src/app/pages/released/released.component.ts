8e11e791f9207265acaed9ff205d92d5c85d7a2b
Yesid Llamas <yellaber@gmail.com>
2025-05-17 18:33:57 -0500

    released.component.ts modificado

--- src/app/pages/released/released.component.ts
+++ src/app/pages/released/released.component.ts
@@ -1,8 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { SeoFriendlyService } from '@services/seo-friendly.service';
 
 @Component({
   imports: [],
   templateUrl: './released.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class ReleasedComponent { }
+export default class ReleasedComponent implements OnInit {
+  private seoFriendlyService = inject(SeoFriendlyService);
+
+  ngOnInit() {
+    this.seoFriendlyService.setMetaTags('En estreno', 'Esta es la página para las películas en estreno');
+  }
+}
