34ab874866401faeae801de69e74a40b7fe21e84
Yesid Llamas <yellaber@gmail.com>
2025-05-17 18:32:17 -0500

    trending.component.ts modificado

--- src/app/pages/trending/trending.component.ts
+++ src/app/pages/trending/trending.component.ts
@@ -1,8 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { SeoFriendlyService } from '@services/seo-friendly.service';
 
 @Component({
   imports: [],
   templateUrl: './trending.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class TrendingComponent { }
+export default class TrendingComponent implements OnInit {
+  private seoFriendlyService = inject(SeoFriendlyService);
+
+  ngOnInit() {
+    this.seoFriendlyService.setMetaTags('En tendencia', 'Esta es la página para las películas en tendencia');
+  }
+}
