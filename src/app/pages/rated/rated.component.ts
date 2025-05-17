5b46513599418e428ec2070256ecb110b59094c8
Yesid Llamas <yellaber@gmail.com>
2025-05-17 18:34:30 -0500

    rated.component.ts modificado

--- src/app/pages/rated/rated.component.ts
+++ src/app/pages/rated/rated.component.ts
@@ -1,8 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { SeoFriendlyService } from '@services/seo-friendly.service';
 
 @Component({
   imports: [],
   templateUrl: './rated.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class RatedComponent { }
+export default class RatedComponent implements OnInit {
+  private seoFriendlyService = inject(SeoFriendlyService);
+
+  ngOnInit() {
+    this.seoFriendlyService.setMetaTags('Más valoradas', 'Esta es la página para las películas más valoradas');
+  }
+}
