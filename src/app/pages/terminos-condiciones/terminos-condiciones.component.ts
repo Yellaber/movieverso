8b0cd666671373466ad936b15dc35b96f6dab626
Yesid Llamas <yellaber@gmail.com>
2025-05-17 18:32:56 -0500

    terminos-condiciones.component.ts modificado

--- src/app/pages/terminos-condiciones/terminos-condiciones.component.ts
+++ src/app/pages/terminos-condiciones/terminos-condiciones.component.ts
@@ -1,8 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { SeoFriendlyService } from '@services/seo-friendly.service';
 
 @Component({
   imports: [],
   templateUrl: './terminos-condiciones.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export default class TerminosCondicionesComponent { }
+export default class TerminosCondicionesComponent implements OnInit {
+  private seoFriendlyService = inject(SeoFriendlyService);
+
+  ngOnInit() {
+    this.seoFriendlyService.setMetaTags('Términos y condiciones', 'Esta es la página para los términos y condiciones del sitio');
+  }
+}
