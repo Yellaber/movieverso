548b036487d1a8efec2f15ed6736255d7ad6219f
Yesid Llamas <yellaber@gmail.com>
2025-05-20 21:32:50 -0500

    src/app/shared/tag/ modificado

--- src/app/shared/tag/tag.component.spec.ts
+++ src/app/shared/tag/tag.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TagComponent } from './tag.component';
 
-describe('Tag component', () => {
+describe('Tag Component:', () => {
   let component: TagComponent;
   let fixture: ComponentFixture<TagComponent>;
 
@@ -16,19 +16,37 @@ describe('Tag component', () => {
     component = fixture.componentInstance;
   });
 
-  it('Should create.', () => {
+  it('Should create component.', () => {
     expect(component).toBeTruthy();
   });
 
-  it('getIcon return an Icon object.', () => {
-    fixture.componentRef.setInput('type', 'popularity');
-    const icon = component.getIcon();
-    expect(icon).toBeTruthy();
+  it('getIcon return an undefined value.', () => {
+    fixture.componentRef.setInput('type', '');
+    expect(component.getIcon()).toBeUndefined();
   });
 
-  it('getIcon return an undefined value.', () => {
-    fixture.componentRef.setInput('type', 'top_rated');
-    const icon = component.getIcon();
-    expect(icon).toBeUndefined();
+  it('getIcon return an Icon object about now playing.', () => {
+    fixture.componentRef.setInput('type', 'now_playing');
+    expect(component.getIcon()).toBeTruthy();
+  });
+
+  it('getIcon return an Icon object about popularity.', () => {
+    fixture.componentRef.setInput('type', 'popularity');
+    expect(component.getIcon()).toBeTruthy();
+  });
+
+  it('getIcon return an Icon object about rated.', () => {
+    fixture.componentRef.setInput('type', 'rated');
+    expect(component.getIcon()).toBeTruthy();
+  });
+
+  it('getIcon return an Icon object about calendar.', () => {
+    fixture.componentRef.setInput('type', 'calendar');
+    expect(component.getIcon()).toBeTruthy();
+  });
+
+  it('getIcon return an Icon object about trending.', () => {
+    fixture.componentRef.setInput('type', 'trending');
+    expect(component.getIcon()).toBeTruthy();
   });
 });
--- src/app/shared/tag/tag.component.ts
+++ src/app/shared/tag/tag.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faFire, faStar, faCalendarCheck, faArrowTrendUp, faFilm,
          IconDefinition } from '@fortawesome/free-solid-svg-icons';
@@ -8,24 +8,28 @@ interface Icon {
   faIcon: IconDefinition
 }
 
+const icons: Icon[] = [
+  { text: 'now_playing', faIcon: faFilm }, { text: 'popularity', faIcon: faFire },
+  { text: 'rated', faIcon: faStar }, { text: 'calendar', faIcon: faCalendarCheck },
+  { text: 'trending', faIcon: faArrowTrendUp }
+];
+
 @Component({
   selector: 'tag',
   imports: [ FontAwesomeModule ],
-  templateUrl: './tag.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  template: `
+    @if(getIcon()) {
+      <div class="flex items-center rounded-full bg-stone-800 text-yellow-600 gap-2 px-3 py-2">
+        <fa-icon [icon]="getIcon()!.faIcon"></fa-icon>
+        <span class="text-xs lg:text-sm">{{ text() }}</span>
+      </div>
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: { class: 'flex' }
 })
 export class TagComponent {
   text = input.required<string>();
   type = input<string>('');
-  icons: Icon[] = [
-    { text: 'now_playing', faIcon: faFilm },
-    { text: 'popularity', faIcon: faFire },
-    { text: 'rated', faIcon: faStar },
-    { text: 'calendar', faIcon: faCalendarCheck },
-    { text: 'trending', faIcon: faArrowTrendUp }
-  ];
-
-  getIcon(): Icon | undefined {
-    return this.icons.find(icon => icon.text === this.type());
-  }
+  getIcon = computed(() => icons.find(icon => icon.text === this.type()));
 }
