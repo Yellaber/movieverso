dd50eec44d649916981ab38464a43139e716fff9
Yesid Llamas <yellaber@gmail.com>
2025-05-20 21:36:54 -0500

    slugify.service.ts modificado

--- src/app/services/slugify.service.ts
+++ src/app/services/slugify.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class SlugifyService {
   getSlug(title: string): string {
-    return title.toLowerCase().normalize('NFD')
-            .replace(/[\u0300-\u036f]/g,'').replace(/[^a-z0-9]+/g,'-');
-  }
-}
+    return title.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g,'')
+            .replace(/[^a-z0-9]+/g,'-');
+  };
+};
