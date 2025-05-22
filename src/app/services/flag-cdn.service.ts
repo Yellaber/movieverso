f0894a9759355e1c46d58c79a20c6c4f621dfb9a
Yesid Llamas <yellaber@gmail.com>
2025-05-22 18:23:23 -0500

    flag-cdn.service.ts creado

---
+++ src/app/services/flag-cdn.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+type TypeSizeFlag = '20' | '40' | '80';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FlagCdnService {
+
+  getFlagCountry(iso31661Code: string, sizeFlag: TypeSizeFlag) {
+    return `https://flagcdn.com/w${sizeFlag}/${iso31661Code.toLowerCase()}.png`;
+  };
+}
