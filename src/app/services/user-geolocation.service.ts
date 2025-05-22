5afc07fb3804d724e0b09f92ef97ec6fe4218c8a
Yesid Llamas <yellaber@gmail.com>
2025-05-22 12:34:57 -0500

    user-geolocation.service.ts modificado

--- src/app/services/user-geolocation.service.ts
+++ src/app/services/user-geolocation.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { Observable } from 'rxjs';
 import { UserGeolocation } from '@interfaces/';
 
+const USER_LOCAL_LOCATION = 'userLocalLocation';
 const API_URL_IPGEOLOCATION = 'https://api.ipgeolocation.io/v2/ipgeo';
 const API_KEY_IPGEOLOCATION = '65139d689b9a48b2b125c9365c130b1f';
 
@@ -10,10 +12,31 @@ const API_KEY_IPGEOLOCATION = '65139d689b9a48b2b125c9365c130b1f';
   providedIn: 'root'
 })
 export class UserGeolocationService {
+  private platformId = inject(PLATFORM_ID);
   private httpClient = inject(HttpClient);
+  private userGeolocation: UserGeolocation | null = null;
 
-  getLocation(): Observable<UserGeolocation> {
+  constructor() {
+    isPlatformBrowser(this.platformId) && this.initUserLocation();
+  };
+
+  private getLocation(): Observable<UserGeolocation> {
     const url = API_URL_IPGEOLOCATION + '?apiKey=' + API_KEY_IPGEOLOCATION;
     return this.httpClient.get<UserGeolocation>(url);
   };
+
+  private initUserLocation() {
+    const userLocalLocation = localStorage.getItem(USER_LOCAL_LOCATION);
+    if(userLocalLocation) {
+      this.userGeolocation = JSON.parse(userLocalLocation);
+      return;
+    }
+    this.getLocation().subscribe(geolocation => {
+      localStorage.setItem(USER_LOCAL_LOCATION, JSON.stringify(geolocation))
+    });
+  };
+
+  getUserLocation() {
+    return this.userGeolocation;
+  }
 }
