2745e247fe9be4f9cbc0b9f1a621296490293a6a
Yesid Llamas <yellaber@gmail.com>
2025-05-22 17:59:24 -0500

    logo.component.ts modificado

--- src/app/components/menubar/logo/logo.component.ts
+++ src/app/components/menubar/logo/logo.component.ts
@@ -1,6 +1,5 @@
-import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { FlagCdnService } from '@services/';
 import { environment } from '@environments/environment.developments';
 
 @Component({
@@ -10,7 +9,6 @@ import { environment } from '@environments/environment.developments';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LogoComponent {
-  flagcdnService = inject(FlagCdnService);
   countryFlag = input.required<string | undefined>();
   appName = environment.appName;
   slogan = environment.slogan;
