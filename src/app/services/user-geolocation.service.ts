64d08c4b8aae934afae86ecf5c0e778634f5d0cc
Yesid Llamas <yellaber@gmail.com>
2025-05-22 02:50:24 -0500

    user-geolocation.service.ts modificado

--- src/app/services/user-geolocation.service.ts
+++ src/app/services/user-geolocation.service.ts
@@ -3,6 +3,9 @@ import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserGeolocation } from '@interfaces/';
 
+const API_URL_IPGEOLOCATION = 'https://api.ipgeolocation.io/v2/ipgeo';
+const API_KEY_IPGEOLOCATION = '65139d689b9a48b2b125c9365c130b1f';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,6 +13,7 @@ export class UserGeolocationService {
   private httpClient = inject(HttpClient);
 
   getLocation(): Observable<UserGeolocation> {
-    return this.httpClient.get<UserGeolocation>('https://ipapi.co/json/');
+    const url = API_URL_IPGEOLOCATION + '?apiKey=' + API_KEY_IPGEOLOCATION;
+    return this.httpClient.get<UserGeolocation>(url);
   };
 }
