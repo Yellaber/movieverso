bd5bb790c4b7f5626420800cdd984401641a174d
Yesid Llamas <yellaber@gmail.com>
2025-05-21 23:10:17 -0500

    carrusel-title.component.ts modificado

--- src/app/shared/carrusel-movies/carrusel-title/carrusel-title.component.ts
+++ src/app/shared/carrusel-movies/carrusel-title/carrusel-title.component.ts
@@ -6,7 +6,7 @@ import { RouterLink } from '@angular/router';
   imports: [ RouterLink ],
   template: `
     <div class="flex justify-between items-center text-yellow-600 font-semibold mb-5">
-      <div class="rounded-full bg-stone-800 px-4 py-2">
+      <div class="rounded-full bg-yellow-900/50 px-4 py-2">
         <h2 class="text-xl md:text-2xl lg:text-3xl">{{ carruselTitle() }}</h2>
       </div>
       <a class="text-sm lg:text-xl" [routerLink]="route()">Ver todas</a>
