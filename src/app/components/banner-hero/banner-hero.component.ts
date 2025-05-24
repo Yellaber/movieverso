f892fd77e0b76b6fc60c836b59acb073808dca12
Yesid Llamas <yellaber@gmail.com>
2025-05-23 20:10:27 -0500

    banner-hero.component.ts modificado

--- src/app/components/banner-hero/banner-hero.component.ts
+++ src/app/components/banner-hero/banner-hero.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
 import { BackdropImageComponent } from './backdrop-image/backdrop-image.component';
 import { ShortInformationComponent } from './short-information/short-information.component';
 import { environment } from '@environments/environment.developments';
@@ -13,15 +13,13 @@ import { Movie } from '@interfaces/movie-response.interface';
   templateUrl: './banner-hero.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
-    class: 'relative flex items-center rounded-md shadow-md md:gap-10 lg:gap-20 p-5 md:p-10 mt-5'
+    class: 'relative flex items-center rounded-md shadow-md md:gap-10 lg:gap-20 p-5 md:p-10 mt-10 mb-3 lg:mt-20 lg:mb-10'
   }
 })
 export class BannerHeroComponent {
   heroType = input.required<string>();
   heroTitle = input.required<string>();
   movie = input.required<Movie>();
-
-  getBackdropImageUrl(): string {
-    return environment.imageUrl + this.movie().backdrop_path || '';
-  }
+  getBackdropImageUrl = computed<string>(() =>
+    environment.imageUrl + this.movie().backdrop_path || '');
 }
