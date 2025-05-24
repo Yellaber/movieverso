6c45bf5ab1f02e1d8fa78633147ec32674d251fe
Yesid Llamas <yellaber@gmail.com>
2025-05-23 20:12:28 -0500

    carrusel-movies.component.ts modificado

--- src/app/shared/carrusel-movies/carrusel-movies.component.ts
+++ src/app/shared/carrusel-movies/carrusel-movies.component.ts
@@ -4,9 +4,9 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { CarruselTitleComponent } from './carrusel-title/carrusel-title.component';
 import { CarruselCardMoviesComponent } from './carrusel-card-movies/carrusel-card-movies.component';
 import { CarruselButtonComponent } from './carrusel-button/carrusel-button.component';
-import { Movie } from '@interfaces/movie-response.interface';
+import { CarouselConfig } from '@interfaces/';
 
-const CARD_MOVIE_SIZE = 216; //200px(card size movie) + gap-4(16px)
+const CARD_MOVIE_SIZE = 176; //200px(card size movie) + gap-4(16px)
 
 @Component({
   selector: 'carrusel-movies',
@@ -18,47 +18,38 @@ const CARD_MOVIE_SIZE = 216; //200px(card size movie) + gap-4(16px)
   ],
   templateUrl: './carrusel-movies.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  host: { class: 'flex flex-col gap-5 py-7 lg:pt-10 lg:pb-15' }
+  host: { class: 'flex flex-col gap-5' }
 })
 export class CarruselMoviesComponent implements AfterViewInit {
   carouselContainer = viewChild<ElementRef<HTMLDivElement>>('carouselContainer');
-  carruselTitle = input.required<string>();
-  route = input.required<string>();
-  movies = input.required<Movie[]>();
+  carouselConfig = input.required<CarouselConfig>();
   carruselContainerWidth = signal<number>(0);
   scrollStep = signal<number>(0);
   totalScrollStep = signal<number>(0);
   visibleMovies = computed<number>(() => this.carruselContainerWidth() / CARD_MOVIE_SIZE);
+  scrollVisibleMovies = computed<number>(() => Math.floor(this.visibleMovies()) * CARD_MOVIE_SIZE);
+  getPartNotVisible = computed<number>(() =>
+    (this.visibleMovies() - Math.floor(this.visibleMovies())) * CARD_MOVIE_SIZE + 16);
 
   ngAfterViewInit() {
+    const { movies } = this.carouselConfig();
     this.carruselContainerWidth.set(this.carouselContainer()!.nativeElement.offsetWidth);
-    this.totalScrollStep.set(this.movies().length * CARD_MOVIE_SIZE - this.visibleMovies() * CARD_MOVIE_SIZE);
+    this.totalScrollStep.set(movies.length * CARD_MOVIE_SIZE - this.scrollVisibleMovies());
   }
 
   next() {
-    if(this.scrollStep() + this.visibleMovies() * CARD_MOVIE_SIZE < this.totalScrollStep()) {
-      this.scrollStep.update(value => value + this.visibleMovies() * CARD_MOVIE_SIZE);
-    } else {
-      this.scrollStep.set(this.totalScrollStep() - 16);
-    }
+    (this.scrollStep() + this.scrollVisibleMovies() < this.totalScrollStep())?
+    this.scrollStep.update(value => value + this.scrollVisibleMovies()):
+    this.scrollStep.set(this.totalScrollStep() - this.getPartNotVisible());
   }
 
   previous() {
-    if(this.scrollStep() - this.visibleMovies() * CARD_MOVIE_SIZE > 0) {
-      this.scrollStep.update(value => value - this.visibleMovies() * CARD_MOVIE_SIZE);
-    } else {
-      this.scrollStep.set(0);
-    }
+    (this.scrollStep() - this.scrollVisibleMovies() > 0)?
+    this.scrollStep.update(value => value - this.scrollVisibleMovies()): this.scrollStep.set(0);
   }
 
   onClick(direction: string) {
-    if(direction === 'next') {
-      this.next();
-      return;
-    }
-    if(direction === 'previous') {
-      this.previous();
-      return;
-    }
+    (direction === 'next') && this.next();
+    (direction === 'previous') && this.previous();
   }
 }
