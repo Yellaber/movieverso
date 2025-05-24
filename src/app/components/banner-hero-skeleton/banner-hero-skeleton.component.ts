0ec3f8cf858029e331c272d0baa8f29e5724e7ad
Yesid Llamas <yellaber@gmail.com>
2025-05-23 20:09:26 -0500

    banner-hero-skeleton.component.ts modificado

--- src/app/components/banner-hero-skeleton/banner-hero-skeleton.component.ts
+++ src/app/components/banner-hero-skeleton/banner-hero-skeleton.component.ts
@@ -5,6 +5,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   imports: [],
   templateUrl: './banner-hero-skeleton.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  host: { class: 'flex items-center rounded-md shadow-md animate-pulse bg-stone-800 md:gap-10 lg:gap-20 p-5 md:p-10 mt-5' }
+  host: { class: 'flex items-center rounded-md shadow-md animate-pulse bg-stone-800 md:gap-10 lg:gap-20 p-5 md:p-10 mt-10 mb-3 mt-20 mb-10' }
 })
 export class BannerHeroSkeletonComponent { }
