5dac4d973183b7eaeff7099df0c6b325f8087f31
Yesid Llamas <yellaber@gmail.com>
2025-05-23 20:05:41 -0500

    src/app/components/banner-upcoming/ modificado

--- src/app/components/banner-upcoming/banner-upcoming.component.ts
+++ src/app/components/banner-upcoming/banner-upcoming.component.ts
@@ -1,44 +1,38 @@
-import { ChangeDetectionStrategy, Component, inject, input, OnInit, signal } from '@angular/core';
-import { CarruselTitleComponent } from '@shared/carrusel-movies/carrusel-title/carrusel-title.component';
-import { CarruselCardMoviesComponent } from '@shared/carrusel-movies/carrusel-card-movies/carrusel-card-movies.component';
+import { ChangeDetectionStrategy, Component, computed, inject, input, OnInit,
+         signal } from '@angular/core';
 import { CarruselMoviesComponent } from '@shared/carrusel-movies/carrusel-movies.component';
 import { BannerUpcomingSkeletonComponent } from '@components/banner-upcoming-skeleton/banner-upcoming-skeleton.component';
-import { environment } from '@environments/environment.developments';
-import { TmdbService, SlugifyService } from '@services/';
-import { SectionMovie, Movie } from '@interfaces/';
+import { TmdbService } from '@services/';
+import { SectionMovie, Movie, CarouselConfig } from '@interfaces/';
 
 @Component({
   selector: 'banner-upcoming',
   imports: [
-    CarruselTitleComponent,
-    CarruselCardMoviesComponent,
     CarruselMoviesComponent,
     BannerUpcomingSkeletonComponent
   ],
   templateUrl: './banner-upcoming.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  host: { class: 'relative flex flex-col gap-5 lg:pt-10 lg:pb-15' }
+  host: { class: 'relative flex flex-col gap-5 pt-3 lg:pt-15' }
 })
 export class BannerUpcomingComponent implements OnInit {
   section = input.required<SectionMovie>();
   upcommingMovies = signal<Movie[]>([]);
-  private slugifyService = inject(SlugifyService);
+  carouselConfig = computed<CarouselConfig>(() => ({
+    carouselTitle: this.section().carruselTitle,
+    movies: this.upcommingMovies(),
+    route: this.section().route,
+    bgButtons: 'from-stone-900',
+    bgCardFooter: 'bg-stone-800'
+  }));
   private tmdbService = inject(TmdbService);
 
-  ngOnInit(): void {
+  ngOnInit() {
     this.getUpcommingMovies();
   }
 
-  getBackdropImage(urlImage: string): string {
-    return urlImage? environment.imageUrl + urlImage: '';
-  }
-
-  slugify(title: string): string {
-    return title? this.slugifyService.getSlug(title): '';
-  }
-
   getUpcommingMovies() {
-    this.tmdbService.getUpcommingMovies(4)
+    this.tmdbService.getUpcommingMovies(10)
       .subscribe(upcommingMovies => this.upcommingMovies.set(upcommingMovies));
   }
 }
