92aaffa507303e0056eac64b63dc8038c9db6518
Yesid Llamas <yellaber@gmail.com>
2025-05-23 20:11:56 -0500

    carrusel-title.component.ts modificado

--- src/app/shared/carrusel-movies/carrusel-title/carrusel-title.component.ts
+++ src/app/shared/carrusel-movies/carrusel-title/carrusel-title.component.ts
@@ -5,14 +5,17 @@ import { RouterLink } from '@angular/router';
   selector: 'carrusel-title',
   imports: [ RouterLink ],
   template: `
-    <div class="flex justify-between items-center text-yellow-600 font-semibold mb-5">
+    @if(carruselTitle()) {
       <div class="rounded-full bg-yellow-900/50 px-4 py-2">
         <h2 class="text-xl md:text-2xl lg:text-3xl">{{ carruselTitle() }}</h2>
       </div>
+    }
+    @if(route()) {
       <a class="text-sm lg:text-xl" [routerLink]="route()">Ver todas</a>
-    </div>
+    }
   `,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: { class: 'flex justify-between items-center text-yellow-600 font-semibold mb-5'}
 })
 export class CarruselTitleComponent {
   carruselTitle = input.required<string>();
