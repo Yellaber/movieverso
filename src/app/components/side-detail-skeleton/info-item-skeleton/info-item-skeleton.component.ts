4e3b628a29a3b2511c623740001b8dfb9b1a5b7d
Yesid Llamas <yellaber@gmail.com>
2025-05-27 17:33:33 -0500

    src/app/components/side-detail-skeleton/ componente modificado

--- src/app/components/side-detail-skeleton/info-item-skeleton/info-item-skeleton.component.ts
+++ src/app/components/side-detail-skeleton/info-item-skeleton/info-item-skeleton.component.ts
@@ -4,12 +4,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'info-item-skeleton',
   imports: [],
   template: `
-    @for(item of [1, 2, 3]; track $index) {
-      <div class="flex flex-wrap items-center gap-3">
-        <div class="w-24 h-6 bg-stone-700 rounded"></div>
-        <div class="w-20 h-6 bg-stone-600 rounded"></div>
-      </div>
-    }
+    <div class="flex flex-wrap items-center gap-3">
+      <div class="w-32 h-6 bg-stone-700 rounded"></div>
+      <div class="w-20 h-6 bg-stone-600 rounded"></div>
+    </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
--- src/app/components/side-detail-skeleton/production-countries-skeleton/production-countries-skeleton.component.ts
+++ src/app/components/side-detail-skeleton/production-countries-skeleton/production-countries-skeleton.component.ts
@@ -4,7 +4,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'production-countries-skeleton',
   imports: [],
   template: `
-    <div class="w-28 h-6 bg-stone-700 rounded"></div>
+    <div class="w-32 h-6 bg-stone-700 rounded"></div>
     <div class="flex items-center gap-3">
       <div class="w-10 h-8 bg-stone-700 rounded"></div>
       <div class="w-24 h-6 bg-stone-600 rounded"></div>
