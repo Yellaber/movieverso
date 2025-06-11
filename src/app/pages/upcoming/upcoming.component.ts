46bfa9d8ca90e05a73c402c4a1d5c74ef61bdbb1
Yesid <yellaber@gmail.com>
2025-06-11 17:49:07 -0500

    src/app/pages/upcoming/ componente modificado

--- src/app/pages/upcoming/upcoming.component.ts
+++ src/app/pages/upcoming/upcoming.component.ts
@@ -1,54 +1,86 @@
-import { ChangeDetectionStrategy, Component, effect, inject, PLATFORM_ID,
-         signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, HostListener, inject, linkedSignal, OnInit,
+         PLATFORM_ID, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
 import { isPlatformBrowser } from '@angular/common';
-import { Genre, MovieResponse } from '@interfaces/';
-import { SeoFriendlyService, TmdbService } from '@services/';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFilter } from '@fortawesome/free-solid-svg-icons';
 import { CarruselCardMoviesComponent } from '@shared/carrusel-movies/carrusel-card-movies/carrusel-card-movies.component';
 import { CardMovieSkeletonComponent } from '@components/carrusel-movies-skeleton/card-movie-skeleton/card-movie-skeleton.component';
-import { PaginationComponent } from '@shared/pagination/pagination.component';
+import { FilterButtonComponent } from '@shared/filter-button/filter-button.component';
+import { FilterModalUpcomingMoviesComponent } from '@components/filter-modal-upcoming-movies/filter-modal-upcoming-movies.component';
+import { NotificationComponent } from '@shared/notification/notification.component';
+import { FilterService, SeoFriendlyService, TmdbService } from '@services/';
 
 @Component({
   imports: [
+    FontAwesomeModule,
     CarruselCardMoviesComponent,
     CardMovieSkeletonComponent,
-    PaginationComponent
+    FilterButtonComponent,
+    FilterModalUpcomingMoviesComponent,
+    NotificationComponent
   ],
   templateUrl: './upcoming.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  host: { class: 'flex flex-col gap-10 pt-3 lg:pt-15' }
+  host: { class: 'flex flex-col gap-10 pt-12 lg:pt-15' }
 })
-export default class UpcomingComponent {
-  skeletonCards = Array(20);
-  genresMovies = signal<Genre[]>([]);
-  upcomingMovies = signal<MovieResponse | null>(null);
-  page = signal<number>(1);
+export default class UpcomingComponent implements OnInit {
   private platformId = inject(PLATFORM_ID);
   private seoFriendlyService = inject(SeoFriendlyService);
+  private filterService = inject(FilterService);
   private tmdbService = inject(TmdbService);
-
-  constructor() {
-    effect(() => {
-      this.getUpcomingMoviesFiltered('', this.page());
-    });
+  faFilter = faFilter;
+  cardMoviesSkeleton = Array(20);
+  showModal = signal<boolean>(false);
+  page = signal<number>(1);
+  genresIdSelected = linkedSignal(() =>
+    this.filterService.getGenresId().length > 0? this.filterService.getGenresId().toString(): ''
+  );
+  upcomingMoviesFiltered = rxResource({
+    request: this.genresIdSelected,
+    loader: () => this.tmdbService.getUpcomingMoviesFiltered(this.genresIdSelected())
+  });
+  upcomingMovies = linkedSignal(() =>
+    this.upcomingMoviesFiltered.hasValue()? this.upcomingMoviesFiltered.value(): null
+  );
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.loadUpcomingMovies();
   };
+
+  pageInitializer = effect(() => {
+    const genres = this.genresIdSelected();
+    if(genres) {
+      if(isPlatformBrowser(this.platformId)) {
+        window.scrollTo({ top: 0 });
+      }
+      this.page.set(1);
+    }
+  });
 
   ngOnInit() {
     this.seoFriendlyService.setMetaTags('Próximamente', 'Esta es la página para las películas que estarán próximamente en cine.');
-    this.getGenreMovieList();
   };
 
-  getGenreMovieList() {
-    this.tmdbService.getGenreMovieList().subscribe(genres => this.genresMovies.set(genres));
+  loadUpcomingMovies() {
+    if(isPlatformBrowser(this.platformId)) {
+      const scrollTop = document.documentElement.scrollTop;
+      const clientHeight = document.documentElement.clientHeight;
+      const scrollHeight = document.documentElement.scrollHeight;
+      const isAtBottom = scrollTop + clientHeight + 300 >= scrollHeight;
+      if(isAtBottom) {
+        this.page.update(value => value + 1);
+        this.tmdbService.getUpcomingMoviesFiltered(this.genresIdSelected(), this.page())
+          .subscribe(movies => this.upcomingMovies.set([ ...this.upcomingMovies()!, ...movies ]));
+      }
+    }
   };
 
-  getUpcomingMoviesFiltered(genres?: string, page?: number) {
-    this.upcomingMovies.set(null);
-    this.tmdbService.getUpcomingMoviesFiltered(genres, page)
-      .subscribe(movieResponse => { this.upcomingMovies.set(movieResponse) });
+  onShowModal() {
+    this.showModal.set(true);
   };
 
-  onClick(newPage: number) {
-    if(isPlatformBrowser(this.platformId)) { window.scrollTo({ top: 0, behavior: 'smooth' }); }
-    this.page.set(newPage);
+  onCloseModal(event: boolean) {
+    this.showModal.set(event);
   };
 }
