5d2560b7b71c99acb37a485391514096933092f4
Yesid <yellaber@gmail.com>
2025-06-11 17:45:18 -0500

    src/app/shared/filter-button/ componente creado

---
+++ src/app/shared/filter-button/filter-button.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component, output } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSliders } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'filter-button',
+  imports: [ FontAwesomeModule ],
+  templateUrl: './filter-button.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: { class: 'relative flex flex-col items-center' }
+})
+export class FilterButtonComponent {
+  faSliders = faSliders;
+  emmitClick = output<boolean>();
+
+  onClick() { this.emmitClick.emit(true); };
+}
