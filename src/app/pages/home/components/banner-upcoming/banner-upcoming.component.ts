796a3bf6c21f2e2f742a61c32a4fcc3c76be797f
Yesid <yellaber@gmail.com>
2025-06-15 22:43:45 -0500

    src/app/pages/home/components/ directorio creado

---
+++ src/app/pages/home/components/banner-upcoming/banner-upcoming.component.ts
@@ -0,0 +1,35 @@
+import { ChangeDetectionStrategy, Component, computed, inject, input, OnInit, signal } from '@angular/core';
+import { CarruselMoviesComponent } from '@shared/carrusel-movies/carrusel-movies.component';
+import { CarruselMoviesSkeletonComponent } from '@components/carrusel-movies-skeleton/carrusel-movies-skeleton.component';
+import { TmdbService } from '@services/';
+import { SectionMovie, Movie, CarouselConfig } from '@interfaces/';
+
+@Component({
+  selector: 'banner-upcoming',
+  imports: [
+    CarruselMoviesComponent,
+    CarruselMoviesSkeletonComponent
+  ],
+  templateUrl: './banner-upcoming.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: { class: 'relative flex flex-col gap-5 pt-3 lg:pt-15' }
+})
+export class BannerUpcomingComponent implements OnInit {
+  private tmdbService = inject(TmdbService);
+  section = input.required<SectionMovie>();
+  upcommingMovies = signal<Movie[]>([]);
+  carouselConfig = computed<CarouselConfig>(() => ({
+    carouselTitle: this.section().carruselTitle,
+    movies: this.upcommingMovies(),
+    route: this.section().route,
+    bgButtons: 'from-stone-900',
+    bgCardFooter: 'bg-stone-800'
+  }));
+
+  ngOnInit() { this.getUpcommingMovies(); }
+
+  getUpcommingMovies() {
+    this.tmdbService.getUpcommingMovies(10)
+      .subscribe(upcommingMovies => this.upcommingMovies.set(upcommingMovies));
+  }
+}
---
+++ src/app/pages/home/components/section-movie/section-movie.component.ts
@@ -0,0 +1,63 @@
+import { ChangeDetectionStrategy, Component, computed, inject, input, OnInit, signal } from '@angular/core';
+import { BannerHeroComponent } from '@components/banner-hero/banner-hero.component';
+import { CarruselMoviesComponent } from '@shared/carrusel-movies/carrusel-movies.component';
+import { BannerHeroSkeletonComponent } from '@components/banner-hero-skeleton/banner-hero-skeleton.component';
+import { CarruselMoviesSkeletonComponent } from '@components/carrusel-movies-skeleton/carrusel-movies-skeleton.component';
+import { TmdbService } from '@services/tmdb.service';
+import { SectionMovie, Movie, CarouselConfig } from '@interfaces/';
+
+@Component({
+  selector: 'section-movie',
+  imports: [
+    BannerHeroComponent,
+    CarruselMoviesComponent,
+    BannerHeroSkeletonComponent,
+    CarruselMoviesSkeletonComponent
+  ],
+  template: `
+    @if(movies().length > 0) {
+      <banner-hero [heroType]="section().heroType" [heroTitle]="section().heroTitle"
+                   [movie]="movies()[0]"/>
+      <carrusel-movies [carouselConfig]="carouselConfig()"/>
+    } @else {
+      <banner-hero-skeleton/>
+      <carrusel-movies-skeleton/>
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SectionMovieComponent implements OnInit {
+  private tmdbService = inject(TmdbService);
+  section = input.required<SectionMovie>();
+  movies = signal<Movie[]>([]);
+  carouselConfig = computed<CarouselConfig>(() => ({
+    carouselTitle: this.section().carruselTitle,
+    movies: this.movies(),
+    route: this.section().route,
+    bgButtons: 'from-stone-900',
+    bgCardFooter: 'bg-stone-800'
+  }));
+
+  ngOnInit() { this.getDataSectionMovies(); }
+
+  getDataSectionMovies() {
+    switch(this.section().heroType) {
+      case 'now_playing':
+        this.tmdbService.getNowPlayingMovies()
+          .subscribe(movies => this.movies.set(movies));
+        break;
+      case 'popularity':
+        this.tmdbService.getMovies('/movie/popular')
+          .subscribe(movies => this.movies.set(movies));
+        break;
+      case 'rated':
+        this.tmdbService.getMovies('/movie/top_rated')
+          .subscribe(movies => this.movies.set(movies));
+        break;
+      case 'trending':
+        this.tmdbService.getMovies('/trending/movie/day')
+          .subscribe(movies => this.movies.set(movies));
+        break;
+    };
+  }
+}
