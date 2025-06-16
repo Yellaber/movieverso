d64520052c9d314e5f48bb31c86bc77bff01e4a4
Yesid <yellaber@gmail.com>
2025-06-16 13:28:29 -0500

    src/app/pages/detail-movie/components/banner-detail/ componente agregado

---
+++ src/app/pages/detail-movie/components/banner-detail/card-detail/card-detail.component.ts
@@ -0,0 +1,21 @@
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBookmark, faHeart, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { environment } from '@environments/environment.developments';
+import { DetailMovieResponse } from '@interfaces/detail-movie-response.interface';
+
+@Component({
+  selector: 'card-detail',
+  imports: [ FontAwesomeModule ],
+  templateUrl: './card-detail.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: { class: 'hidden md:flex flex-col rounded-md z-10' }
+})
+export class CardDetailComponent {
+  faBookmark = faBookmark;
+  faHeart = faHeart;
+  faThumbsUp = faThumbsUp;
+  movieDetail = input.required<DetailMovieResponse>();
+  posterImage = computed(() =>
+    this.movieDetail().poster_path? environment.imageUrl + this.movieDetail().poster_path: '/images/no-poster.jpg');
+}
