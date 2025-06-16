919f1ae269e9e5b1024cc80276fafef7bec95da5
Yesid <yellaber@gmail.com>
2025-06-15 22:51:42 -0500

    src/app/pages/upcoming/ componente modificado

--- src/app/pages/upcoming/upcoming.component.ts
+++ src/app/pages/upcoming/upcoming.component.ts
@@ -1,90 +1,32 @@
-import { ChangeDetectionStrategy, Component, effect, HostListener, inject, linkedSignal, OnInit, PLATFORM_ID, signal } from '@angular/core';
-import { rxResource } from '@angular/core/rxjs-interop';
-import { isPlatformBrowser } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, inject, OnInit, signal } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faFilter } from '@fortawesome/free-solid-svg-icons';
-import { CarruselCardMoviesComponent } from '@shared/carrusel-movies/carrusel-card-movies/carrusel-card-movies.component';
-import { CardMovieSkeletonComponent } from '@components/carrusel-movies-skeleton/card-movie-skeleton/card-movie-skeleton.component';
 import { FilterButtonComponent } from '@shared/filter-button/filter-button.component';
-import { FilterModalUpcomingMoviesComponent } from '@components/filter-modal-upcoming-movies/filter-modal-upcoming-movies.component';
-import { NotificationComponent } from '@shared/notification/notification.component';
-import { FilterService, SeoFriendlyService, TmdbService } from '@services/';
+import { FilterModalUpcomingMoviesComponent } from './components/filter-modal-upcoming-movies/filter-modal-upcoming-movies.component';
+import { LoadUpcomingMoviesComponent } from './components/load-upcoming-movies/load-upcoming-movies.component';
+import { FilterService, SeoFriendlyService } from '@services/';
 import { Genre } from '@interfaces/';
 
 @Component({
   imports: [
     FontAwesomeModule,
-    CarruselCardMoviesComponent,
-    CardMovieSkeletonComponent,
     FilterButtonComponent,
     FilterModalUpcomingMoviesComponent,
-    NotificationComponent
+    LoadUpcomingMoviesComponent
   ],
   templateUrl: './upcoming.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: { class: 'flex flex-col gap-10 pt-12 lg:pt-15' }
 })
 export default class UpcomingComponent implements OnInit {
-  private platformId = inject(PLATFORM_ID);
   private seoFriendlyService = inject(SeoFriendlyService);
   private filterService = inject(FilterService);
-  private tmdbService = inject(TmdbService);
   faFilter = faFilter;
-  cardMoviesSkeleton = Array(20);
   showModal = signal<boolean>(false);
-  page = signal<number>(1);
-  genresSelected = linkedSignal(() => this.filterService.getGenresUpcomingMoviesFiltered());
-  upcomingMoviesFiltered = rxResource({
-    request: () => ({genres: this.genresSelected(), page: this.page()}),
-    loader: ({request}) => {
-      const genresIdSelected = this.getGenresId(request.genres);
-      const page = request.page;
-      return this.tmdbService.getUpcomingMoviesFiltered(genresIdSelected, page)
-    }
-  });
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    this.loadNextPage();
-  };
-
-  pageInitializer = effect(() => {
-    if(isPlatformBrowser(this.platformId)) {
-      window.scrollTo({top: 0});
-    }
-    const genres = this.genresSelected();
-    if(genres) {
-      this.page.set(1);
-    }
-  });
+  genresSelected = computed<Genre[]>(() => this.filterService.getGenresUpcomingMoviesFiltered());
 
   ngOnInit() {
     this.seoFriendlyService.setMetaTags('Próximamente', 'Esta es la página para las películas que estarán próximamente en cine.');
-  };
-
-  loadNextPage() {
-    if(this.isAtBottom() && this.upcomingMoviesFiltered.hasValue()) {
-      const moviesPagination = this.upcomingMoviesFiltered.value();
-      const totalPages = moviesPagination[this.page() - 1].total_pages;
-      if(this.page() === totalPages) {
-        return;
-      }
-      this.page.update(value => value + 1);
-    }
-  };
-
-  isAtBottom(): boolean {
-    if(isPlatformBrowser(this.platformId)) {
-      const scrollTop = document.documentElement.scrollTop;
-      const clientHeight = document.documentElement.clientHeight;
-      const scrollHeight = document.documentElement.scrollHeight;
-      return scrollTop + clientHeight + 300 >= scrollHeight;
-    }
-    return false;
-  };
-
-  getGenresId(genresSelected: Genre[]): string {
-    const genresId = genresSelected.map(genre => genre.id);
-    return genresId.toString();
   };
 
   onShowModal() {
