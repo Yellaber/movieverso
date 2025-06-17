2e9cc3911b8d3be5a1e632e3acf74cd84cf33c62
Yesid <yellaber@gmail.com>
2025-06-17 16:40:58 -0500

    src/app/pages/detail-movie/components/youtube-video/youtube-video.component.ts modificado

--- src/app/pages/detail-movie/components/youtube-video/youtube-video.component.ts
+++ src/app/pages/detail-movie/components/youtube-video/youtube-video.component.ts
@@ -1,18 +1,32 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { YouTubePlayer } from '@angular/youtube-player';
 import { CarruselTitleComponent } from '@shared/carrusel-movies/carrusel-title/carrusel-title.component';
+import { NotificationComponent } from '@shared/notification/notification.component';
+import { Trailer } from '@interfaces/';
 
 @Component({
   selector: 'youtube-video',
-  imports: [ YouTubePlayer, CarruselTitleComponent ],
+  imports: [
+    CarruselTitleComponent,
+    YouTubePlayer,
+    NotificationComponent
+  ],
   template: `
     <carrusel-title carruselTitle="Trailer" route=""/>
-    <div class="flex w-full justify-center">
-      <youtube-player [videoId]="idVideo()" placeholderImageQuality="high"/>
-    </div>
+    @if(trailers() && trailers().length > 0) {
+      <div class="flex w-full justify-center">
+        <youtube-player [videoId]="getMovieKey()" placeholderImageQuality="high"/>
+      </div>
+    } @else {
+      <notification notificationTitle="Spoiler invisible." message="Nada por aquí, nada por allá… ni un solo avance que mostrar. ¡La película se está haciendo la difícil!"/>
+    }
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class YoutubeVideoComponent {
-  idVideo = input.required<string>();
+  trailers = input.required<Trailer[]>();
+
+  getMovieKey(): string {
+    return this.trailers()[0].key;
+  };
 }
