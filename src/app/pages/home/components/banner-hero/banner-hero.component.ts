1023a7050eb03ca45131425e7e159c664825c621
Yesid <yellaber@gmail.com>
2025-06-17 18:37:36 -0500

    src/app/pages/home/components/banner-hero/banner-hero.component.ts modificado

--- src/app/pages/home/components/banner-hero/banner-hero.component.ts
+++ src/app/pages/home/components/banner-hero/banner-hero.component.ts
@@ -13,7 +13,7 @@ import { Movie } from '@interfaces/';
   templateUrl: './banner-hero.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
-    class: 'relative flex items-center rounded-md shadow-md md:gap-10 lg:gap-20 p-5 md:p-10 mt-10 mb-3 lg:mt-20 lg:mb-10'
+    class: 'relative flex items-center rounded-md shadow-md md:gap-10 lg:gap-20 p-5 md:p-10 mt-10 lg:mt-20 mb-10'
   }
 })
 export class BannerHeroComponent {
