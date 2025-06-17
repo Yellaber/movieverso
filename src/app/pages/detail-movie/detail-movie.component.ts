7815ed52daeca122fa763e5ff7b85a5a0746ec49
Yesid <yellaber@gmail.com>
2025-06-17 16:39:23 -0500

    src/app/pages/detail-movie/detail-movie.component.ts componente modificado

--- src/app/pages/detail-movie/detail-movie.component.ts
+++ src/app/pages/detail-movie/detail-movie.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { map, tap } from 'rxjs';
+import { map, of, tap } from 'rxjs';
+import { rxResource } from '@angular/core/rxjs-interop';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BannerDetailComponent } from './components/banner-detail/banner-detail.component';
 import { SideDetailComponent } from './components/side-detail/side-detail.component';
@@ -12,7 +13,6 @@ import { NotificationComponent } from '@shared/notification/notification.compone
 import { YoutubeVideoComponent } from './components/youtube-video/youtube-video.component';
 import { environment } from '@environments/environment.developments';
 import { TmdbService, SeoFriendlyService } from '@services/';
-import { DetailMovieResponse, Keyword, Movie, Trailer } from '@interfaces/';
 
 @Component({
   imports: [
@@ -33,80 +33,53 @@ export default class DetailMovieComponent implements OnInit {
   private route = inject(ActivatedRoute);
   private tmdbService = inject(TmdbService);
   private seoFriendlyService = inject(SeoFriendlyService);
-  movieDetail = signal<DetailMovieResponse | null>(null);
-  movieKeywords = signal<Keyword[]>([]);
-  movieTrailer = signal<Trailer[]>([]);
-  moviesRecomended = signal<Movie[]>([]);
-  moviesSimilars = signal<Movie[]>([]);
-  movieCollection = signal<Movie[]>([]);
   idMovie = signal<number | null>(null);
-  errorMovie = signal<boolean>(false);
-
-  ngOnInit() {
-    this.route.paramMap.subscribe(params => {
-      const idSlug = params.get('id-slug') || '';
-      this.idMovie.set(+idSlug.split('-')[0]);
-      window.scrollTo({ top: 0, behavior: 'smooth' });
-      const id = this.idMovie();
-      if(!id) {
-        this.errorMovie.set(true);
-        return;
-      }
-      this.movieDetail.set(null);
-      this.getMovieById(id);
-      this.getMovieKeywords(id);
-      this.getMovieTrailer(id);
-      this.getMoviesRecommendations(id);
-      this.getMoviesSimilars(id);
-    });
-  };
-
-  getMovieById(id: number) {
-    this.tmdbService.getMovieById(id)
+  movieSelected = rxResource({
+    request: this.idMovie,
+    loader: () => this.tmdbService.getMovieById(this.idMovie()!)
       .pipe(
         tap(detailMovie => {
           const movieYear = detailMovie.release_date.toString().split('-')[0];
           this.seoFriendlyService.setMetaTags(`${detailMovie.title} (${movieYear})`, `Detalles, sinopsis, reparto y más sobre la película ${detailMovie.title}.`,
           `${environment.imageUrl}${detailMovie.backdrop_path}`)
         }))
-      .subscribe({
-          next: detailMovie => {
-                  this.movieDetail.set(detailMovie);
-                  this.movieCollection.set([]);
-                  if(detailMovie.belongs_to_collection) {
-                    const {id} = detailMovie.belongs_to_collection;
-                    this.getMovieCollection(id);
-                  }
-                },
-          error: () => this.errorMovie.set(true)
-      });
-  };
+  });
+  movieKeywords = rxResource({
+    request: this.idMovie,
+    loader: () => this.idMovie()? this.tmdbService.getMovieKeywords(this.idMovie()!): of([])
+  });
+  movieTrailer = rxResource({
+    request: this.idMovie,
+    loader: () => this.idMovie()? this.tmdbService.getMovieTrailers(this.idMovie()!): of([])
+  });
+  moviesRecomended = rxResource({
+    request: this.idMovie,
+    loader: () =>
+      this.idMovie()? this.tmdbService.getMovieRecommendations(this.idMovie()!)
+        .pipe(map(({results}) => results)): of([])
+  });
+  moviesSimilars = rxResource({
+    request: this.idMovie,
+    loader: () =>
+      this.idMovie()? this.tmdbService.getMovieSimilar(this.idMovie()!)
+        .pipe(map(({results}) => results)): of([])
+  });
+  idCollection = computed<number | null>(() =>
+    (this.movieSelected.hasValue() && this.movieSelected.value().belongs_to_collection)?
+      this.movieSelected.value().belongs_to_collection.id: null
+  );
+  movieCollection = rxResource({
+    request: this.idCollection,
+    loader: () =>
+      this.idCollection()? this.tmdbService.getMovieCollectionById(this.idCollection()!)
+        .pipe(map(({parts}) => parts)): of([])
+  });
 
-  getMovieKeywords(id: number) {
-    this.tmdbService.getMovieKeywords(id)
-      .subscribe(movieKeywords => this.movieKeywords.set(movieKeywords));
-  };
-
-  getMovieTrailer(id: number) {
-    this.tmdbService.getMovieTrailers(id)
-      .subscribe(trailer => this.movieTrailer.set(trailer));
-  }
-
-  getMoviesRecommendations(id: number) {
-    this.tmdbService.getMovieRecommendations(id, 1)
-      .pipe(map(({results}) => results))
-      .subscribe(results => this.moviesRecomended.set(results));
-  }
-
-  getMoviesSimilars(id: number) {
-    this.tmdbService.getMovieSimilar(id, 1)
-      .pipe(map(({results}) => results))
-      .subscribe(results => this.moviesSimilars.set(results));
-  }
-
-  getMovieCollection(idCollection: number) {
-    this.tmdbService.getMovieCollectionById(idCollection)
-      .pipe(map(({parts}) => parts))
-      .subscribe(parts => this.movieCollection.set(parts));
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const idSlug = params.get('id-slug') || '';
+      this.idMovie.set(+idSlug.split('-')[0]);
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
   };
 };
