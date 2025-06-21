8d5aa618aeab7b04143ab78ba8656b02568f9b0a
Yesid <yellaber@gmail.com>
2025-06-21 17:33:41 -0500

    src/app/services/flag-cdn.service.ts modificado

--- src/app/services/flag-cdn.service.ts
+++ src/app/services/flag-cdn.service.ts
@@ -6,7 +6,6 @@ type TypeSizeFlag = '20' | '40' | '80';
   providedIn: 'root'
 })
 export class FlagCdnService {
-
   getFlagCountry(iso31661Code: string, sizeFlag: TypeSizeFlag) {
     return `https://flagcdn.com/w${sizeFlag}/${iso31661Code.toLowerCase()}.png`;
   };
