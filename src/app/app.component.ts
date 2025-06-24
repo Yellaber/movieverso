b65cfa7b71d897ed5fc9f2679da4c9868d334616
Yesid <yellaber@gmail.com>
2025-06-24 12:35:05 -0500

    src/app/app.component.ts modificado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { MenubarComponent } from '@components/menubar/menubar.component';
 import { FooterSiteComponent } from '@components/footer-site/footer-site.component';
 import { RouterOutlet } from '@angular/router';
-import { RoutesService } from '@services/';
+import { RoutesService, UserGeolocationService } from '@services/';
 import { routes } from './app.routes';
+import { rxResource } from '@angular/core/rxjs-interop';
+
+const API_URL_IPGEOLOCATION = 'https://api.ipgeolocation.io/v2/ipgeo';
 
 @Component({
   selector: 'app-root',
@@ -16,7 +19,13 @@ import { routes } from './app.routes';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
+  private userGeolocationService = inject(UserGeolocationService);
   private routesService = inject(RoutesService);
+  apiUrlGeolocation = signal<string>(API_URL_IPGEOLOCATION);
+  geoLocation = rxResource({
+    request: this.apiUrlGeolocation,
+    loader: () => this.userGeolocationService.loadUserLocation(this.apiUrlGeolocation())
+  });
   title = 'MovieVerso';
 
   constructor() {
