a783c49196b6416ba07fb49378f8f58e7673bfb4
Yesid <yellaber@gmail.com>
2025-06-27 15:53:10 -0500

    src/app/pages/upcoming/ componente modificado

--- src/app/pages/upcoming/upcoming.component.ts
+++ src/app/pages/upcoming/upcoming.component.ts
@@ -1,32 +1,30 @@
 import { ChangeDetectionStrategy, Component, computed, inject, OnInit, signal } from '@angular/core';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faFilter } from '@fortawesome/free-solid-svg-icons';
 import { FilterButtonComponent } from '@shared/filter-button/filter-button.component';
-import { FilterModalUpcomingMoviesComponent } from './components/filter-modal-upcoming-movies/filter-modal-upcoming-movies.component';
-import { LoadUpcomingMoviesComponent } from './components/load-upcoming-movies/load-upcoming-movies.component';
+import { FilterModalMoviesComponent } from '@shared/filter-modal-movies/filter-modal-movies.component';
+import { LoadMoviesFilteredComponent } from '@shared/load-movies-filtered/load-movies-filtered.component';
 import { FilterService, SeoFriendlyService } from '@services/';
-import { Genre } from '@interfaces/';
+import { QueryParams } from '@interfaces/';
 
 @Component({
   imports: [
-    FontAwesomeModule,
     FilterButtonComponent,
-    FilterModalUpcomingMoviesComponent,
-    LoadUpcomingMoviesComponent
+    FilterModalMoviesComponent,
+    LoadMoviesFilteredComponent
   ],
   templateUrl: './upcoming.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: { class: 'flex flex-col gap-10 pt-12 lg:pt-15' }
 })
 export default class UpcomingComponent implements OnInit {
+  showModal = signal<boolean>(false);
+  queryParams = signal<QueryParams>({});
+  genresSelected = computed<QueryParams>(() => this.queryParams());
   private seoFriendlyService = inject(SeoFriendlyService);
   private filterService = inject(FilterService);
-  faFilter = faFilter;
-  showModal = signal<boolean>(false);
-  genresSelected = computed<Genre[]>(() => this.filterService.getGenresUpcomingMoviesFiltered());
 
   ngOnInit() {
     this.seoFriendlyService.setMetaTags('Próximamente', 'Esta es la página para las películas que estarán próximamente en cine.');
+    this.queryParams.set(this.filterService.getQueryParams('upcoming'));
   };
 
   onShowModal() {
@@ -35,5 +33,9 @@ export default class UpcomingComponent implements OnInit {
 
   onCloseModal(event: boolean) {
     this.showModal.set(event);
+  };
+
+  onQueryParams(event: QueryParams) {
+    this.queryParams.set(event);
   };
 }
