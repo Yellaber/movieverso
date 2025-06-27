9f3afeb62190aa5841d3a7ba114543450847a166
Yesid <yellaber@gmail.com>
2025-06-27 15:51:25 -0500

    src/app/services/filter.service.ts modificado

--- src/app/services/filter.service.ts
+++ src/app/services/filter.service.ts
@@ -1,17 +1,57 @@
-import { Injectable, signal } from '@angular/core';
-import { Genre } from '@interfaces/';
+import { Injectable } from '@angular/core';
+import { QueryParams } from '@interfaces/';
+
+const initialQueryParams: QueryParams = {
+  withGenres: [],
+  withOriginalLanguage: '',
+  primaryReleaseYear: new Date().getFullYear(),
+  voteAverageGte: 0,
+  withOriginCountry: '',
+  primaryReleaseDateGte: '',
+  primaryReleaseDateLte: '',
+  withCompanies: '',
+  releaseDateGte: '',
+  sortBy: 'primary_release_date.asc',
+  voteCountGte: 0
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilterService {
-  private genresUpcomingMoviesFiltered = signal<Genre[]>([]);
+  private today: string = '';
+  private cacheQueryParams = new Map<string, QueryParams>();
 
-  setGenresUpcomingMoviesFiltered(genresSelected: Genre[]) {
-    this.genresUpcomingMoviesFiltered.set(genresSelected)
+  constructor() {
+    this.today = new Date().toISOString().split('T')[0];
+  }
+
+  setQueryParams(key: string, queryParams: QueryParams) {
+    this.cacheQueryParams.set(key, queryParams);
   };
 
-  getGenresUpcomingMoviesFiltered(): Genre[] {
-    return this.genresUpcomingMoviesFiltered();
+  getQueryParams(key: string): QueryParams {
+    if(this.cacheQueryParams.has(key)) {
+      return this.cacheQueryParams.get(key)!;
+    }
+    const queryParams = this.getInitialQueryParams(key);
+    this.setQueryParams(key, queryParams);
+    return queryParams;
+  };
+
+  getInitialQueryParams(key: string): QueryParams {
+    switch(key) {
+      case 'released':
+        return {
+          ...initialQueryParams,
+          primaryReleaseDateLte: this.today,
+          sortBy: 'primary_release_date.desc'
+        };
+      default:
+        return {
+          ...initialQueryParams,
+          primaryReleaseDateGte: this.today,
+        };
+    }
   };
 }
