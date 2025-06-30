ce9094df5117916b0548d4702172ce9a6ca09747
Yesid <yellaber@gmail.com>
2025-06-30 00:55:52 -0500

    src/app/pages/home/home.component.ts modificado

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,12 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, HostListener, inject, OnDestroy, OnInit } from '@angular/core';
 import { BannerUpcomingComponent } from './components/banner-upcoming/banner-upcoming.component';
 import { SectionMovieComponent } from './components/section-movie/section-movie.component';
-import { SeoFriendlyService } from '@services/seo-friendly.service';
+import { SeoFriendlyService, ScrollService } from '@services/';
 import { SectionMovie } from '@interfaces/';
 
+
 @Component({
+  selector: 'home',
   imports: [
     BannerUpcomingComponent,
     SectionMovieComponent
@@ -20,13 +22,24 @@ import { SectionMovie } from '@interfaces/';
   `,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export default class HomeComponent implements OnInit {
+export default class HomeComponent implements OnInit, AfterViewInit {
   sections: SectionMovie[] = [];
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if(this.scrollService.getScrollTop() > 0) {
+      this.scrollService.saveScrollPosition('home');
+    }
+  };
   private seoFriendlyService = inject(SeoFriendlyService);
+  private scrollService = inject(ScrollService);
 
   ngOnInit() {
     this.seoFriendlyService.setMetaTags('Inicio', 'Descubre películas de forma rápida y divertida en MovieVerso. Explora por nombre, género o popularidad y accede a información detallada de tus películas favoritas. ¡Tu universo de cine comienza aquí!');
     this.loadSections();
+  }
+
+  ngAfterViewInit() {
+    this.scrollService.restoreScrollPosition('home');
   }
 
   loadSections() {
