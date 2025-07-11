7feb0d15a8b50fece581427eaa9693618e81c04c
Yesid <yellaber@gmail.com>
2025-07-10 21:41:29 -0500

    src/app/pages/popular/popular.component.ts componente modificado

--- src/app/pages/popular/popular.component.ts
+++ src/app/pages/popular/popular.component.ts
@@ -1,15 +1,42 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
-import { SeoFriendlyService } from '@services/seo-friendly.service';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
+import { CategoriesComponent } from '@shared/categories/categories.component';
+import { LoadMoviesFilteredComponent } from '@shared/load-movies-filtered/load-movies-filtered.component';
+import { SeoFriendlyService } from '@services/';
+import { EndPointValid } from '@interfaces/';
+
+const menuItems = ['upcoming', 'now-playing', 'top-rated', 'trending'];
 
 @Component({
-  imports: [],
-  templateUrl: './popular.component.html',
+  selector: 'popular',
+  imports: [
+    CategoriesComponent,
+    LoadMoviesFilteredComponent,
+  ],
+  template: `
+    <div class="flex flex-col gap-5 pt-12 lg:pt-15">
+      <h3 class="text-sm lg:text-xl font-bold rounded-full text-yellow-600 gap-2">
+        {{ titlePage() }}
+      </h3>
+      <p class="text-xs lg:text-sm text-stone-300 leading-relaxed">
+        {{ textPage() }}
+      </p>
+    </div>
+    <categories [menuItems]="menuItems()"/>
+    <load-movies-filtered [endPoint]="endPointValid()"/>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class PopularComponent implements OnInit {
+  titlePage = signal<string>('');
+  textPage = signal<string>('');
+  menuItems = signal<string[]>([]);
+  endPointValid = signal<EndPointValid>(EndPointValid.popular);
   private seoFriendlyService = inject(SeoFriendlyService);
 
   ngOnInit(): void {
-    this.seoFriendlyService.setMetaTags('Popular', 'Esta es la página para las películas populres');
-  }
+    this.menuItems.set(menuItems);
+    this.titlePage.set('Populares');
+    this.textPage.set('Las favoritas del momento: Estas películas están rompiendo el internet… y la taquilla. ¿Ya viste por qué todos hablan de ellas?');
+    this.seoFriendlyService.setMetaTags(this.titlePage(), this.textPage());
+  };
 }
