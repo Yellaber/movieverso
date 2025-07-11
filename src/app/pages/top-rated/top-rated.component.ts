4c6baa7450d4163f0c7820a7c65cb6e294ecc04e
Yesid <yellaber@gmail.com>
2025-07-10 21:59:55 -0500

    src/app/pages/top-rated/ componente creado

---
+++ src/app/pages/top-rated/top-rated.component.ts
@@ -0,0 +1,42 @@
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
+import { CategoriesComponent } from '@shared/categories/categories.component';
+import { LoadMoviesFilteredComponent } from '@shared/load-movies-filtered/load-movies-filtered.component';
+import { SeoFriendlyService } from '@services/seo-friendly.service';
+import { EndPointValid } from '@interfaces/';
+
+const menuItems = ['upcoming', 'now-playing', 'popular', 'trending'];
+
+@Component({
+  selector: 'top-rated',
+  imports: [
+    CategoriesComponent,
+    LoadMoviesFilteredComponent
+  ],
+  template: `
+    <div class="flex flex-col gap-5 pt-12 lg:pt-15">
+      <h3 class="text-sm lg:text-xl font-bold rounded-full text-yellow-600 gap-2">
+        {{ titlePage() }}
+      </h3>
+      <p class="text-xs lg:text-sm text-stone-300 leading-relaxed">
+        {{ textPage() }}
+      </p>
+    </div>
+    <categories [menuItems]="menuItems()"/>
+    <load-movies-filtered [endPoint]="endPointValid()"/>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export default class TopRatedComponent implements OnInit {
+  titlePage = signal<string>('');
+  textPage = signal<string>('');
+  menuItems = signal<string[]>([]);
+  endPointValid = signal<EndPointValid>(EndPointValid.topRated);
+  private seoFriendlyService = inject(SeoFriendlyService);
+
+  ngOnInit() {
+    this.menuItems.set(menuItems);
+    this.titlePage.set('Mejor valoradas');
+    this.textPage.set('Alta puntuación, alto impacto: Si buscas calidad garantizada, esta es tu zona segura. Películas que se ganaron un 10… o casi.');
+    this.seoFriendlyService.setMetaTags(this.titlePage(), this.textPage());
+  }
+}
