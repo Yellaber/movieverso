b4172e7a922cf567fec32845d5eb295df73019f8
Yesid <yellaber@gmail.com>
2025-07-10 21:39:29 -0500

    src/app/services/index.ts modificado

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,6 +1,7 @@
-export { FilterService } from './filter.service';
 export { FlagCdnService } from './flag-cdn.service';
+export { QueryParamsService } from './query-params.service';
 export { RoutesService } from './routes.service';
+export { ScrollService } from './scroll.service';
 export { SeoFriendlyService } from './seo-friendly.service';
 export { SlugifyService } from './slugify.service';
 export { TmdbService } from './tmdb.service';
