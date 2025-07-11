aea73ec5bd8a352e228e2a8b16c6cc888b1cc1c1
Yesid <yellaber@gmail.com>
2025-07-10 21:49:21 -0500

    src/app/pages/detail-movie/components/movie-list/movie-list.component.ts modificado

--- src/app/pages/detail-movie/components/movie-list/movie-list.component.ts
+++ src/app/pages/detail-movie/components/movie-list/movie-list.component.ts
@@ -4,7 +4,7 @@ import { CarruselTitleComponent } from '@shared/carrusel-movies/carrusel-title/c
 import { NotificationComponent } from '@shared/notification/notification.component';
 import { CarouselConfig, Movie } from '@interfaces/';
 
-type TypeMovieList = 'Recomendadas' | 'Similares' | 'Colección';
+type TypeMovieList = 'recommendations' | 'similar' | 'collection';
 
 interface Notification {
   title: string,
@@ -16,7 +16,7 @@ interface Notification {
   imports: [
     CarruselMoviesComponent,
     CarruselTitleComponent,
-    NotificationComponent
+    NotificationComponent,
   ],
   template: `
     @if(movies() && movies().length > 0) {
@@ -35,24 +35,24 @@ export class MovieListComponent {
   carouselConfig = computed<CarouselConfig>(() => ({
     carouselTitle: this.typeMovieList(),
     movies: this.movies(),
-    route: this.hasRoute()? `/list`: '',
+    route: this.hasRoute()? `${this.typeMovieList()}`: '',
     bgButtons: 'from-stone-800',
     bgCardFooter: 'bg-stone-700'
   }));
 
   getNotification(): Notification {
     switch(this.typeMovieList()) {
-      case('Recomendadas'):
+      case('recommendations'):
         return {
           title: 'Sin recomendaciones.',
           message: 'Parece que esta película es única en su especie. ¡Ni los algoritmos supieron qué sugerirte!'
         };
-      case('Similares'):
+      case('similar'):
         return {
           title: 'Irrepetible.',
           message: 'Buscamos algo parecido, pero nada le llega a los talones a esta película. ¡Es un caso especial!'
         };
-      case('Colección'):
+      case('collection'):
         return {
           title: 'Sin familia cinematográfica.',
           message: 'Esta película es como un lobo solitario… No pertenece a ninguna colección. ¡Libre como el viento!'
