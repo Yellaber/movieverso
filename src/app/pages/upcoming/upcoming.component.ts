18b9afc4502ee1b381eac3fa2d3d2299e637b19a
Yesid <yellaber@gmail.com>
2025-07-10 21:40:21 -0500

    src/app/pages/upcoming/upcoming.component.ts componente modificado

--- src/app/pages/upcoming/upcoming.component.ts
+++ src/app/pages/upcoming/upcoming.component.ts
@@ -1,41 +1,42 @@
-import { ChangeDetectionStrategy, Component, computed, inject, OnInit, signal } from '@angular/core';
-import { FilterButtonComponent } from '@shared/filter-button/filter-button.component';
-import { FilterModalMoviesComponent } from '@shared/filter-modal-movies/filter-modal-movies.component';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
+import { CategoriesComponent } from '@shared/categories/categories.component';
 import { LoadMoviesFilteredComponent } from '@shared/load-movies-filtered/load-movies-filtered.component';
-import { FilterService, SeoFriendlyService } from '@services/';
-import { QueryParams } from '@interfaces/';
+import { SeoFriendlyService } from '@services/';
+import { EndPointValid } from '@interfaces/';
+
+const menuItems = ['now-playing', 'popular', 'top-rated', 'trending'];
 
 @Component({
+  selector: 'upcoming',
   imports: [
-    FilterButtonComponent,
-    FilterModalMoviesComponent,
-    LoadMoviesFilteredComponent
+    CategoriesComponent,
+    LoadMoviesFilteredComponent,
   ],
-  templateUrl: './upcoming.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  host: { class: 'flex flex-col gap-10 pt-12 lg:pt-15' }
+  template: `
+    <div class="flex flex-col gap-5 pt-12 lg:pt-15">
+      <h3 class="text-sm lg:text-xl font-bold rounded-full text-yellow-600 gap-2">
+        {{ titlePage() }}
+      </h3>
+      <p class="text-xs lg:text-sm text-stone-300 leading-relaxed">
+        {{ textPage() }}
+      </p>
+    </div>
+    <categories [menuItems]="menuItems()"/>
+    <load-movies-filtered [endPoint]="endPointValid()"/>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class UpcomingComponent implements OnInit {
-  showModal = signal<boolean>(false);
-  queryParams = signal<QueryParams>({});
-  genresSelected = computed<QueryParams>(() => this.queryParams());
+  titlePage = signal<string>('');
+  textPage = signal<string>('');
+  menuItems = signal<string[]>([]);
+  endPointValid = signal<EndPointValid>(EndPointValid.upcoming);
   private seoFriendlyService = inject(SeoFriendlyService);
-  private filterService = inject(FilterService);
 
   ngOnInit() {
-    this.seoFriendlyService.setMetaTags('Próximamente', 'Esta es la página para las películas que estarán próximamente en cine.');
-    this.queryParams.set(this.filterService.getQueryParams('upcoming'));
-  };
-
-  onShowModal() {
-    this.showModal.set(true);
-  };
-
-  onCloseModal(event: boolean) {
-    this.showModal.set(event);
-  };
-
-  onQueryParams(event: QueryParams) {
-    this.queryParams.set(event);
+    this.menuItems.set(menuItems);
+    this.titlePage.set('Próximos estrenos');
+    this.textPage.set('¿Te gusta adelantarte a la función? Aquí encontrarás los próximos estrenos antes de que lleguen a la gran pantalla. ¡No dejes que nadie te spoilee lo que está por venir!');
+    this.seoFriendlyService.setMetaTags(this.titlePage(), this.textPage());
   };
 }
