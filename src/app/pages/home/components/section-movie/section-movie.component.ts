65f9ed880ded5539d8d0521cbb311080a8988edb
Yesid <yellaber@gmail.com>
2025-07-10 21:43:48 -0500

    src/app/pages/home/components/section-movie/section-movie.component.ts modificado

--- src/app/pages/home/components/section-movie/section-movie.component.ts
+++ src/app/pages/home/components/section-movie/section-movie.component.ts
@@ -4,7 +4,7 @@ import { BannerHeroSkeletonComponent } from '../banner-hero-skeleton/banner-hero
 import { CarruselMoviesComponent } from '@shared/carrusel-movies/carrusel-movies.component';
 import { CarruselMoviesSkeletonComponent } from '@shared/carrusel-movies-skeleton/carrusel-movies-skeleton.component';
 import { TmdbService } from '@services/tmdb.service';
-import { SectionMovie, Movie, CarouselConfig } from '@interfaces/';
+import { SectionMovie, Movie, CarouselConfig, EndPointValid } from '@interfaces/';
 
 @Component({
   selector: 'section-movie',
@@ -16,8 +16,7 @@ import { SectionMovie, Movie, CarouselConfig } from '@interfaces/';
   ],
   template: `
     @if(movies().length > 0) {
-      <banner-hero [heroType]="section().heroType" [heroTitle]="section().heroTitle"
-                   [movie]="movies()[0]"/>
+      <banner-hero [heroType]="section().heroType" [heroTitle]="section().heroTitle" [movie]="movies()[0]"/>
       <carrusel-movies [carouselConfig]="carouselConfig()"/>
     } @else {
       <banner-hero-skeleton/>
@@ -30,10 +29,11 @@ export class SectionMovieComponent implements OnInit {
   private tmdbService = inject(TmdbService);
   section = input.required<SectionMovie>();
   movies = signal<Movie[]>([]);
+  route = computed(() => '/' + this.section().heroType);
   carouselConfig = computed<CarouselConfig>(() => ({
     carouselTitle: this.section().carruselTitle,
     movies: this.movies(),
-    route: this.section().route,
+    route: this.route(),
     bgButtons: 'from-stone-900',
     bgCardFooter: 'bg-stone-800'
   }));
@@ -44,22 +44,18 @@ export class SectionMovieComponent implements OnInit {
 
   getDataSectionMovies() {
     switch(this.section().heroType) {
-      case 'now_playing':
-        this.tmdbService.getNowPlayingMovies()
-          .subscribe(movies => this.movies.set(movies));
+      case 'now-playing':
+          this.tmdbService.getMovies(EndPointValid.nowPlaying).subscribe(movies => this.movies.set(movies));
         break;
-      case 'popularity':
-        this.tmdbService.getMovies('/movie/popular')
-          .subscribe(movies => this.movies.set(movies));
+      case 'popular':
+        this.tmdbService.getMovies(EndPointValid.popular).subscribe(movies => this.movies.set(movies));
         break;
-      case 'rated':
-        this.tmdbService.getMovies('/movie/top_rated')
-          .subscribe(movies => this.movies.set(movies));
+      case 'top-rated':
+        this.tmdbService.getMovies(EndPointValid.topRated).subscribe(movies => this.movies.set(movies));
         break;
       case 'trending':
-        this.tmdbService.getMovies('/trending/movie/day')
-          .subscribe(movies => this.movies.set(movies));
+        this.tmdbService.getMovies(EndPointValid.trending).subscribe(movies => this.movies.set(movies));
         break;
-    };
-  }
+    }
+  };
 }
