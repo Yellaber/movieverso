debc1110c76d78432d17fe19bf81bae6f4b22b03
Yesid <yellaber@gmail.com>
2025-07-10 21:32:47 -0500

    src/app/shared/load-movies-filtered/load-movies-filtered.component.ts componente modificado

--- src/app/shared/load-movies-filtered/load-movies-filtered.component.ts
+++ src/app/shared/load-movies-filtered/load-movies-filtered.component.ts
@@ -1,10 +1,9 @@
-import { ChangeDetectionStrategy, Component, effect, HostListener, inject, input, PLATFORM_ID, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener, inject, input, OnInit, signal, effect } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
-import { isPlatformBrowser } from '@angular/common';
 import { CarruselCardMoviesComponent } from '@shared/carrusel-movies/carrusel-card-movies/carrusel-card-movies.component';
 import { NotificationComponent } from '@shared/notification/notification.component';
 import { CardMovieSkeletonComponent } from '@shared/carrusel-movies-skeleton/card-movie-skeleton/card-movie-skeleton.component';
-import { TmdbService } from '@services/';
+import { QueryParamsService, ScrollService, TmdbService } from '@services/';
 import { QueryParams } from '@interfaces/';
 
 @Component({
@@ -12,72 +11,77 @@ import { QueryParams } from '@interfaces/';
   imports: [
     CarruselCardMoviesComponent,
     NotificationComponent,
-    CardMovieSkeletonComponent
+    CardMovieSkeletonComponent,
   ],
   template: `
-    @if(!moviesFiltered.isLoading()) {
-      @for(movieResponse of moviesFiltered.value(); track $index) {
-        @for(movie of movieResponse.results; track $index) {
-          <carrusel-card-movies [movie]="movie" bgCardFooter="bg-stone-800"/>
+    <div class="flex flex-wrap justify-evenly gap-5">
+      @if(!moviesFiltered.isLoading()) {
+        @for(movieResponse of moviesFiltered.value(); track $index) {
+          @for(movie of movieResponse.results; track movie.id) {
+            <carrusel-card-movies [movie]="movie" bgCardFooter="bg-stone-800"/>
+          }
         } @empty {
-          <notification notificationTitle="Pantalla en blanco" message="Silencio absoluto... No hay títulos que coincidan con tu búsqueda. ¡Prueba ajustando el filtro y volvamos a rodar!"/>
-        }
-      }
-    } @else {
+            <notification notificationTitle="Pantalla en blanco" message="Silencio absoluto... No hay títulos que coincidan con tu búsqueda. ¡Prueba ajustando el filtro y volvamos a rodar!"/>
+          }
+      } @else {
         @for(_ of cardMoviesSkeleton; track $index) {
           <card-movie-skeleton/>
         }
-    }
+      }
+    </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  host: { class: 'flex flex-wrap justify-evenly gap-5' }
+  host: { class: 'flex flex-col gap-10 pt-12 lg:pt-15' }
 })
-export class LoadMoviesFilteredComponent {
-  private platformId = inject(PLATFORM_ID);
-  private tmdbService = inject(TmdbService);
+export class LoadMoviesFilteredComponent implements OnInit {
   cardMoviesSkeleton = Array(20);
+  endPoint = input.required<string>();
+  showModal = signal<boolean>(false);
   page = signal<number>(1);
-  queryParams = input.required<QueryParams>();
+  queryParams = signal<QueryParams>({});
   moviesFiltered = rxResource({
-    request: () => ({genres: this.queryParams(), page: this.page()}),
+    request: () => ({endPoint: this.endPoint(), page: this.page()}),
     loader: ({request}) => {
+      const endPoint = request.endPoint;
       const page = request.page;
-      return this.tmdbService.getMoviesFiltered(this.queryParams(), page)
+      return this.tmdbService.getMoviesFilteredByCategory(endPoint, page);
     }
   });
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    this.loadNextPage();
-  };
-
-  pageInitializer = effect(() => {
-    if(isPlatformBrowser(this.platformId)) {
-      window.scrollTo({top: 0});
+    if(this.scrollService.isAtBottom() && this.moviesFiltered.hasValue()) {
+      const movieResponse = this.moviesFiltered.value()[this.page() - 1];
+      if(this.page() < movieResponse.total_pages) {
+        this.page.update(value => value + 1);
+      }
     }
-    const genres = this.queryParams().withGenres ?? [];
-    if(genres) {
+  };
+  private tmdbService = inject(TmdbService);
+  private scrollService = inject(ScrollService);
+  //private queryParamsService = inject(QueryParamsService);
+
+  queryParamsChanged = effect(() => {
+    const queryParams = this.queryParams();
+    if(queryParams) {
       this.page.set(1);
+      this.scrollService.scrollTop();
     }
   });
 
-  loadNextPage() {
-    if(this.isAtBottom() && this.moviesFiltered.hasValue()) {
-      const moviesPagination = this.moviesFiltered.value();
-      const totalPages = moviesPagination[this.page() - 1].total_pages;
-      if(this.page() === totalPages) {
-        return;
-      }
-      this.page.update(value => value + 1);
-    }
+  ngOnInit() {
+    //const queryParams = this.queryParamsService.get(this.currentComponent());
+    //this.queryParams.set(queryParams);
+  }
+
+  onShowModal() {
+    this.showModal.set(true);
   };
 
-  isAtBottom(): boolean {
-    if(isPlatformBrowser(this.platformId)) {
-      const scrollTop = document.documentElement.scrollTop;
-      const clientHeight = document.documentElement.clientHeight;
-      const scrollHeight = document.documentElement.scrollHeight;
-      return scrollTop + clientHeight + 300 >= scrollHeight;
-    }
-    return false;
+  onCloseModal(event: boolean) {
+    this.showModal.set(event);
+  };
+
+  onQueryParams(event: QueryParams) {
+    this.queryParams.set(event);
   };
 }
