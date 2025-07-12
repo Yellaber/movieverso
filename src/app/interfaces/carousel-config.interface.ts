7525d2f5d1d2f23e96f00e4d81f719a887ef60fe
Yesid <yellaber@gmail.com>
2025-07-12 18:22:55 -0500

    src/app/interfaces/carousel-config.interface.ts modificado

--- src/app/interfaces/carousel-config.interface.ts
+++ src/app/interfaces/carousel-config.interface.ts
@@ -2,6 +2,7 @@ import { Movie } from './movie-response.interface';
 
 export interface CarouselConfig {
   carouselTitle: string,
+  text: string;
   movies: Movie[],
   route: string,
   bgButtons: string,
