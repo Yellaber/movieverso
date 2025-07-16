1257ecbe56f72a44e4c8fefe0d54c9b6d17cf3f7
Yesid <yellaber@gmail.com>
2025-07-16 16:50:38 -0500

    src/app/services/query-params.service.ts modificado

--- src/app/services/query-params.service.ts
+++ src/app/services/query-params.service.ts
@@ -1,62 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Signal, signal } from '@angular/core';
 import { QueryParams } from '@interfaces/';
-
-const initialQueryParams: QueryParams = {
-  withGenres: [],
-  withOriginalLanguage: '',
-  primaryReleaseYear: new Date().getFullYear(),
-  voteAverageGte: 0,
-  withOriginCountry: '',
-  primaryReleaseDateGte: '',
-  primaryReleaseDateLte: '',
-  withCompanies: '',
-  releaseDateGte: '',
-  sortBy: 'primary_release_date.desc',
-  voteCountGte: 0
-};
 
 @Injectable({
   providedIn: 'root'
 })
 export class QueryParamsService {
-  private today: string = '';
-  private cacheQueryParams = new Map<string, QueryParams>();
+  private queryParams = signal<QueryParams | undefined>(undefined);
 
-  constructor() {
-    this.today = new Date().toISOString().split('T')[0];
-  }
-
-  set(key: string, queryParams: QueryParams) {
-    this.cacheQueryParams.set(key, queryParams);
+  set(queryParams: QueryParams) {
+    this.queryParams.set(queryParams);
   };
 
-  get(key: string): QueryParams {
-    if(this.cacheQueryParams.has(key)) {
-      return this.cacheQueryParams.get(key)!;
-    }
-    const queryParams = this.getInitialQueryParams(key);
-    this.set(key, queryParams);
-    return queryParams;
-  };
-
-  private getInitialQueryParams(key: string): QueryParams {
-    return {
-      ...initialQueryParams,
-      primaryReleaseDateLte: this.today,
-      sortBy: 'vote_count.desc'
-    };
-    /*switch(key) {
-      case ComponentToFilter.nowPlaying:
-        return {
-          ...initialQueryParams,
-          primaryReleaseDateLte: this.today,
-          sortBy: 'vote_count.desc'
-        };
-      default:
-        return {
-          ...initialQueryParams,
-          primaryReleaseDateGte: this.today,
-        };
-    }*/
+  get(): Signal<QueryParams | undefined> {
+    return this.queryParams.asReadonly();
   };
 }
