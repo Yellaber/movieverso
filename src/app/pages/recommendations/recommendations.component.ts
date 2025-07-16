eeafaa18e7a98ea3165703f37e51a53d15867418
Yesid <yellaber@gmail.com>
2025-07-16 16:46:04 -0500

    src/app/pages/recommendations/ componente modificado

--- src/app/pages/recommendations/recommendations.component.ts
+++ src/app/pages/recommendations/recommendations.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { CategoriesComponent } from '@shared/categories/categories.component';
 import { NotificationComponent } from '@shared/notification/notification.component';
-import { LoadResultsComponent } from '@shared/load-results/load-results.component';
+import { LoadRelationedComponent } from '@shared/load-relationed/load-relationed.component';
 import { SeoFriendlyService, TmdbService } from '@services/';
 
 const menuItems = ['upcoming', 'now-playing', 'popular', 'top-rated', 'trending'];
@@ -14,15 +14,16 @@ const menuItems = ['upcoming', 'now-playing', 'popular', 'top-rated', 'trending'
     RouterLink,
     CategoriesComponent,
     NotificationComponent,
-    LoadResultsComponent,
+    LoadRelationedComponent,
   ],
   templateUrl: './recommendations.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class RecommendationsComponent implements OnInit {
   private route = inject(ActivatedRoute);
   private seoFriendlyService = inject(SeoFriendlyService);
   private tmdbService = inject(TmdbService);
+  idSlug = signal<string>('');
   titlePage = signal<string>('');
   menuItems = signal<string[]>([]);
   movieId = signal<number | undefined>(undefined);
@@ -31,15 +32,13 @@ export default class RecommendationsComponent implements OnInit {
     request: this.movieId,
     loader: () => this.tmdbService.getMovieById(this.movieId()!)
   });
-  titleDescription = computed(() =>
-    this.movie.hasValue()? `${this.titlePage()} basadas en: "${this.movie.value().title}"`: ''
-  );
+  titleDescription = computed(() => this.movie.hasValue()? `${this.titlePage()} basadas en:`: '');
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
-      const idSlug = params.get('id-slug') || '';
-      this.linkSimilarMovies.set(`/movie/${idSlug}/similar`);
-      this.movieId.set(+idSlug.split('-')[0]);
+      this.idSlug.set(params.get('id-slug') || '');
+      this.linkSimilarMovies.set(`/movie/${this.idSlug()}/similar`);
+      this.movieId.set(+this.idSlug().split('-')[0]);
     });
     this.menuItems.set(menuItems);
     this.titlePage.set('Películas recomendadas');
