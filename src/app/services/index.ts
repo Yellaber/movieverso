b3f419bb6201ad7cfd6a346d3dbdcb7f02251681
Yesid <yellaber@gmail.com>
2025-07-16 16:51:04 -0500

    src/app/services/index.ts modificado

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,4 +1,5 @@
 export { FlagCdnService } from './flag-cdn.service';
+export { MenubarService } from './menubar.service.ts.service';
 export { QueryParamsService } from './query-params.service';
 export { RoutesService } from './routes.service';
 export { ScrollService } from './scroll.service';
