1cef563291635d75931c2c29b692b9f66c77c1d6
Yesid <yellaber@gmail.com>
2025-07-16 16:59:44 -0500

    src/app/shared/carrusel-movies/components/ creado

---
+++ src/app/shared/carrusel-movies/components/carrusel-button/carrusel-button.component.ts
@@ -0,0 +1,39 @@
+import { ChangeDetectionStrategy, Component, computed, input, output } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+
+interface Direction {
+  label: string,
+  faIcon: IconDefinition,
+  class: string
+};
+
+const directions: Direction[] = [
+  { label: 'previous', faIcon: faAngleLeft, class: 'left-0 bg-gradient-to-r' },
+  { label: 'next', faIcon: faAngleRight, class: 'right-0 bg-gradient-to-l' }
+];
+
+@Component({
+  selector: 'carrusel-button',
+  imports: [ FontAwesomeModule ],
+  template: `
+    @if(getDirection()) {
+      <button (click)="handleChangeScrollState()" [class]="classButton()">
+        <fa-icon class="text-2xl lg:text-3xl" [icon]="getDirection()!.faIcon"></fa-icon>
+      </button>
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CarruselButtonComponent {
+  direction = input.required<string>();
+  bgButton = input.required<string>();
+  emitDirection = output<string>();
+  getDirection = computed(() => directions.find(angle => angle.label === this.direction()));
+  classButton = computed<string>(() => `absolute top-1/2 -translate-y-1/2 hover:cursor-pointer h-full ${this.getDirection()?.class} ${this.bgButton()} to-transparent z-10 px-2 lg:px-3`);
+
+  handleChangeScrollState() {
+    this.getDirection() && this.emitDirection.emit(this.getDirection()!.label);
+  }
+}
---
+++ src/app/shared/carrusel-movies/components/carrusel-card-movies/carrusel-card-movies.component.ts
@@ -0,0 +1,32 @@
+import { ChangeDetectionStrategy, Component, computed, inject, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBookmark } from '@fortawesome/free-solid-svg-icons';
+import { RatingComponent } from '@shared/rating/rating.component';
+import { environment } from '@environments/environment.developments';
+import { SlugifyService } from '@services/slugify.service';
+import { Movie } from '@interfaces/movie-response.interface';
+
+@Component({
+  selector: 'carrusel-card-movies',
+  imports: [
+    RouterLink,
+    FontAwesomeModule,
+    RatingComponent
+  ],
+  templateUrl: './carrusel-card-movies.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: { class: 'flex flex-col min-w-[160px] max-w-[160px] rounded-md shadow-md' }
+})
+export class CarruselCardMoviesComponent {
+  faBokmark = faBookmark;
+  movie = input.required<Movie>();
+  bgCardFooter = input.required<string>();
+  srcImage = computed(() => environment.imageUrl + this.movie().poster_path);
+  bgClassCardFooter = computed<string>(() => `flex justify-between items-center ${this.bgCardFooter()} rounded-b-md p-3`);
+  private slugifyService = inject(SlugifyService);
+
+  slugify(title: string): string {
+    return this.slugifyService.getSlug(title);
+  };
+}
