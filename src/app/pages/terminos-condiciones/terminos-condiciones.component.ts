df70af4ba0d92b7f11c3d7c34f862b065d7c53af
Yesid <yellaber@gmail.com>
2025-07-18 14:00:51 -0500

    src/app/pages/terminos-condiciones/ se agregaron los componentes <secondary-header>, <paragraph> y las clases correspondientes en el host.

--- src/app/pages/terminos-condiciones/terminos-condiciones.component.ts
+++ src/app/pages/terminos-condiciones/terminos-condiciones.component.ts
@@ -1,15 +1,22 @@
 import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
-import { SeoFriendlyService } from '@services/seo-friendly.service';
+import { SecondaryHeaderComponent } from './components/secondary-header/secondary-header.component';
+import { ParagraphComponent } from './components/paragraph/paragraph.component';
+import { SeoFriendlyService } from '@services/';
 
 @Component({
-  imports: [],
+  selector: 'terminos-condiciones',
+  imports: [
+    SecondaryHeaderComponent,
+    ParagraphComponent
+  ],
   templateUrl: './terminos-condiciones.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: { class: 'flex flex-col text-xs lg:text-sm text-stone-300 mt-10 lg:mt-15' }
 })
 export default class TerminosCondicionesComponent implements OnInit {
   private seoFriendlyService = inject(SeoFriendlyService);
 
   ngOnInit() {
-    this.seoFriendlyService.setMetaTags('Términos y condiciones', 'Esta es la página para los términos y condiciones del sitio');
+    this.seoFriendlyService.setMetaTags('Términos y condiciones', '');
   }
 }
