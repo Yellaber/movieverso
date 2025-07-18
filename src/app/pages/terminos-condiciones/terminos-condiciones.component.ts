7c1b3a0f5bd61533ec082fe0fee6a745d320e4b7
Yesid <yellaber@gmail.com>
2025-07-18 14:06:59 -0500

    src/app/pages/terminos-condiciones/terminos-condiciones.component.ts se agregó scrollService para mover el scroll a la posición top del sitio.

--- src/app/pages/terminos-condiciones/terminos-condiciones.component.ts
+++ src/app/pages/terminos-condiciones/terminos-condiciones.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { SecondaryHeaderComponent } from './components/secondary-header/secondary-header.component';
 import { ParagraphComponent } from './components/paragraph/paragraph.component';
-import { SeoFriendlyService } from '@services/';
+import { ScrollService, SeoFriendlyService } from '@services/';
 
 @Component({
   selector: 'terminos-condiciones',
@@ -14,9 +14,11 @@ import { SeoFriendlyService } from '@services/';
   host: { class: 'flex flex-col text-xs lg:text-sm text-stone-300 mt-10 lg:mt-15' }
 })
 export default class TerminosCondicionesComponent implements OnInit {
+  private scrollService = inject(ScrollService);
   private seoFriendlyService = inject(SeoFriendlyService);
 
   ngOnInit() {
     this.seoFriendlyService.setMetaTags('Términos y condiciones', '');
+    this.scrollService.scrollTop();
   }
 }
