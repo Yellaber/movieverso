3bae995e6a5a8de1c9ebcb9da0bdbfad3dcc3991
Yesid <yellaber@gmail.com>
2025-07-20 22:21:33 -0500

    src/app/services/ eliminado

--- src/app/services/index.ts
+++
@@ -1,9 +0,0 @@
-export { FlagCdnService } from './flag-cdn.service';
-export { MenubarService } from './menubar.service.ts.service';
-export { QueryParamsService } from './query-params.service';
-export { RoutesService } from './routes.service';
-export { ScrollService } from './scroll.service';
-export { SeoFriendlyService } from './seo-friendly.service';
-export { SlugifyService } from './slugify.service';
-export { TmdbService } from './tmdb.service';
-export { UserGeolocationService } from './user-geolocation.service';
