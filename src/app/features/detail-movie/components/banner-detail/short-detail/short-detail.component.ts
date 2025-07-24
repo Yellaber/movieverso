dab876b7e6b798f04eece64bd4b673ba69912843
Yesid <yellaber@gmail.com>
2025-07-24 15:53:46 -0500

    src/app/features/detail-movie/components/banner-detail/short-detail/short-detail.component.ts se agregó la clase w-full al host

--- src/app/features/detail-movie/components/banner-detail/short-detail/short-detail.component.ts
+++ src/app/features/detail-movie/components/banner-detail/short-detail/short-detail.component.ts
@@ -13,7 +13,7 @@ import { DetailMovieResponse } from '@shared/interfaces';
   ],
   templateUrl: './short-detail.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  host: { class: 'relative text-stone-50 z-10 md:pl-10 lg:pl-20' }
+  host: { class: 'relative w-full text-stone-50 z-10 md:pl-10 lg:pl-20' }
 })
 export class ShortDetailComponent {
   movieDetail = input.required<DetailMovieResponse>();
