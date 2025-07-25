374c12cacac93fff15a9f0d0719a3b49a7171efa
Yesid <yellaber@gmail.com>
2025-07-25 16:41:53 -0500

    test(services): añade pruebas unitarias para RoutesService
    Introduce una suite de pruebas para \RoutesService\.
    
    Esta suite de pruebas cubre los siguientes escenarios:
    - Creación e inyección correcta del servicio.
    - Verificación del estado inicial como un array vacío.
    - Correcta actualización del estado al usar el método \setRoutes()\.
    - Garantiza que \getRoutes\ es un \computed signal\ de solo lectura.

---
+++ src/app/shared/services/routes.service.spec.ts
@@ -0,0 +1,52 @@
+import { TestBed } from '@angular/core/testing';
+import { Route } from '@angular/router';
+import { RoutesService } from './routes.service';
+
+const mockRoutes: Route[] = [
+  { path: 'home', title: 'Home' },
+  { path: 'upcoming', title: 'Upcoming' },
+  { path: 'popular', title: 'Popular' },
+  { path: 'top-rated', title: 'Top Rated' },
+  { path: 'now-playing', title: 'Now Playing' },
+  { path: 'trending', title: 'Trending' }
+];
+
+const updatedMockRoutes: Route[] = [
+  { path: 'contact', title: 'Contact' }
+];
+
+describe('RoutesService', () => {
+  let service: RoutesService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [RoutesService]
+    });
+    service = TestBed.inject(RoutesService);
+  });
+
+  it('Should be created.', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('Should have an initial empty array of routes.', () => {
+    expect(service.getRoutes()).toEqual([]);
+  });
+
+  it('Should set routes correctly.', () => {
+    service.setRoutes(mockRoutes);
+    expect(service.getRoutes()).toEqual(mockRoutes);
+  });
+
+  it('Should update routes when setRoutes is called again.', () => {
+    service.setRoutes(mockRoutes);
+    expect(service.getRoutes()).toEqual(mockRoutes);
+
+    service.setRoutes(updatedMockRoutes);
+    expect(service.getRoutes()).toEqual(updatedMockRoutes);
+  });
+
+  it('getRoutes should return a readonly computed signal.', () => {
+    expect((service.getRoutes as any).set).toBeUndefined();
+  });
+});
