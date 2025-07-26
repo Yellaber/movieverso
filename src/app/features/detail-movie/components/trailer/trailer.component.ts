66a0ed9e185d8fd860e5e19a493b737b98e0de40
Yesid <yellaber@gmail.com>
2025-07-26 14:55:01 -0500

    feat(detail-movie): añade componente Trailer para mostrar videos
    Introduce el componente \TrailerComponent\ para mostrar el tráiler de una película.
    
    Utiliza \xResource\ para cargar los datos del tráiler de forma reactiva e implementa \DomSanitizer\ para asegurar que la URL de YouTube sea segura.
    
    Muestra una notificación si no se encuentran tráilers disponibles.

--- src/app/features/detail-movie/components/trailer/trailer.component.ts
+++ src/app/features/detail-movie/components/trailer/trailer.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
-import { YouTubePlayer } from '@angular/youtube-player';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { of } from 'rxjs';
 import { CarruselTitleComponent } from '@shared/components/carrusel-movies/carrusel-title/carrusel-title.component';
 import { NotificationComponent } from '@shared/components/notification/notification.component';
@@ -10,14 +10,15 @@ import { DetailService } from '../../services/detail.service';
   selector: 'trailer',
   imports: [
     CarruselTitleComponent,
-    YouTubePlayer,
     NotificationComponent
   ],
   template: `
     <carrusel-title carruselTitle="Trailer" route=""/>
     @if(movieTrailers.hasValue() && movieTrailers.value()[0]) {
-      <div class="flex w-full justify-center">
-        <youtube-player [videoId]="movieTrailers.value()[0].key" placeholderImageQuality="high"/>
+      <div class="w-full max-w-3xl mx-auto aspect-video pt-5">
+        <iframe class="w-full h-full" [src]="getSafeYoutubeUrl(movieTrailers.value()[0].key)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen>
+        </iframe>
       </div>
     } @else {
       <notification notificationTitle="Spoiler invisible." message="Nada por aquí, nada por allá… ni un solo avance que mostrar. ¡La película se está haciendo la difícil!"/>
@@ -27,10 +28,15 @@ import { DetailService } from '../../services/detail.service';
 })
 export class TrailerComponent {
   private detailService = inject(DetailService);
+  private sanitizer = inject(DomSanitizer);
   movieId = input.required<number>();
 
   movieTrailers = rxResource({
     request: this.movieId,
     loader: () => this.movieId()? this.detailService.getMovieTrailers(this.movieId()!): of([])
   });
+
+  getSafeYoutubeUrl(key: string): SafeResourceUrl {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${key}`);
+  }
 }
