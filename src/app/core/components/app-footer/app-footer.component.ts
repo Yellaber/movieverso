d2eb5f4d2d1b5f3b76f03f7f00d22682d3760248
Yesid <yellaber@gmail.com>
2025-07-26 20:17:45 -0500

    feat(app-footer): crear componente Footer simple y responsivo
    - Se implementa el componente Footer con diseño responsivo y minimalista.
    - Se utiliza ChangeDetectionStrategy.OnPush para optimizar el rendimiento.
    - Incluye el año y el nombre de la aplicación.

--- src/app/core/components/app-footer/app-footer.component.ts
+++ src/app/core/components/app-footer/app-footer.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterLink],
-  templateUrl: './app-footer.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [],
+  template: `
+    <footer class="flex flex-col md:flex-row justify-center items-center text-stone-400 gap-2 md:gap-5 p-5">
+      <span class="text-xs lg:text-sm">&copy; 2025-MovieVerso</span>
+    </footer>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FooterComponent { }
