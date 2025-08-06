eb2c796a8e239f22c203c36064284c272172e7a4
Yesid <yellaber@gmail.com>
2025-08-06 14:13:26 -0500

    feat(header): agrega la lógica para el componente principal de la cabecera

--- src/app/core/components/header/app-header.component.ts
+++ src/app/core/components/header/app-header.component.ts
@@ -1,26 +1,28 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal, viewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { TranslatePipe } from "@ngx-translate/core";
 import { LogoComponent } from '@app/core/components/header/logo/logo.component';
 import { SearchBarComponent } from '@app/core/components/header/search-bar/search-bar.component';
 import { FilterModalMoviesComponent } from '@shared/components/filter-modal-movies/filter-modal-movies.component';
 import { UserGeolocationService } from '@app/core/services';
-import { UserGeolocation } from '@app/shared/interfaces';
+import { UserGeolocation } from '@shared/interfaces';
 
 @Component({
   selector: 'app-header',
   imports: [
     LogoComponent,
     SearchBarComponent,
-    FilterModalMoviesComponent
+    FilterModalMoviesComponent,
+    TranslatePipe
   ],
   templateUrl: './app-header.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: { class: 'relative' }
 })
-export class HeaderComponent {
+export class AppHeaderComponent {
   private router = inject(Router);
   private userGeolocationService = inject(UserGeolocationService);
-  showModal = signal<boolean>(false);
+  searchBarRef = viewChild(SearchBarComponent);
   userGeolocation = signal<UserGeolocation | undefined>(undefined);
 
   ngOnInit() {
@@ -34,12 +36,8 @@ export class HeaderComponent {
     }
   };
 
-  onShowModal() {
-    this.showModal.set(true);
-  };
-
-  onCloseModal(event: boolean) {
-    this.showModal.set(event);
+  onCloseModal() {
+    this.searchBarRef()?.isOpenedFilter.set(false);
   };
 
   navigateToUpcoming() {
