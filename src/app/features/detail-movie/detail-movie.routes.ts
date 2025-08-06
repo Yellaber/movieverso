30b52fa2f5c826ceeb0354dc493c1ea8695010f8
Yesid <yellaber@gmail.com>
2025-08-06 15:54:40 -0500

    feat(detail-movie): agrega las rutas para la página de detalle de película

--- src/app/features/detail-movie/detail-movie.routes.ts
+++ src/app/features/detail-movie/detail-movie.routes.ts
@@ -5,20 +5,21 @@ export const detailMovieRoutes: Routes = [
     path: 'movie/:id-slug',
     children: [
       {
-        title: 'Detalle',
         path: '',
         loadComponent: () => import('./detail-movie.component')
       },
       {
-        title: 'Recomendaciones',
         path: 'recommendations',
         loadComponent: () => import('../recommendations/recommendations.component')
       },
       {
-        title: 'Similares',
         path: 'similar',
         loadComponent: () => import('../similars/similars.component')
       },
+      {
+        path: '**',
+        redirectTo: ''
+      }
     ]
   }
 ];
