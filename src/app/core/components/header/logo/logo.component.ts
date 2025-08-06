1ad7f6ab4a7ffe91abcc2362ee22faf98315cd1e
Yesid <yellaber@gmail.com>
2025-08-05 21:59:14 -0500

    feat(header): agrega lógica para el componente de logo

--- src/app/core/components/header/logo/logo.component.ts
+++ src/app/core/components/header/logo/logo.component.ts
@@ -1,15 +1,18 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TranslatePipe } from "@ngx-translate/core";
 import { environment } from '@environments/environment.developments';
 
 @Component({
   selector: 'logo',
-  imports: [ RouterLink ],
+  imports: [
+    RouterLink,
+    TranslatePipe
+  ],
   templateUrl: './logo.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LogoComponent {
   countryFlag = input.required<string | undefined>();
   appName = environment.appName;
-  slogan = environment.slogan;
 }
