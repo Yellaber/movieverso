a16e3fad7bfd6e72504ce405d56abb4e44ab230f
Yesid <yellaber@gmail.com>
2025-08-06 14:46:57 -0500

    feat(detail-movie): agrega lógica para el componente de países de producción

--- src/app/features/detail-movie/components/side-detail/production-countries/production-countries.component.ts
+++ src/app/features/detail-movie/components/side-detail/production-countries/production-countries.component.ts
@@ -1,19 +1,21 @@
 import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 import { FlagCdnService } from '@app/features/detail-movie/services/flag-cdn.service';
 import { ProductionCountry } from '@shared/interfaces';
 
 @Component({
   selector: 'production-countries',
-  imports: [],
+  imports: [ TranslatePipe ],
   templateUrl: './production-countries.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: { class: 'flex flex-col gap-3' }
 })
 export class ProductionCountriesComponent {
   private flagcdnService = inject(FlagCdnService);
+  private readonly FLAG_SIZE = '80';
   productionCountries = input.required<ProductionCountry[]>();
 
   getFlagCountry(iso31661Code: string): string {
-    return this.flagcdnService.getFlagCountry(iso31661Code, '80');
+    return this.flagcdnService.getFlagCountry(iso31661Code, this.FLAG_SIZE);
   };
 }
