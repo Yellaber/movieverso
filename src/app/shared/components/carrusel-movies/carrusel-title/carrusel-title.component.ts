cb2154cd901133dae6fa5ef16f6e99d65b5aa799
Yesid <yellaber@gmail.com>
2025-08-06 20:15:07 -0500

    feat(shared): agrega componente para el título de los carruseles

--- src/app/shared/components/carrusel-movies/carrusel-title/carrusel-title.component.ts
+++ src/app/shared/components/carrusel-movies/carrusel-title/carrusel-title.component.ts
@@ -1,9 +1,13 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'carrusel-title',
-  imports: [ RouterLink ],
+  imports: [
+    RouterLink,
+    TranslatePipe
+  ],
   template: `
     @if(carruselTitle()) {
       <div class="rounded-full bg-yellow-900/50 px-4 py-2">
@@ -11,13 +15,13 @@ import { RouterLink } from '@angular/router';
       </div>
     }
     @if(route()) {
-      <a class="text-sm lg:text-xl" [routerLink]="route()">Ver todas</a>
+      <a class="text-sm lg:text-xl" [routerLink]="route()">{{ 'carouselLink' | translate }}</a>
     }
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  host: { class: 'flex justify-between items-center text-yellow-600 font-semibold mb-5'}
+  host: { class: 'flex justify-between items-center text-yellow-600 font-semibold mb-5' }
 })
 export class CarruselTitleComponent {
   carruselTitle = input.required<string>();
-  route = input.required<string>();
+  route = input<string>();
 }
