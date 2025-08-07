1cca4f1a6da04ea93474c676a6f37626b58e28ee
Yesid <yellaber@gmail.com>
2025-08-07 11:30:13 -0500

    refactor(core): mejora la configuración de idioma inicial en ngx-translate

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,24 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { provideTranslateService } from "@ngx-translate/core";
+import { provideTranslateHttpLoader } from "@ngx-translate/http-loader";
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideHttpClient(withFetch()), provideClientHydration(withEventReplay()),
+    provideHttpClient(withFetch()),
+    provideClientHydration(withEventReplay()),
     provideRouter(routes),
-    provideAnimationsAsync(),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: './i18n/',
+        suffix: '.json'
+      }),
+      fallbackLang: 'en',
+      lang: 'es'
+    })
   ]
 };
