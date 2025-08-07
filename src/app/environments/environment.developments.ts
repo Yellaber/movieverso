85e1db944e27e04719672a031482bb5103c84eb5
Yesid <yellaber@gmail.com>
2025-08-07 12:04:32 -0500

    fix(env): corrige el indicador de producción para el entorno de desarrollo

--- src/app/environments/environment.developments.ts
+++ src/app/environments/environment.developments.ts
@@ -1,8 +1,8 @@
 export const environment = {
-  production: true,
+  production: false,
   appName: 'MovieVerso',
-  slogan: 'Tu universo de peliculas',
   tmdbApiKey: 'c6eb6487500a7727c7f8fec7a57eb1ab',
   tmdbApiUrl: 'https://api.themoviedb.org/3',
-  imageUrl: 'https://image.tmdb.org/t/p/original'
+  imageUrl: 'https://image.tmdb.org/t/p/original',
+  ipGeolocationApiUrl: 'https://api.ipgeolocation.io/v2/ipgeo'
 };
