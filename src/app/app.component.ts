9cdf21928c38e059a89e35fca610965cb3ad9fd0
Yesid <yellaber@gmail.com>
2025-08-07 10:48:15 -0500

    feat(core): agrega la lógica para el componente raíz de la aplicación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,16 @@ import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/cor
 import { RouterOutlet } from '@angular/router';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { routes } from './app.routes';
-import { HeaderComponent } from './core/components/header/app-header.component';
+import { AppHeaderComponent } from './core/components/header/app-header.component';
 import { FooterComponent } from './core/components/app-footer/app-footer.component';
+import { environment } from './environments/environment.developments';
 import { UserGeolocationService } from './core/services';
 import { RoutesService } from './shared/services/routes.service';
-
-const API_URL_IPGEOLOCATION = 'https://api.ipgeolocation.io/v2/ipgeo';
 
 @Component({
   selector: 'app-root',
   imports: [
-    HeaderComponent,
+    AppHeaderComponent,
     FooterComponent,
     RouterOutlet,
   ],
@@ -22,12 +21,12 @@ const API_URL_IPGEOLOCATION = 'https://api.ipgeolocation.io/v2/ipgeo';
 export class AppComponent {
   private userGeolocationService = inject(UserGeolocationService);
   private routesService = inject(RoutesService);
-  apiUrlGeolocation = signal<string>(API_URL_IPGEOLOCATION);
+  apiUrlGeolocation = signal<string>(environment.ipGeolocationApiUrl);
   geoLocation = rxResource({
     request: this.apiUrlGeolocation,
-    loader: () => this.userGeolocationService.loadUserLocation(this.apiUrlGeolocation())
+    loader: ({ request }) => this.userGeolocationService.loadUserLocation(request)
   });
-  title = 'MovieVerso';
+  title = environment.appName;
 
   constructor() {
     this.routesService.setRoutes(routes);
