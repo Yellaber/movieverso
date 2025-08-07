9bddf9929dc07c5c6bfb0bd53867f11007042dec
Yesid <yellaber@gmail.com>
2025-08-06 21:06:37 -0500

    feat(shared): agrega la lógica para el componente de navegación de categorías

--- src/app/shared/components/categories/navigation/navigation.component.ts
+++ src/app/shared/components/categories/navigation/navigation.component.ts
@@ -1,15 +1,16 @@
-import { ChangeDetectionStrategy, Component, computed, inject, input, OnInit, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, input, OnInit, signal } from '@angular/core';
 import { Route, RouterLink } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 import { RoutesService } from '@shared/services';
 
 @Component({
   selector: 'navigation',
-  imports: [RouterLink],
+  imports: [ RouterLink ],
   template: `
     <ul class="flex flex-wrap">
-      @for(route of mainRoutes(); track $index) {
+      @for(route of routesCategories(); track $index) {
         <li class="pr-5">
-          <a [routerLink]="getAbsouteRoute(route.path!)" class="text-xs lg:text-sm text-yellow-600 font-bold">
+          <a [routerLink]="route.path" class="text-xs lg:text-sm text-yellow-600 font-bold">
             {{ route.title }}
           </a>
         </li>
@@ -20,17 +21,39 @@ import { RoutesService } from '@shared/services';
 })
 export class NavigationComponent implements OnInit {
   private routesService = inject(RoutesService);
+  private translateService = inject(TranslateService);
   menuItems = input.required<string[]>();
-  menubarRoutes = signal<Route[]>([]);
-  mainRoutes = computed(() =>
-    this.menubarRoutes().filter(route => this.menuItems()?.includes(route.path!))
-  );
+  routesCategories = signal<Route[]>([]);
 
   ngOnInit() {
-    this.menubarRoutes.set(this.routesService.getRoutes());
+    this.getRoutesCategories();
   };
 
-  getAbsouteRoute(route: string) {
-    return `/${route}`;
+  private getRoutesCategories() {
+    const routes = this.routesService.getRoutes();
+    this.menuItems().forEach(path => {
+      if(routes.find(route => route.path === path)) {
+        this.saveTranslationRouteCategory(path);
+      }
+    });
+  };
+
+  private saveTranslationRouteCategory(path: string) {
+    this.translateService.get(`routes.${this.transformPath(path)}`).subscribe((category: string) =>
+      this.routesCategories.update(routes => [ ...routes, { title: category, path: `/${path}` } ]));
+  };
+
+
+  private transformPath(path: string): string {
+    if(path.includes('-')) {
+      const words = path.split('-');
+      words.forEach((word, index) => {
+        if(index > 0) {
+          words[index] = `${word.charAt(0).toUpperCase()}${word.slice(1)}`;
+        }
+      });
+      return words.toString().replaceAll(',', '');
+    }
+    return path;
   };
 }
