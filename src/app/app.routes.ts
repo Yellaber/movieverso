29ad6bf6f0d45b61d6a3fea77841f9dfb29d2257
Yesid <yellaber@gmail.com>
2025-08-07 11:55:18 -0500

    feat(routing): agrega las rutas principales de la aplicación con carga diferida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,38 +4,31 @@ import { detailMovieRoutes } from './features/detail-movie/detail-movie.routes';
 
 export const routes: Routes = [
   {
-    title: 'Inicio',
     path: '',
     loadComponent: () => import('./features/home/home.component')
   },
   ...detailMovieRoutes,
   {
-    title: 'Próximos estrenos',
     path: 'upcoming',
     loadComponent: () => import('./features/upcoming/upcoming.component')
   },
   {
-    title: 'En cartelera',
     path: 'now-playing',
     loadComponent: () => import('./features/now-playing/now-playing.component')
   },
   {
-    title: 'Populares',
     path: 'popular',
     loadComponent: () => import('./features/popular/popular.component')
   },
   {
-    title: 'Mejor valoradas',
     path: 'top-rated',
     loadComponent: () => import('./features/top-rated/top-rated.component')
   },
   {
-    title: 'En tendencia',
     path: 'trending',
     loadComponent: () => import('./features/trending/trending.component')
   },
   {
-    title: 'Buscar',
     path: 'search',
     loadComponent: () => import('./features/search/search.component')
   },
