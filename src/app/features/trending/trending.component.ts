b17a61e795e2189631ba2b03ebb63de578da4792
Yesid <yellaber@gmail.com>
2025-08-06 19:58:25 -0500

    feat(trending): agrega la lógica para la página de películas en tendencia

--- src/app/features/trending/trending.component.ts
+++ src/app/features/trending/trending.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 import { CategoriesComponent } from '@shared/components/categories/categories.component';
 import { LoadCategoryComponent } from '@shared/components/load-category/load-category.component';
 import { SeoFriendlyService } from '@app/core/services/seo-friendly.service';
 import { EndPointValid } from '@shared/interfaces';
 
-const menuItems = ['upcoming', 'now-playing', 'popular', 'top-rated'];
+const menuItems = [ 'upcoming', 'now-playing', 'popular', 'top-rated' ];
 
 @Component({
   selector: 'trending',
@@ -28,6 +29,7 @@ const menuItems = ['upcoming', 'now-playing', 'popular', 'top-rated'];
 })
 export default class TrendingComponent implements OnInit {
   private seoFriendlyService = inject(SeoFriendlyService);
+  private translateService = inject(TranslateService);
   titlePage = signal<string>('');
   textPage = signal<string>('');
   menuItems = signal<string[]>([]);
@@ -35,8 +37,14 @@ export default class TrendingComponent implements OnInit {
 
   ngOnInit() {
     this.menuItems.set(menuItems);
-    this.titlePage.set('En tendencia');
-    this.textPage.set('Lo que está en boca de todos (hoy): Películas que explotaron en búsquedas, likes y visualizaciones. Si pestañeas, te lo pierdes.');
+    this.loadTranslations();
     this.seoFriendlyService.setMetaTags(this.titlePage(), this.textPage());
+  };
+
+  private loadTranslations() {
+    this.translateService.get('routes.trending')
+      .subscribe((title: string) => this.titlePage.set(title));
+    this.translateService.get('trending.paragraph')
+      .subscribe((paragraph: string) => this.textPage.set(paragraph));
   };
 }
