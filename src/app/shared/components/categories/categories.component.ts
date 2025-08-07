e295ddad382dbd67f851ce8fcdd5d18698af4021
Yesid <yellaber@gmail.com>
2025-08-06 21:13:19 -0500

    feat(shared): agrega componente contenedor para la navegación de categorías

--- src/app/shared/components/categories/categories.component.ts
+++ src/app/shared/components/categories/categories.component.ts
@@ -1,12 +1,16 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'categories',
-  imports: [NavigationComponent],
+  imports: [
+    NavigationComponent,
+    TranslatePipe
+  ],
   template: `
     <div class="flex flex-col lg:flex-row lg:gap-5 pt-5">
-      <span class="text-xs lg:text-sm text-stone-300 font-semibold">Universos paralelos que vale la pena explorar:</span>
+      <span class="text-xs lg:text-sm text-stone-300 font-semibold">{{ 'categories.text' | translate }}</span>
       <navigation [menuItems]="menuItems()"/>
     </div>
   `,
