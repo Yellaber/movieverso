12778d19be6ef57308ee1a46acb71faf784162e1
Yesid <yellaber@gmail.com>
2025-08-08 16:09:14 -0500

    refactor(rating): refactoriza el componente para usar signals y control de flujo

--- src/app/shared/components/rating/rating.component.ts
+++ src/app/shared/components/rating/rating.component.ts
@@ -5,12 +5,12 @@ import { faFire, faStar, faThumbsUp, IconDefinition } from '@fortawesome/free-so
 
 type TypeRating = 'popularity' | 'vote_average' | 'vote_count';
 
-interface Rating {
+interface Icon {
   type: TypeRating,
   faIcon: IconDefinition
 }
 
-const ratings: Rating[] = [
+const icons: Icon[] = [
   { type: 'popularity', faIcon: faFire }, { type: 'vote_average', faIcon: faStar },
   { type: 'vote_count', faIcon: faThumbsUp }
 ];
@@ -22,9 +22,9 @@ const ratings: Rating[] = [
     FontAwesomeModule
   ],
   template: `
-    @if(getRating() && value() >= 0) {
+    @if(getIcon() && value() >= 0) {
       <a class="flex items-center gap-1">
-        <fa-icon class="text-yellow-600" [icon]="getRating()!.faIcon"></fa-icon>
+        <fa-icon class="text-yellow-600" [icon]="getIcon()!.faIcon"></fa-icon>
         @switch(type()) {
           @case('popularity') {
             <small class="text-stone-300">{{ value() | number: '1.1-1' }}</small>
@@ -44,5 +44,5 @@ const ratings: Rating[] = [
 export class RatingComponent {
   type = input.required<TypeRating>();
   value = input.required<number>();
-  getRating = computed(() => ratings.find(rating => rating.type === this.type()));
+  getIcon = computed(() => icons.find(icon => icon.type === this.type()));
 }
