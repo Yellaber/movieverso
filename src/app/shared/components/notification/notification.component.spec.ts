90a810c09499150c1bf8e61c5e1ef832d5bfc030
Yesid <yellaber@gmail.com>
2025-08-09 17:04:42 -0500

    test(notification): refactoriza las pruebas para usar @testing-library/angular

--- src/app/shared/components/notification/notification.component.spec.ts
+++ src/app/shared/components/notification/notification.component.spec.ts
@@ -1,30 +1,16 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { NotificationComponent } from "@shared/components/notification/notification.component";
+import { render, screen } from '@testing-library/angular';
+import { NotificationComponent } from '@shared/components/notification/notification.component';
 
 describe('NotificationComponent', () => {
-  let notificationComponent: NotificationComponent;
-  let fixture: ComponentFixture<NotificationComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ NotificationComponent ],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(NotificationComponent);
-    notificationComponent = fixture.componentInstance;
-  });
-
-  it('Should create the component.', () => {
-    expect(notificationComponent).toBeTruthy();
-  });
-
-  it('Should display the notification title and message correctly.', () => {
-    fixture.componentRef.setInput('notificationTitle', 'Esto es un título');
-    fixture.componentRef.setInput('message', 'Esto es un mensaje');
-    fixture.detectChanges();
-    const strongElement = fixture.nativeElement.querySelector('strong') as HTMLElement;
-    const spanElement = fixture.nativeElement.querySelector('span') as HTMLSpanElement;
-    expect(strongElement.textContent).toContain(notificationComponent.notificationTitle());
-    expect(spanElement.textContent).toContain(notificationComponent.message());
+  it('Should display the notification title in bold and the message correctly.', async() => {
+    const { container } = await render(NotificationComponent, {
+      inputs: {
+        notificationTitle: 'Esto es un título',
+        message: 'Esto es un mensaje',
+      },
+    });
+    const spanElement = container.querySelector('span');
+    expect(spanElement).toHaveTextContent('Esto es un título Esto es un mensaje');
+    expect(screen.getByText('Esto es un título').tagName).toBe('STRONG');
   });
 });
