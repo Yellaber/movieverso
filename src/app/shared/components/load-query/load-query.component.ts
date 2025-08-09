23f1a5481e94bd59e814bc011abcdba79d0820d6
Yesid <yellaber@gmail.com>
2025-08-09 18:59:27 -0500

    src/app/shared/components/load-query/load-query.component.ts componente eliminado

--- src/app/shared/components/load-query/load-query.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'load-query',
-  imports: [],
-  template: `<p>load-query works!</p>`,
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class LoadQueryComponent { }
