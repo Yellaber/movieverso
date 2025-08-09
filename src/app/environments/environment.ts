53bb1a5eae3d9f6e1ed637635d14762c2ebccea6
Yesid <yellaber@gmail.com>
2025-08-09 18:18:18 -0500

    build(env): agrega la configuración del entorno de producción con placeholders

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -4,5 +4,6 @@ export const environment = {
   tmdbApiKey: '__TMDB_API_KEY__',
   tmdbApiUrl: 'https://api.themoviedb.org/3',
   imageUrl: 'https://image.tmdb.org/t/p/original',
+  ipGeolocationApiKey: '__IPGEOLOCATION_API_KEY__',
   ipGeolocationApiUrl: 'https://api.ipgeolocation.io/v2/ipgeo'
 };
