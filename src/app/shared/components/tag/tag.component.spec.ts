393db6185e69fb1ab320cf4b5a2e662f97358c87
Yesid <yellaber@gmail.com>
2025-08-09 15:33:40 -0500

    test(tag): agrega pruebas unitarias para el componente de tag

--- src/app/shared/components/tag/tag.component.spec.ts
+++ src/app/shared/components/tag/tag.component.spec.ts
@@ -1,77 +1,79 @@
-import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { TagComponent } from './tag.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faFire, faStar, faCalendarCheck, faArrowTrendUp, faFilm } from '@fortawesome/free-solid-svg-icons';
+import { render, screen } from '@testing-library/angular';
 
 describe('TagComponent.', () => {
-  let fixture: ComponentFixture<TagComponent>;
-  let tagComponent: TagComponent;
-
-  beforeEach(async() => {
-    await TestBed.configureTestingModule({
-      imports: [ TagComponent, FontAwesomeModule ]
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(TagComponent);
-    tagComponent = fixture.componentInstance;
+  it('Should display the correct icon and text for type "now-playing."', async() => {
+    const { fixture } = await render(TagComponent, {
+      inputs: {
+        text: 'En cartelera',
+        type: 'now-playing'
+      }
+    });
+    expect(screen.getByText('En cartelera')).toBeInTheDocument();
+    expect(fixture.componentInstance.getIcon()?.faIcon).toBe(faFilm);
   });
 
-  it('Should create the Tag component.', () => {
-    expect(tagComponent).toBeTruthy();
+  it('Should display the correct icon and text for type "top-rated".', async() => {
+    const { fixture } = await render(TagComponent, {
+      inputs: {
+        text: 'Mejor valoradas',
+        type: 'top-rated'
+      }
+    });
+    expect(screen.getByText('Mejor valoradas')).toBeInTheDocument();
+    expect(fixture.componentInstance.getIcon()?.faIcon).toBe(faStar);
   });
 
-  it('getIcon() should return the correct icon for a valid type "now-playing".', fakeAsync(() => {
-    fixture.componentRef.setInput('text', 'En cartelera');
-    fixture.componentRef.setInput('type', 'now-playing');
-    fixture.detectChanges();
-    tick();
-    expect(tagComponent.getIcon()?.faIcon).toBe(faFilm);
-  }));
+  it('Should display the correct icon and text for type "trending".', async() => {
+    const { fixture } = await render(TagComponent, {
+      inputs: {
+        text: 'En tendencia',
+        type: 'trending'
+      }
+    });
+    expect(screen.getByText('En tendencia')).toBeInTheDocument();
+    expect(fixture.componentInstance.getIcon()?.faIcon).toBe(faArrowTrendUp);
+  });
 
-  it('getIcon() should return the correct icon for a valid type "top-rated".', fakeAsync(() => {
-    fixture.componentRef.setInput('text', 'Mejor valoradas');
-    fixture.componentRef.setInput('type', 'top-rated');
-    fixture.detectChanges();
-    tick();
-    expect(tagComponent.getIcon()?.faIcon).toBe(faStar);
-  }));
+  it('Should display the correct icon and text for type "popular".', async() => {
+    const { fixture } = await render(TagComponent, {
+      inputs: {
+        text: 'Más populares',
+        type: 'popular'
+      }
+    });
+    expect(screen.getByText('Más populares')).toBeInTheDocument();
+    expect(fixture.componentInstance.getIcon()?.faIcon).toBe(faFire);
+  });
 
-  it('getIcon() should return the correct icon for a valid type "trending".', fakeAsync(() => {
-    fixture.componentRef.setInput('text', 'En tendencia');
-    fixture.componentRef.setInput('type', 'trending');
-    fixture.detectChanges();
-    tick();
-    expect(tagComponent.getIcon()?.faIcon).toBe(faArrowTrendUp);
-  }));
+  it('Should display the correct icon and text for type "calendar".', async() => {
+    const { fixture } = await render(TagComponent, {
+      inputs: {
+        text: '2025-08-01',
+        type: 'calendar'
+      }
+    });
+    expect(screen.getByText('2025-08-01')).toBeInTheDocument();
+    expect(fixture.componentInstance.getIcon()?.faIcon).toBe(faCalendarCheck);
+  });
 
-  it('getIcon() should return the correct icon for a valid type "popular".', fakeAsync(() => {
-    fixture.componentRef.setInput('text', 'Más populares');
-    fixture.componentRef.setInput('type', 'popular');
-    fixture.detectChanges();
-    tick();
-    expect(tagComponent.getIcon()?.faIcon).toBe(faFire);
-  }));
+  it('Should not render when type input is not provided.', async() => {
+    const { container } = await render(TagComponent, {
+      inputs: {
+        text: 'En cartelera',
+      }
+    });
+    expect(container.querySelector('div')).toBeNull();
+  });
 
-  it('getIcon() should return the correct icon for a valid type "calendar".', fakeAsync(() => {
-    fixture.componentRef.setInput('text', 'Próximamente');
-    fixture.componentRef.setInput('type', 'calendar');
-    fixture.detectChanges();
-    tick();
-    expect(tagComponent.getIcon()?.faIcon).toBe(faCalendarCheck);
-  }));
-
-  it('getIcon() should be undefined when type input is not provided.', fakeAsync(() => {
-    fixture.componentRef.setInput('text', 'En cartelera');
-    fixture.detectChanges();
-    tick();
-    expect(tagComponent.getIcon()).toBeUndefined();
-  }));
-
-  it('getIcon() should be undefined when type input is invalid.', fakeAsync(() => {
-    fixture.componentRef.setInput('text', 'Tipo inválido');
-    fixture.componentRef.setInput('type', 'invalid-type');
-    fixture.detectChanges();
-    tick();
-    expect(tagComponent.getIcon()).toBeUndefined();
-  }));
+  it('Should not render when type input is invalid.', async() => {
+    const { container } = await render(TagComponent, {
+      inputs: {
+        text: 'Tipo inválido',
+        type: 'invalid-type'
+      }
+    });
+    expect(container.querySelector('div')).toBeNull();
+  });
 });
