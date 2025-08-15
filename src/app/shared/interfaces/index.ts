b98b74ea389ed06eb2c6d1add9d37edfbe2fdd59
Yesid <yellaber@gmail.com>
2025-08-15 12:48:19 -0500

    chore(shared): crea un archivo barrel para las interfaces

--- src/app/shared/interfaces/index.ts
+++ src/app/shared/interfaces/index.ts
@@ -1,6 +1,7 @@
 export * from './carousel-config.interface';
 export * from './category.interface';
 export * from './detail-movie-response.interface';
+export * from './direction.interface';
 export * from './endpoint-valid.interface';
 export * from './genre-movies-response.interface';
 export * from './movie-collection-response.interface';
@@ -13,3 +14,4 @@ export * from './option-dropdown.interface';
 export * from './query-params.interface';
 export * from './sectionMovie.interface';
 export * from './user-geolocation.interface';
+
