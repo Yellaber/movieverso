5648619fe3836688eec34fb18f43456abe3a6f1c
Yesid <yellaber@gmail.com>
2025-08-15 23:13:48 -0500

    chore(testing): agrega mocks para ngx-translate (Pipe y Service)

---
+++ src/app/testing/mock-translate.ts
@@ -0,0 +1,22 @@
+import { Injectable, Pipe, PipeTransform } from '@angular/core';
+import { of } from 'rxjs';
+
+@Pipe({ name: 'translate' })
+export class MockTranslatePipe implements PipeTransform {
+  transform(value: string): string {
+    return value;
+  };
+};
+
+@Injectable()
+export class MockTranslateService {
+  addLangs = jest.fn();
+  setDefaultLang = jest.fn();
+  use = jest.fn().mockReturnValue(of('es'));
+  getLangs = jest.fn().mockReturnValue(['es', 'en']);
+  get = jest.fn().mockImplementation((key: string) => of(`translated:${key}`));
+  instant = jest.fn((key: string) => key);
+  onLangChange = of({ lang: 'en' });
+  onTranslationChange = of();
+  onDefaultLangChange = of();
+};
