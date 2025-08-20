d2a42a523313367916d256825363b6ed76cdfebb
Yesid <yellaber@gmail.com>
2025-08-19 21:58:41 -0500

    refactor(testing): mejora el stub de CarruselControl para emitir eventos

---
+++ src/app/testing/stubs-components.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'carrusel-title',
+  template: `<li>Stub Carrusel Title Component</li>`
+})
+export class StubCarruselTitleComponent {};
+
+@Component({
+  selector: 'carrusel-card-movies',
+  template: `<li>Stub Carrusel Card Movies Component</li>`
+})
+export class StubCarruselCardMoviesComponent {};
+
+@Component({
+  selector: 'carrusel-control',
+  template: `<li>Stub Carrusel Control Component</li>`
+})
+export class StubCarruselControlComponent {};
