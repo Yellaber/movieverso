1610e358f029ed26f3e735f3e7fefca872c09b48
Yesid <yellaber@gmail.com>
2025-08-19 22:00:14 -0500

    chore(testing): crea un archivo barrel para las utilidades de prueba

--- src/app/testing/index.ts
+++ src/app/testing/index.ts
@@ -1,4 +1,6 @@
+export * from './mock-carousel-config';
 export * from './mock-geolocation';
 export * from './mock-movie-response';
 export * from './mock-route';
 export * from './mock-translate';
+export * from './stubs-components';
