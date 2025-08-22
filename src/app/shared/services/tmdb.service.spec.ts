05d642e1b83f1d7e44ca018ed9d4aa8032e44b97
Yesid <yellaber@gmail.com>
2025-08-22 13:57:11 -0500

    test(shared): agrega prueba para exponer bug de estado en TmdbService

--- src/app/shared/services/tmdb.service.spec.ts
+++ src/app/shared/services/tmdb.service.spec.ts
@@ -4,164 +4,8 @@ import { HttpTestingController, provideHttpClientTesting } from '@angular/common
 import { TmdbService } from './tmdb.service';
 import { UserGeolocationService } from '../../core/services/user-geolocation.service';
 import { environment } from '@environments/environment.developments';
-import { Genre, GenreMoviesResponse, MovieResponse, DetailMovieResponse, UserGeolocation, Movie } from '@shared/interfaces';
-
-const mockGeolocation: UserGeolocation = {
-  ip: '1.2.3.4',
-  location: {
-    continent_code: 'SA',
-    continent_name: 'Sounth America',
-    country_code2: 'CO',
-    country_code3: 'COL',
-    country_name: 'Colombia',
-    country_name_official: 'Republic of Colombia',
-    country_capital: 'Bogota',
-    state_prov: 'Bolivar',
-    state_code: 'CO-BOL',
-    district: 'Cartagena Province',
-    city: 'Cartagena',
-    zipcode: '130008',
-    latitude: '10.48366',
-    longitude: '-75.45778',
-    is_eu: false,
-    country_flag: 'https://ipgeolocation.io/static/flags/co_64.png',
-    geoname_id: '3830557',
-    country_emoji: 'co'
-  },
-  country_metadata: {
-    calling_code: '+57',
-    tld: '.co',
-    languages: ['es-CO']
-  },
-  currency: {
-    code: 'COP',
-    name: 'Colombian Peso',
-    symbol: '$'
-  }
-};
-
-const mockMovies: Movie[] = [
-  {
-    adult: false,
-    backdrop_path: '/7ONMDhnErvpkKvkZqM82ud7bzcT.jpg',
-    genre_ids: [ 28, 12, 53 ],
-    id: 575265,
-    original_language: 'en',
-    original_title: 'Mission: Impossible - The Final Reckoning',
-    overview: "Ethan Hunt and the IMF team continue their search for the terrifying AI known as the Entity — which has infiltrated intelligence networks all over the globe — with the world's governments and a mysterious ghost from Ethan's past on their trail. Joined by new allies and armed with the means to shut the Entity down for good, Hunt is in a race against time to prevent the world as we know it from changing forever.",
-    popularity: 205.7232,
-    poster_path: '/z53D72EAOxGRqdr7KXXWp9dJiDe.jpg',
-    release_date: new Date( '2025-05-17' ),
-    title: 'Mission: Impossible - The Final Reckoning',
-    video: false,
-    vote_average: 7.393,
-    vote_count: 89
-  },
-  {
-    adult: false,
-    backdrop_path: '/j0NUh5irX7q2jIRtbLo8TZyRn6y.jpg',
-    genre_ids: [ 27, 9648 ],
-    id: 574475,
-    original_language: 'en',
-    original_title: 'Final Destination Bloodlines',
-    overview: 'Plagued by a violent recurring nightmare, college student Stefanie heads home to track down the one person who might be able to break the cycle and save her family from the grisly demise that inevitably awaits them all.',
-    popularity: 503.2304,
-    poster_path: '/6WxhEvFsauuACfv8HyoVX6mZKFj.jpg',
-    release_date: new Date( '2025-05-09' ),
-    title: 'Final Destination Bloodlines',
-    video: false,
-    vote_average: 7.113,
-    vote_count: 257
-  },
-  {
-    adult: false,
-    backdrop_path: '/4A5HH9HkCPqAwyYL6CnA0mxbYjn.jpg',
-    genre_ids: [ 28, 80, 53, 18 ],
-    id: 1144430,
-    original_language: 'fr',
-    original_title: 'Balle perdue 3',
-    overview: 'Car genius Lino returns to conclude his vendetta against Areski and the corrupt commander who ruined their lives in this turbo-charged trilogy finale.',
-    popularity: 179.5989,
-    poster_path: '/lCI5y1KkjgMoTkZiCleqji00XeE.jpg',
-    release_date: new Date( '2025-05-06' ),
-    title: 'Last Bullet',
-    video: false,
-    vote_average: 6.7,
-    vote_count: 119
-  },
-  {
-    adult: false,
-    backdrop_path: '/rthMuZfFv4fqEU4JVbgSW9wQ8rs.jpg',
-    genre_ids: [ 28, 878, 12 ],
-    id: 986056,
-    original_language: 'en',
-    original_title: 'Thunderbolts*',
-    overview: 'After finding themselves ensnared in a death trap, seven disillusioned castoffs must embark on a dangerous mission that will force them to confront the darkest corners of their pasts.',
-    popularity: 195.4565,
-    poster_path: '/m9EtP1Yrzv6v7dMaC9mRaGhd1um.jpg',
-    release_date: new Date( '2025-04-30' ),
-    title: 'Thunderbolts*',
-    video: false,
-    vote_average: 7.471,
-    vote_count: 920
-  },
-  {
-    adult: false,
-    backdrop_path: '/bVm6udIB6iKsRqgMdQh6HywuEBj.jpg',
-    genre_ids: [ 53, 28 ],
-    id: 1233069,
-    original_language: 'de',
-    original_title: 'Exterritorial',
-    overview: 'When her son vanishes inside a US consulate, ex-special forces soldier Sara does everything in her power to find him — and uncovers a dark conspiracy.',
-    popularity: 176.2712,
-    poster_path: '/jM2uqCZNKbiyStyzXOERpMqAbdx.jpg',
-    release_date: new Date( '2025-04-29' ),
-    title: 'Exterritorial',
-    video: false,
-    vote_average: 6.6,
-    vote_count: 354
-  }
-];
-
-const mockMovieResponse: MovieResponse = {
-  page: 1,
-  results: mockMovies,
-  total_pages: 1,
-  total_results: mockMovies.length
-};
-
-const mockGenres: Genre[] = [{ id: 28, name: 'Action' }, { id: 12, name: 'Adventure' }, { id: 35, name: 'Comedy' }];
-
-const mockGenreMoviesResponse: GenreMoviesResponse = { genres: mockGenres };
-
-const mockDetailMovieResponse: DetailMovieResponse = {
-  id: 123,
-  title: 'Mock Movie Detail',
-  overview: 'A detailed overview.',
-  release_date: new Date( '2025-01-01' ),
-  genres: [{ id: 28, name: 'Action' }],
-  backdrop_path: '/backdrop.jpg',
-  poster_path: '/poster.jpg',
-  popularity: 123.45,
-  vote_average: 8.5,
-  vote_count: 1000,
-  tagline: 'An amazing tagline.',
-  status: 'Released',
-  production_companies: [],
-  production_countries: [],
-  spoken_languages: [],
-  belongs_to_collection: {} as any,
-  budget: 1000000,
-  homepage: 'http://example.com',
-  imdb_id: 'tt1234567',
-  original_language: 'en',
-  original_title: 'Mock Movie Detail',
-  revenue: 2000000,
-  runtime: 120,
-  origin_country: [ 'US' ],
-  video: false,
-  adult: false
-};
+import { Genre, MovieResponse, DetailMovieResponse } from '@shared/interfaces';
+import { mockDetailMovieResponse, mockGenreMoviesResponse, mockGeolocation, mockMovieResponse } from '@app/testing';
 
 describe('TmdbService', () => {
   let service: TmdbService;
@@ -177,10 +21,7 @@ describe('TmdbService', () => {
         provideHttpClient(),
         provideHttpClientTesting(),
         TmdbService,
-        {
-          provide: UserGeolocationService,
-          useValue: userGeolocationServiceMock
-        }
+        { provide: UserGeolocationService, useValue: userGeolocationServiceMock }
       ]
     });
     service = TestBed.inject(TmdbService);
@@ -230,8 +71,8 @@ describe('TmdbService', () => {
 
   describe('getGenreMovieListByIds', () => {
     it('Should fetch and filter genres by ids.', () => {
-      const genreIds = [28, 35];
-      const expectedGenres = [{ id: 28, name: 'Action' }, { id: 35, name: 'Comedy' }];
+      const genreIds = [1, 3];
+      const expectedGenres = [{ id: 1, name: 'Genre 1' }, { id: 3, name: 'Genre 3' }];
       let genresResponse: Genre[] | undefined;
       service.getGenreMovieListByIds(genreIds).subscribe(response => { genresResponse = response; });
       const req = httpMock.expectOne(`${environment.tmdbApiUrl}/genre/movie/list?api_key=${environment.tmdbApiKey}&language=es-CO`);
@@ -240,8 +81,8 @@ describe('TmdbService', () => {
     });
 
     it('Should return genres from cache on second call.', () => {
-      const genreIds = [28, 35];
-      const expectedGenres = [{ id: 28, name: 'Action' }, { id: 35, name: 'Comedy' }];
+      const genreIds = [1, 3];
+      const expectedGenres = [{ id: 1, name: 'Genre 1' }, { id: 3, name: 'Genre 3' }];
       service.getGenreMovieListByIds(genreIds).subscribe();
       const req = httpMock.expectOne(`${environment.tmdbApiUrl}/genre/movie/list?api_key=${environment.tmdbApiKey}&language=es-CO`);
       req.flush(mockGenreMoviesResponse);
@@ -292,7 +133,7 @@ describe('TmdbService', () => {
       let genresResponse: Genre[] | undefined;
       service.getGenreMovieList().subscribe(response => { genresResponse = response; });
       httpMock.expectNone(`${environment.tmdbApiUrl}/genre/movie/list?api_key=${environment.tmdbApiKey}&language=es-CO`);
-      expect(genresResponse).toEqual(mockGenres);
+      expect(genresResponse).toEqual(mockGenreMoviesResponse.genres);
     });
 
     it('Should handle empty genre list.', () => {
