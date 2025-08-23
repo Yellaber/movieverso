5c12f048b73bdd1392f58febf25b8dea0dc62da1
Yesid <yellaber@gmail.com>
2025-08-22 22:42:05 -0500

    refactor(testing): extrae los datos de prueba de MockQueryParamsService

---
+++ src/app/testing/mock-queryparams-service.ts
@@ -0,0 +1,20 @@
+import { QueryParams } from '@shared/interfaces';
+
+export const mockQueryParams: QueryParams = {
+  primaryReleaseDateGte: '2025-01-01',
+  primaryReleaseDateLte: '2025-02-01',
+  voteAverageGte: 8,
+  voteCountGte: 200,
+  sortBy: 'vote_average.desc',
+  withGenres: '1,2,3'
+};
+
+export class MockQueryParamsService {
+  getQueryParams = jest.fn().mockReturnValue(mockQueryParams);
+  set = jest.fn();
+};
+
+export class MockQueryParamsServiceEmpty {
+  getQueryParams = jest.fn().mockReturnValue(undefined);
+  set = jest.fn();
+};
