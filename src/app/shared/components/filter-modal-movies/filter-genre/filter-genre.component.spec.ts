b554df0c91058a43a13f61852d708aa9ad6ee72c
Yesid <yellaber@gmail.com>
2025-08-25 14:11:29 -0500

    test(shared): agrega pruebas exhaustivas para FilterGenreComponent

--- src/app/shared/components/filter-modal-movies/filter-genre/filter-genre.component.spec.ts
+++ src/app/shared/components/filter-modal-movies/filter-genre/filter-genre.component.spec.ts
@@ -1,107 +1,122 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { FilterGenreComponent } from './filter-genre.component';
-import { TmdbService } from '@shared/services';
-import { mockGenreMoviesResponse, MockTmdbService, MockTranslatePipe, MockTranslateService } from '@app/testing';
+import { QueryParamsService, TmdbService } from '@shared/services';
+import { mockGenreMoviesResponse, mockQueryParams, MockQueryParamsService, MockQueryParamsServiceEmpty, MockTmdbService, MockTranslatePipe, MockTranslateService } from '@app/testing';
 
 describe('FilterGenreComponent.', () => {
   let fixture: ComponentFixture<FilterGenreComponent>;
   let component: FilterGenreComponent;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [ FilterGenreComponent ],
-      providers: [
-        { provide: TmdbService, useClass: MockTmdbService },
-        { provide: TranslateService, useClass: MockTranslateService }
-      ]
-    })
-    .overrideComponent(FilterGenreComponent, {
-      remove: { imports: [ TranslatePipe ] },
-      add: { imports: [ MockTranslatePipe ] },
-    })
-    .compileComponents();
+  describe('When the queryParams is present.', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [ FilterGenreComponent ],
+        providers: [
+          { provide: TmdbService, useClass: MockTmdbService },
+          { provide: TranslateService, useClass: MockTranslateService },
+          { provide: QueryParamsService, useClass: MockQueryParamsService }
+        ]
+      })
+      .overrideComponent(FilterGenreComponent, {
+        remove: { imports: [ TranslatePipe ] },
+        add: { imports: [ MockTranslatePipe ] },
+      })
+      .compileComponents();
 
-    fixture = TestBed.createComponent(FilterGenreComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      fixture = TestBed.createComponent(FilterGenreComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it('Should render the component and select the genre present in the queryParams.', () => {
+      const spanElement = fixture.nativeElement.querySelector('span');
+      const buttonElements = fixture.nativeElement.querySelectorAll('button');
+      expect(spanElement).toBeTruthy();
+      expect(buttonElements.length).toBe(mockGenreMoviesResponse.genres.length);
+      expect(component.genresIdSelected()).toEqual(mockQueryParams.withGenres);
+      expect(component.isSelected(mockGenreMoviesResponse.genres[0])).toBeTruthy();
+      expect(buttonElements[0].textContent.trim()).toBe(mockGenreMoviesResponse.genres[0].name);
+      expect(buttonElements[0].classList.contains('bg-yellow-600')).toBeTruthy();
+      expect(buttonElements[0].classList.contains('font-bold')).toBeTruthy();
+    });
+
+    it('Should select another genre.', () => {
+      const buttonElements = fixture.nativeElement.querySelectorAll('button');
+      buttonElements[1].click();
+      fixture.detectChanges();
+      const currentGenresIds = `${mockGenreMoviesResponse.genres[0].id.toString()},${mockGenreMoviesResponse.genres[1].id.toString()}`;
+      expect(component.genresIdSelected()).toEqual(currentGenresIds);
+      expect(component.isSelected(mockGenreMoviesResponse.genres[0])).toBeTruthy();
+      expect(buttonElements[0].textContent.trim()).toBe(mockGenreMoviesResponse.genres[0].name);
+      expect(buttonElements[0].classList.contains('bg-yellow-600')).toBeTruthy();
+      expect(buttonElements[0].classList.contains('font-bold')).toBeTruthy();
+      expect(component.isSelected(mockGenreMoviesResponse.genres[1])).toBeTruthy();
+      expect(buttonElements[1].textContent.trim()).toBe(mockGenreMoviesResponse.genres[1].name);
+      expect(buttonElements[1].classList.contains('bg-yellow-600')).toBeTruthy();
+      expect(buttonElements[1].classList.contains('font-bold')).toBeTruthy();
+    });
+
+    it('Should deselect the genre present in the queryParams.', () => {
+      const buttonElements = fixture.nativeElement.querySelectorAll('button');
+      buttonElements[0].click();
+      fixture.detectChanges();
+      expect(component.genresIdSelected()).toEqual('');
+      expect(component.isSelected(mockGenreMoviesResponse.genres[0])).toBeFalsy();
+      expect(buttonElements[0].textContent.trim()).toBe(mockGenreMoviesResponse.genres[0].name);
+      expect(buttonElements[0].classList.contains('bg-stone-400')).toBeTruthy();
+      expect(buttonElements[0].classList.contains('text-stone-700')).toBeTruthy();
+    });
+
+    it('Should reset the genres selected.', () => {
+      const buttonElements = fixture.nativeElement.querySelectorAll('button');
+      fixture.detectChanges();
+      expect(component.genresIdSelected()).toEqual(mockGenreMoviesResponse.genres[0].id.toString());
+
+      component.reset();
+      fixture.detectChanges();
+      expect(component.genresIdSelected()).toEqual('');
+      expect(component.isSelected(mockGenreMoviesResponse.genres[0])).toBeFalsy();
+      expect(buttonElements[0].classList.contains('bg-stone-400')).toBeTruthy();
+      expect(buttonElements[0].classList.contains('text-stone-700')).toBeTruthy();
+    });
   });
 
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+  describe('When the queryParams is not present.', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [ FilterGenreComponent ],
+        providers: [
+          { provide: TmdbService, useClass: MockTmdbService },
+          { provide: TranslateService, useClass: MockTranslateService },
+          { provide: QueryParamsService, useClass: MockQueryParamsServiceEmpty }
+        ]
+      })
+      .overrideComponent(FilterGenreComponent, {
+        remove: { imports: [ TranslatePipe ] },
+        add: { imports: [ MockTranslatePipe ] },
+      })
+      .compileComponents();
 
-  it('Should render the component correctly.', () => {
-    const spanElement = fixture.nativeElement.querySelector('span');
-    const buttonElements = fixture.nativeElement.querySelectorAll('button');
-    expect(component.genres()).toEqual(mockGenreMoviesResponse.genres);
-    expect(spanElement).toBeTruthy();
-    expect(buttonElements.length).toBe(mockGenreMoviesResponse.genres.length);
-  });
+      fixture = TestBed.createComponent(FilterGenreComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
 
-  it('Should select a genre.', () => {
-    const buttonElements = fixture.nativeElement.querySelectorAll('button');
-    buttonElements[0].click();
-    fixture.detectChanges();
-    expect(component.genresIdSelected()).toEqual(mockGenreMoviesResponse.genres[0].id.toString());
-    expect(buttonElements[0].textContent.trim()).toBe(mockGenreMoviesResponse.genres[0].name);
-    expect(buttonElements[0].classList.contains('bg-yellow-600')).toBeTruthy();
-    expect(buttonElements[0].classList.contains('font-bold')).toBeTruthy();
-  });
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
 
-  it('Should deselect a genre.', () => {
-    const buttonElements = fixture.nativeElement.querySelectorAll('button');
-    buttonElements[0].click();
-    fixture.detectChanges();
-    expect(component.genresIdSelected()).toEqual(mockGenreMoviesResponse.genres[0].id.toString());
-    expect(buttonElements[0].textContent.trim()).toBe(mockGenreMoviesResponse.genres[0].name);
-    expect(buttonElements[0].classList.contains('bg-yellow-600')).toBeTruthy();
-    expect(buttonElements[0].classList.contains('font-bold')).toBeTruthy();
-
-    buttonElements[0].click();
-    fixture.detectChanges();
-    expect(component.genresIdSelected()).toEqual('');
-    expect(buttonElements[0].textContent.trim()).toBe(mockGenreMoviesResponse.genres[0].name);
-    expect(buttonElements[0].classList.contains('bg-stone-400')).toBeTruthy();
-    expect(buttonElements[0].classList.contains('text-stone-700')).toBeTruthy();
-  });
-
-  it('Should select and deselect multiple genres.', () => {
-    const buttonElements = fixture.nativeElement.querySelectorAll('button');
-    buttonElements[0].click();
-    fixture.detectChanges();
-    expect(component.genresIdSelected()).toEqual(mockGenreMoviesResponse.genres[0].id.toString());
-    expect(component.isSelected(mockGenreMoviesResponse.genres[0])).toBeTruthy();
-    expect(buttonElements[0].classList.contains('bg-yellow-600')).toBeTruthy();
-    expect(buttonElements[0].classList.contains('font-bold')).toBeTruthy();
-
-    buttonElements[1].click();
-    fixture.detectChanges();
-    expect(component.genresIdSelected()).toEqual(`${mockGenreMoviesResponse.genres[0].id},${mockGenreMoviesResponse.genres[1].id}`);
-    expect(component.isSelected(mockGenreMoviesResponse.genres[1])).toBeTruthy();
-    expect(buttonElements[1].classList.contains('bg-yellow-600')).toBeTruthy();
-    expect(buttonElements[1].classList.contains('font-bold')).toBeTruthy();
-
-    buttonElements[0].click();
-    fixture.detectChanges();
-    expect(component.genresIdSelected()).toEqual(mockGenreMoviesResponse.genres[1].id.toString());
-    expect(component.isSelected(mockGenreMoviesResponse.genres[0])).toBeFalsy();
-    expect(buttonElements[0].classList.contains('bg-stone-400')).toBeTruthy();
-    expect(buttonElements[0].classList.contains('text-stone-700')).toBeTruthy();
-    expect(component.isSelected(mockGenreMoviesResponse.genres[1])).toBeTruthy();
-    expect(buttonElements[1].classList.contains('bg-yellow-600')).toBeTruthy();
-    expect(buttonElements[1].classList.contains('font-bold')).toBeTruthy();
-  });
-
-  it('Should reset selected genres.', () => {
-    const buttonElements = fixture.nativeElement.querySelectorAll('button');
-    buttonElements[0].click();
-    fixture.detectChanges();
-    expect(component.genresIdSelected()).not.toEqual('');
-
-    component.reset();
-    fixture.detectChanges();
-    expect(component.genresIdSelected()).toEqual('');
-    expect(component.isSelected(mockGenreMoviesResponse.genres[0])).toBeFalsy();
+    it('Should render the component and not select any genre.', () => {
+      const spanElement = fixture.nativeElement.querySelector('span');
+      const buttonElements = fixture.nativeElement.querySelectorAll('button');
+      expect(spanElement).toBeTruthy();
+      expect(buttonElements.length).toBe(mockGenreMoviesResponse.genres.length);
+      expect(component.genresIdSelected()).toEqual('');
+    });
   });
 });
