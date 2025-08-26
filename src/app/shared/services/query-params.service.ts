64a3a2166caa288e7858c1c838ec7f5bd861a781
Yesid <yellaber@gmail.com>
2025-08-26 00:17:41 -0500

    refactor(shared): establece un estado inicial no nulo para QueryParamsService

--- src/app/shared/services/query-params.service.ts
+++ src/app/shared/services/query-params.service.ts
@@ -1,11 +1,21 @@
 import { computed, Injectable, signal } from '@angular/core';
 import { QueryParams } from '@shared/interfaces';
 
+export const initialQueryParams: QueryParams = {
+  primaryReleaseDateGte: '',
+  primaryReleaseDateLte: '',
+  query: '',
+  sortBy: 'popularity.desc',
+  voteAverageGte: 7,
+  voteCountGte: 100,
+  withGenres: '',
+};
+
 @Injectable({
   providedIn: 'root'
 })
 export class QueryParamsService {
-  private queryParams = signal<QueryParams | undefined>(undefined);
+  private queryParams = signal<QueryParams>(initialQueryParams);
   getQueryParams = computed(() => this.queryParams());
 
   set(queryParams: QueryParams) {
