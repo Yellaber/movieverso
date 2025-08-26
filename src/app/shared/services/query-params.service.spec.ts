ab8b3ec53554426c3ae0b8c38829ac666d954db0
Yesid <yellaber@gmail.com>
2025-08-26 00:37:42 -0500

    refactor(shared): simplifica las pruebas de QueryParamsService

--- src/app/shared/services/query-params.service.spec.ts
+++ src/app/shared/services/query-params.service.spec.ts
@@ -1,50 +1,28 @@
 import { TestBed } from '@angular/core/testing';
-import { QueryParamsService } from './query-params.service';
-import { QueryParams } from '@shared/interfaces';
+import { initialQueryParams, QueryParamsService } from './query-params.service';
+import { mockQueryParams } from '@app/testing';
 
 describe('QueryParamsService', () => {
   let service: QueryParamsService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [QueryParamsService]
+      providers: [ QueryParamsService ]
     });
     service = TestBed.inject(QueryParamsService);
   });
 
-  it('Should be created.', () => {
+  it('Should be created and have an initial value.', () => {
     expect(service).toBeTruthy();
+    expect(service.getQueryParams()).toBe(initialQueryParams);
   });
 
-  it('Should have an initial value of undefined.', () => {
-    expect(service.getQueryParams()).toBeUndefined();
+  it('Should update the queryParams when set is called.', () => {
+    service.set(mockQueryParams);
+    expect(service.getQueryParams()).toBe(mockQueryParams);
   });
 
-  it('Should set and get query params correctly.', () => {
-    const mockParams: QueryParams = {
-      query: 'Inception',
-      sortBy: 'popularity.desc',
-      withGenres: '28,878'
-    };
-    service.set(mockParams);
-    expect(service.getQueryParams()).toEqual(mockParams);
-  });
-
-  it('Should update query params when set is called again.', () => {
-    const initialParams: QueryParams = { query: 'Inception' };
-    service.set(initialParams);
-    expect(service.getQueryParams()).toEqual(initialParams);
-
-    const updatedParams: QueryParams = {
-      query: 'Dune',
-      sortBy: 'primary_release_date.desc'
-    };
-
-    service.set(updatedParams);
-    expect(service.getQueryParams()).toEqual(updatedParams);
-  });
-
-  it('Should return a readonly signal.', () => {
+  it('Should return a readonly signal from getQueryParams.', () => {
     const readonlySignal = service.getQueryParams;
     expect((readonlySignal as any).set).toBeUndefined();
     expect((readonlySignal as any).update).toBeUndefined();
