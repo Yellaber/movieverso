5246489b7e503c2107316a11dffb3ffaeaaf27e3
Yesid <yellaber@gmail.com>
2025-08-25 23:19:14 -0500

    fix(shared): elimina tipo duplicado en la interfaz QueryParams

--- src/app/shared/interfaces/query-params.interface.ts
+++ src/app/shared/interfaces/query-params.interface.ts
@@ -1,15 +1,13 @@
-
 export type TypeSort = 'popularity.desc' | 'popularity.asc' | 'vote_average.desc' | 'vote_average.asc' |
-'vote_count.desc' | 'vote_count.asc' | 'vote_count.desc' | 'primary_release_date.desc' |
+'vote_count.desc' | 'vote_count.asc' | 'primary_release_date.desc' |
 'primary_release_date.asc' | 'title.asc' | 'title.desc';
 
 export interface QueryParams {
-  primaryReleaseDateGte?: string,
-  primaryReleaseDateLte?: string,
-  releaseDateGte?: string,
-  voteAverageGte?: number,
-  voteCountGte?: number,
-  sortBy?: TypeSort,
-  withGenres?: string,
-  query?: string
+  primaryReleaseDateGte: string,
+  primaryReleaseDateLte: string,
+  query: string
+  sortBy: TypeSort,
+  voteAverageGte: number,
+  voteCountGte: number,
+  withGenres: string,
 };
