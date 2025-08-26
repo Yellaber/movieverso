b2a342c730b619274c0c866fd29bebe3fff858cb
Yesid <yellaber@gmail.com>
2025-08-26 17:40:18 -0500

    feat(shared): crea ActiveActionService para gestionar el estado de la acción del usuario

--- src/app/shared/services/active-action.service.ts
+++ src/app/shared/services/active-action.service.ts
@@ -1,15 +1,15 @@
 import { computed, Injectable, signal } from '@angular/core';
 
-type ActiveAction = 'filter' | 'search';
+type ActiveAction = 'filter' | 'search' | '';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActiveActionService {
-  private activeAction = signal<ActiveAction | undefined>(undefined);
+  private activeAction = signal<ActiveAction>('');
   getActiveAction = computed(() => this.activeAction());
 
   set(activeAction: ActiveAction) {
     this.activeAction.set(activeAction);
   };
-}
+};
