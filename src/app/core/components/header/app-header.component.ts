4c694bda3733e2fe0a7b1144a9c5f49fd449ad52
Yesid <yellaber@gmail.com>
2025-08-27 16:21:24 -0500

    refactor(header): hace que AppHeaderComponent sea reactivo al estado de geolocalización

--- src/app/core/components/header/app-header.component.ts
+++ src/app/core/components/header/app-header.component.ts
@@ -1,18 +1,15 @@
-import { ChangeDetectionStrategy, Component, inject, signal, viewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslatePipe } from "@ngx-translate/core";
 import { LogoComponent } from '@app/core/components/header/logo/logo.component';
 import { SearchBarComponent } from '@app/core/components/header/search-bar/search-bar.component';
-import { FilterModalMoviesComponent } from '@shared/components/filter-modal-movies/filter-modal-movies.component';
 import { UserGeolocationService } from '@app/core/services';
-import { UserGeolocation } from '@shared/interfaces';
 
 @Component({
   selector: 'app-header',
   imports: [
     LogoComponent,
     SearchBarComponent,
-    FilterModalMoviesComponent,
     TranslatePipe
   ],
   templateUrl: './app-header.component.html',
@@ -22,25 +19,9 @@ import { UserGeolocation } from '@shared/interfaces';
 export class AppHeaderComponent {
   private router = inject(Router);
   private userGeolocationService = inject(UserGeolocationService);
-  searchBarRef = viewChild(SearchBarComponent);
-  userGeolocation = signal<UserGeolocation | undefined>(undefined);
-
-  ngOnInit() {
-    this.initUserLocation();
-  };
-
-  private initUserLocation() {
-    const userGeolocation = this.userGeolocationService.getUserGeolocation();
-    if(userGeolocation) {
-      this.userGeolocation.set(userGeolocation);
-    }
-  };
-
-  onCloseModal() {
-    this.searchBarRef()?.isOpenedFilter.set(false);
-  };
+  userGeolocation = this.userGeolocationService.getUserGeolocation;
 
   navigateToUpcoming() {
     this.router.navigateByUrl('/upcoming');
   };
-}
+};
