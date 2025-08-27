638469256931b2a89db9bfb9adf1e143b41d7e45
Yesid <yellaber@gmail.com>
2025-08-27 16:49:54 -0500

    test(core): verifica los efectos secundarios en UserGeolocationService

--- src/app/core/services/user-geolocation.service.spec.ts
+++ src/app/core/services/user-geolocation.service.spec.ts
@@ -6,50 +6,11 @@ import { UserGeolocationService } from './user-geolocation.service';
 import { PlatformService } from '@shared/services';
 import { UserGeolocation } from '@shared/interfaces';
 import { environment } from '@app/environments/environment.developments';
-
-const mockGeolocation: UserGeolocation = {
-  ip: '1.2.3.4',
-  location: {
-    continent_code: 'SA',
-    continent_name: 'Sounth America',
-    country_code2: 'CO',
-    country_code3: 'COL',
-    country_name: 'Colombia',
-    country_name_official: 'Republic of Colombia',
-    country_capital: 'Bogota',
-    state_prov: 'Bolivar',
-    state_code: 'CO-BOL',
-    district: 'Cartagena Province',
-    city: 'Cartagena',
-    zipcode: '130008',
-    latitude: '10.48366',
-    longitude: '-75.45778',
-    is_eu: false,
-    country_flag: 'https://ipgeolocation.io/static/flags/co_64.png',
-    geoname_id: '3830557',
-    country_emoji: 'co'
-  },
-  country_metadata: {
-    calling_code: '+57',
-    tld: '.co',
-    languages: ['es-CO']
-  },
-  currency: {
-    code: 'COP',
-    name: 'Colombian Peso',
-    symbol: '$'
-  }
-};
+import { mockGeolocation, mockLocalStorage, mockPlatformService, MockTranslateService } from '@app/testing';
 
 describe('UserGeolocationService', () => {
   let geolocationService: UserGeolocationService;
   let httpClientMock: HttpTestingController;
-  const translateServiceMock = {
-    addLangs: jest.fn(),
-    setFallbackLang: jest.fn(),
-    use: jest.fn()
-  };
-  const platformServiceMock = { isBrowser: jest.fn() };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -57,8 +18,8 @@ describe('UserGeolocationService', () => {
         UserGeolocationService,
         provideHttpClient(),
         provideHttpClientTesting(),
-        { provide: TranslateService, useValue: translateServiceMock },
-        { provide: PlatformService, useValue: platformServiceMock }
+        { provide: TranslateService, useClass: MockTranslateService },
+        { provide: PlatformService, useValue: mockPlatformService }
       ]
     });
 
@@ -74,42 +35,53 @@ describe('UserGeolocationService', () => {
   });
 
   it('Should be created.', () => {
+    const addLangsSpy = jest.spyOn(geolocationService['translateService'], 'addLangs');
+    const setFallbackLangSpy = jest.spyOn(geolocationService['translateService'], 'setFallbackLang');
     expect(geolocationService).toBeTruthy();
-    expect(translateServiceMock.addLangs).toHaveBeenCalledWith(['es', 'en']);
-    expect(translateServiceMock.setFallbackLang).toHaveBeenCalledWith('en');
+    expect(addLangsSpy).toHaveBeenCalledWith(['es', 'en']);
+    expect(setFallbackLangSpy).toHaveBeenCalledWith('en');
   });
 
   describe('loadUserLocation().', () => {
     it('Should return undefined if not platform browser.', () => {
-      platformServiceMock.isBrowser.mockReturnValue(false);
+      mockPlatformService.isBrowser.mockReturnValue(false);
       geolocationService.loadUserLocation('http://api').subscribe(result => {
         expect(result).toBeUndefined();
       });
     });
 
     it('Should return geolocation if localStorage is present.', () => {
-      platformServiceMock.isBrowser.mockReturnValue(true);
-      localStorage.setItem('userLocalLocation', JSON.stringify(mockGeolocation));
+      mockPlatformService.isBrowser.mockReturnValue(true);
+      const useSpy = jest.spyOn(geolocationService['translateService'], 'use');
+      mockLocalStorage.setItem('userLocalLocation', JSON.stringify(mockGeolocation));
       let userLocation: UserGeolocation | undefined;
+
       geolocationService.loadUserLocation('http://api').subscribe(response => {
         userLocation = response;
       });
+
       expect(userLocation).toEqual(mockGeolocation);
+      expect(useSpy).toHaveBeenCalledWith('es');
     });
 
-    it('Should fetch geolocation from API if localStorage in not present.', () => {
-      platformServiceMock.isBrowser.mockReturnValue(true);
+    it('Should fetch geolocation from API if localStorage is not present.', () => {
+      mockPlatformService.isBrowser.mockReturnValue(true);
       const getLocationSpy = jest.spyOn(geolocationService as any, 'getLocation');
+      const useSpy = jest.spyOn(geolocationService['translateService'], 'use');
+      const setItemSpy = jest.spyOn(mockLocalStorage, 'setItem');
       let userLocation: UserGeolocation | undefined;
+
       geolocationService.loadUserLocation('http://api').subscribe(response => {
         userLocation = response;
+        expect(setItemSpy).toHaveBeenCalledWith('userLocalLocation', JSON.stringify(mockGeolocation));
       });
+
       const request = httpClientMock.expectOne(`http://api?apiKey=${environment.ipGeolocationApiKey}`);
       request.flush(mockGeolocation);
       expect(getLocationSpy).toHaveBeenCalledWith('http://api');
       expect(userLocation).toEqual(mockGeolocation);
+      expect(useSpy).toHaveBeenCalledWith('es');
     });
-
   });
 
   describe('getUserLanguage().', () => {
@@ -120,10 +92,5 @@ describe('UserGeolocationService', () => {
     it('Should return ["en-US"] if not includes "es".', () => {
       expect(geolocationService['getUserLanguage']('fr-FR')).toEqual(['en-US']);
     });
-  });
-
-  it('getUserGeolocation() should return cached value.', () => {
-    (geolocationService as any).userGeolocation = mockGeolocation;
-    expect(geolocationService.getUserGeolocation()).toEqual(mockGeolocation);
   });
 });
