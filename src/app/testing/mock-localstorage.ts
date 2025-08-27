d3f53b0bc58796a8131cdc33fc6b64f1b3b2a541
Yesid <yellaber@gmail.com>
2025-08-27 17:01:37 -0500

    refactor(testing): hace que mockLocalStorage sea un mock con estado

---
+++ src/app/testing/mock-localstorage.ts
@@ -0,0 +1,18 @@
+export const mockLocalStorage = (() => {
+  let store: { [key: string]: string } = {};
+
+  return {
+    getItem: jest.fn((key: string) => {
+      return store[key] || null;
+    }),
+    setItem: jest.fn((key: string, value: string) => {
+      store[key] = value.toString();
+    }),
+    removeItem: jest.fn((key: string) => {
+      delete store[key];
+    }),
+    clear: jest.fn(() => {
+      store = {};
+    }),
+  };
+})();
