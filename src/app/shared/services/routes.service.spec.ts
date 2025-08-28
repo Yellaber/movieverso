e543ca974d80e4337f443d9e9a1a09eec22506f8
Yesid <yellaber@gmail.com>
2025-08-27 21:18:43 -0500

    test(shared): agrega pruebas unitarias para RoutesService

--- src/app/shared/services/routes.service.spec.ts
+++ src/app/shared/services/routes.service.spec.ts
@@ -1,21 +1,13 @@
 import { TestBed } from '@angular/core/testing';
-import { Route } from '@angular/router';
 import { RoutesService } from './routes.service';
+import { mockRoutes, mockUpdateRoutes } from '@app/testing';
 
-const mockRoutes: Route[] = [
-  { path: 'home', title: 'Home' }, { path: 'upcoming', title: 'Upcoming' },
-  { path: 'popular', title: 'Popular' }, { path: 'top-rated', title: 'Top Rated' },
-  { path: 'now-playing', title: 'Now Playing' }, { path: 'trending', title: 'Trending' }
-];
-
-const updatedMockRoutes: Route[] = [{ path: 'contact', title: 'Contact' }];
-
-describe('RoutesService', () => {
+describe('RoutesService.', () => {
   let service: RoutesService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [RoutesService]
+      providers: [ RoutesService ]
     });
     service = TestBed.inject(RoutesService);
   });
@@ -24,8 +16,8 @@ describe('RoutesService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('Should have an initial empty array of routes.', () => {
-    expect(service.getRoutes()).toEqual([]);
+  it('Should have one or more routes.', () => {
+    expect(service.getRoutes().length).toBeGreaterThanOrEqual(1);
   });
 
   it('Should set routes correctly.', () => {
@@ -36,8 +28,8 @@ describe('RoutesService', () => {
   it('Should update routes when setRoutes is called again.', () => {
     service.setRoutes(mockRoutes);
     expect(service.getRoutes()).toEqual(mockRoutes);
-    service.setRoutes(updatedMockRoutes);
-    expect(service.getRoutes()).toEqual(updatedMockRoutes);
+    service.setRoutes(mockUpdateRoutes);
+    expect(service.getRoutes()).toEqual(mockUpdateRoutes);
   });
 
   it('Should return a readonly signal.', () => {
