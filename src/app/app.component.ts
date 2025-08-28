c7363d08cbf13e85f4988f0b0039f10d61ca9a1e
Yesid <yellaber@gmail.com>
2025-08-27 20:17:51 -0500

    refactor(core): hace que RoutesService se auto-inicialice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,17 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { rxResource } from '@angular/core/rxjs-interop';
-import { routes } from './app.routes';
 import { AppHeaderComponent } from './core/components/header/app-header.component';
-import { FooterComponent } from './core/components/app-footer/app-footer.component';
 import { environment } from './environments/environment.developments';
-import { UserGeolocationService } from './core/services';
-import { RoutesService } from './shared/services/routes.service';
 
 @Component({
   selector: 'app-root',
   imports: [
     AppHeaderComponent,
-    FooterComponent,
-    RouterOutlet,
+    RouterOutlet
   ],
   templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  private userGeolocationService = inject(UserGeolocationService);
-  private routesService = inject(RoutesService);
-  apiUrlGeolocation = signal<string>(environment.ipGeolocationApiUrl);
-  geoLocation = rxResource({
-    request: this.apiUrlGeolocation,
-    loader: ({ request }) => this.userGeolocationService.loadUserLocation(request)
-  });
   title = environment.appName;
-
-  constructor() {
-    this.routesService.setRoutes(routes);
-  };
-}
+};
--- src/app/shared/services/routes.service.ts
+++ src/app/shared/services/routes.service.ts
@@ -1,5 +1,6 @@
 import { computed, Injectable, signal } from '@angular/core';
 import { Route } from '@angular/router';
+import { routes } from '@app/app.routes';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +8,10 @@ import { Route } from '@angular/router';
 export class RoutesService {
   private routes = signal<Route[]>([]);
   getRoutes = computed(() => this.routes());
+
+  constructor() {
+    this.setRoutes(routes);
+  };
 
   setRoutes(routes: Route[]) {
     this.routes.set(routes);
