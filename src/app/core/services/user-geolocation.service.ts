6dc78b89b5ac8751d21a8ff291b27596a3c02759
Yesid <yellaber@gmail.com>
2025-08-28 01:25:21 -0500

    refactor(core): refactoriza UserGeolocationService para usar rxResource

--- src/app/core/services/user-geolocation.service.ts
+++ src/app/core/services/user-geolocation.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { computed, inject, Injectable, signal } from '@angular/core';
+import { map, Observable, of, tap } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
-import { Observable, of, tap } from 'rxjs';
 import { PlatformService } from '@shared/services';
 import { UserGeolocation } from '@shared/interfaces';
 import { environment } from '@app/environments/environment.developments';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 const USER_LOCAL_LOCATION = 'userLocalLocation';
 
@@ -15,8 +16,12 @@ export class UserGeolocationService {
   private platformService = inject(PlatformService);
   private httpClient = inject(HttpClient);
   private translateService = inject(TranslateService);
-  private userGeolocation = signal<UserGeolocation | undefined>(undefined);
-  getUserGeolocation = computed(() => this.userGeolocation());
+  private ipGeolocationApiUrl = signal<string>(environment.ipGeolocationApiUrl);
+  private geoLocation = rxResource({
+    request: this.ipGeolocationApiUrl,
+    loader: ({ request }) => this.loadUserLocation(request)
+  });
+  getUserGeolocation = computed(() => this.geoLocation.hasValue()? this.geoLocation.value(): undefined);
 
   constructor() {
     this.translateService.addLangs(['es', 'en']);
@@ -27,9 +32,9 @@ export class UserGeolocationService {
     if(!this.platformService.isBrowser()) { return of(undefined); }
     const userLocation = localStorage.getItem(USER_LOCAL_LOCATION);
     if(userLocation) {
-      this.userGeolocation.set(JSON.parse(userLocation) as UserGeolocation);
-      this.translateService.use(this.userGeolocation()!.country_metadata.languages[0].split('-')[0]);
-      return of(this.userGeolocation());
+      const userGeolocation = JSON.parse(userLocation) as UserGeolocation;
+      this.translateService.use(userGeolocation.country_metadata.languages[0].split('-')[0]);
+      return of(userGeolocation);
     }
     return this.getLocation(apiUrlGeolocation);
   };
@@ -38,12 +43,14 @@ export class UserGeolocationService {
     const url = `${apiUrlGeolocation}?apiKey=${environment.ipGeolocationApiKey}`;
     return this.httpClient.get<UserGeolocation>(url)
       .pipe(
-        tap(geoLocation => {
+        map(geoLocation => {
           const { country_metadata } = geoLocation;
           country_metadata.languages = this.getUserLanguage(country_metadata.languages[0]);
-          this.userGeolocation.set({ ...geoLocation, country_metadata });
-          localStorage.setItem(USER_LOCAL_LOCATION, JSON.stringify(this.userGeolocation()));
-          this.translateService.use(country_metadata.languages[0].split('-')[0]);
+          return { ...geoLocation, country_metadata };
+        }),
+        tap(geoLocation => {
+          localStorage.setItem(USER_LOCAL_LOCATION, JSON.stringify(geoLocation));
+          this.translateService.use(geoLocation.country_metadata.languages[0].split('-')[0]);
         })
       );
   };
