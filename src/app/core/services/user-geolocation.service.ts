35171f5f85faf51c36603b2169067eba1ddd4b8a
Yesid <yellaber@gmail.com>
2025-08-29 15:48:23 -0500

    refactor(core): cambia UserGeolocationService a un patrón de carga bajo demanda

--- src/app/core/services/user-geolocation.service.ts
+++ src/app/core/services/user-geolocation.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { computed, inject, Injectable, signal } from '@angular/core';
-import { map, tap } from 'rxjs';
+import { map, Observable, of, tap } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
 import { PlatformService } from '@shared/services';
 import { UserGeolocation } from '@shared/interfaces';
@@ -15,44 +15,44 @@ export class UserGeolocationService {
   private platformService = inject(PlatformService);
   private httpClient = inject(HttpClient);
   private translateService = inject(TranslateService);
-  private geoLocation = signal<UserGeolocation | undefined>(undefined);
-  getUserGeolocation = computed(() => this.geoLocation()?? undefined);
+  private geolocation = signal<UserGeolocation | undefined>(undefined);
+  getUserGeolocation = computed(() => this.geolocation()?? undefined);
 
   constructor() {
     this.translateService.addLangs(['es', 'en']);
     this.translateService.setFallbackLang('en');
-    this.loadUserLocation();
   };
 
-  private loadUserLocation() {
+  loadUserLocation(): Observable<UserGeolocation | undefined> {
     if(!this.platformService.isBrowser()) {
-      this.geoLocation.set(undefined);
-      return;
+      this.geolocation.set(undefined);
+      return of(undefined);
     }
     const userLocation = localStorage.getItem(USER_LOCAL_LOCATION);
     if(userLocation) {
       const userGeolocation = JSON.parse(userLocation) as UserGeolocation;
       this.translateService.use(userGeolocation.country_metadata.languages[0].split('-')[0]);
-      this.geoLocation.set(userGeolocation);
-      return;
+      this.geolocation.set(userGeolocation);
+      return of(userGeolocation);
     }
-    this.getLocation();
+    return this.getLocation();
   };
 
-  private getLocation() {
+  private getLocation(): Observable<UserGeolocation> {
     const url = `${environment.ipGeolocationApiUrl}?apiKey=${environment.ipGeolocationApiKey}`;
-    this.httpClient.get<UserGeolocation>(url)
+    return this.httpClient.get<UserGeolocation>(url)
       .pipe(
-        map(geoLocation => {
-          const { country_metadata } = geoLocation;
+        map(geolocation => {
+          const { country_metadata } = geolocation;
           country_metadata.languages = this.getUserLanguage(country_metadata.languages[0]);
-          return { ...geoLocation, country_metadata };
+          return { ...geolocation, country_metadata };
         }),
-        tap(geoLocation => {
-          localStorage.setItem(USER_LOCAL_LOCATION, JSON.stringify(geoLocation));
-          this.translateService.use(geoLocation.country_metadata.languages[0].split('-')[0]);
+        tap(geolocation => {
+          localStorage.setItem(USER_LOCAL_LOCATION, JSON.stringify(geolocation));
+          this.translateService.use(geolocation.country_metadata.languages[0].split('-')[0]);
+          this.geolocation.set(geolocation);
         })
-      ).subscribe(geoLocation => this.geoLocation.set(geoLocation));
+      )
   };
 
   private getUserLanguage(language: string): string[] {
