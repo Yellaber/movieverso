e69b832edafd6a7cb4496762ad7bd60918e257bf
Yesid <yellaber@gmail.com>
2025-09-01 18:39:33 -0500

    test(shared): agrega pruebas unitarias para QueryParamsService

--- src/app/shared/services/query-params.service.spec.ts
+++ src/app/shared/services/query-params.service.spec.ts
@@ -1,20 +1,54 @@
 import { TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 import { initialQueryParams, QueryParamsService } from './query-params.service';
-import { mockQueryParams } from '@app/testing';
+import { MockActivedRouteServiceWithoutParams, MockActivedRouteServiceWithParams, mockQueryParams } from '@app/testing';
 
 describe('QueryParamsService', () => {
   let service: QueryParamsService;
+  let route: ActivatedRoute;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [ QueryParamsService ]
+  describe('When active route contain query params.', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        providers: [
+          QueryParamsService,
+          { provide: ActivatedRoute, useClass: MockActivedRouteServiceWithParams }
+        ]
+      });
+      service = TestBed.inject(QueryParamsService);
+      route = TestBed.inject(ActivatedRoute);
     });
-    service = TestBed.inject(QueryParamsService);
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it('Should be created and set the queryParams.', () => {
+      expect(service).toBeTruthy();
+      expect(service.getQueryParams()).not.toBe(initialQueryParams);
+    });
   });
 
-  it('Should be created and have an initial value.', () => {
-    expect(service).toBeTruthy();
-    expect(service.getQueryParams()).toBe(initialQueryParams);
+  describe('When active route not contain query params.', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        providers: [
+          QueryParamsService,
+          { provide: ActivatedRoute, useClass: MockActivedRouteServiceWithoutParams }
+        ]
+      });
+      service = TestBed.inject(QueryParamsService);
+      route = TestBed.inject(ActivatedRoute);
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it('Should be created and set the initial queryParams.', () => {
+      expect(service).toBeTruthy();
+      expect(service.getQueryParams()).toStrictEqual(initialQueryParams);
+    });
   });
 
   it('Should update the queryParams when set is called.', () => {
