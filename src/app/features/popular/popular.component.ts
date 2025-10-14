f869f171020861ef66a94707174b09bc1edb319b
Yesid <yellaber@gmail.com>
2025-10-14 17:19:19 -0500

    Se refactorizaron los componentes upcoming, now-playing, top-rated, popular y trending

--- src/app/features/popular/popular.component.ts
+++ src/app/features/popular/popular.component.ts
@@ -1,50 +1,10 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { CategoriesComponent } from '@shared/components/categories/categories.component';
-import { LoadCategoryComponent } from '@shared/components/load-category/load-category.component';
-import { SeoFriendlyService } from '@app/core/services';
-import { EndPointValid } from '@shared/interfaces';
-
-const menuItems = ['upcoming', 'now-playing', 'top-rated', 'trending'];
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CategoryComponent } from '@shared/components/category/category.component';
 
 @Component({
   selector: 'popular',
-  imports: [
-    CategoriesComponent,
-    LoadCategoryComponent,
-  ],
-  template: `
-    <div class="flex flex-col gap-5 mt-10 lg:mt-15">
-      <h3 class="text-sm lg:text-xl font-bold rounded-full text-yellow-600 gap-2">
-        {{ titlePage() }}
-      </h3>
-      <p class="text-xs lg:text-sm text-stone-300 leading-relaxed">
-        {{ textPage() }}
-      </p>
-    </div>
-    <categories [menuItems]="menuItems()"/>
-    <load-category [endPoint]="endPointValid()"/>
-  `,
+  imports: [ CategoryComponent ],
+  template: `<category name="popular"/>`,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export default class PopularComponent implements OnInit {
-  private seoFriendlyService = inject(SeoFriendlyService);
-  private translateService = inject(TranslateService);
-  titlePage = signal<string>('');
-  textPage = signal<string>('');
-  menuItems = signal<string[]>([]);
-  endPointValid = signal<EndPointValid>(EndPointValid.popular);
-
-  ngOnInit(): void {
-    this.menuItems.set(menuItems);
-    this.loadTranslations();
-    this.seoFriendlyService.setMetaTags(this.titlePage(), this.textPage());
-  };
-
-  private loadTranslations() {
-    this.translateService.get('routes.popular')
-      .subscribe((title: string) => this.titlePage.set(title));
-    this.translateService.get('popular.paragraph')
-      .subscribe((paragraph: string) => this.textPage.set(paragraph));
-  };
-}
+export default class PopularComponent { }
